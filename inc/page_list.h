fe9a324fa271578f1ce4008d3e97497e9b0477cf
rohan <rohandvivedi@gmail.com>
2021-03-11 22:51:20 +0530

    added page cursor and page list definitons and declarations, to the project

--- inc/page_list.h
+++ inc/page_list.h
@@ -21,7 +21,7 @@ typedef enum page_lock_type page_lock_type;
 enum page_lock_type
 {
 	READ_LOCKED,
-	WRITE_LOCKED
+	WRITE_LOCKED,
 };
 
 // for reading tuples inside the page you need a page cursor
@@ -29,7 +29,7 @@ typedef struct page_cursor page_cursor;
 struct page_cursor
 {
 	// the page_list that this page cursor is meant to read
-	page_list* pl_p
+	page_list* pg_list_p;
 
 	// the page in side the page_list that this page_cursor is reading
 	void* page;
@@ -47,13 +47,16 @@ struct page_cursor
 	uint16_t tuple_index;	// tuple_index of the tuple in the page
 };
 
-const void* find_in_page_list(const page_list* pl_p, const void* tuple_like, const data_access_methods* dam_p);
+// returns 0 if there are no new tuple in the page_list, else returns 1
+int seek_to_next_tuple_in_page_list(page_cursor* pc_p, const data_access_methods* dam_p);
 
-int insert_in_page_list(page_list* pl_p, const void* tuple_like, const data_access_methods* dam_p);
+int insert_at_end_in_page_list(page_list* pl_p, const void* tuple_like, const data_access_methods* dam_p);
 
-int delete_in_page_list(page_list* pl_p, const void* tuple_like, const data_access_methods* dam_p);
+// page_cursor points to the next tuple in the page_list, and return 1
+// else it returns 0 (if the end of page_list is reached)
+int delete_tuple_at_the_cursor(page_list* pl_p, const void* tuple_like, const data_access_methods* dam_p);
 
 // external merge sort goes here
-int create_externally_sorted_page_list();
+void external_merge_sort_the_page_list(page_list* pl_p);
 
 #endif
--- src/in_memory_data_store.c
+++ src/in_memory_data_store.c
@@ -1,1 +1,1 @@
-#include<in_memory_data_store.c>
+#include<in_memory_data_store.h>
