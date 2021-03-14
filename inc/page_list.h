810825cd8ba1e84f9757304ff5ca1c2d741ef784
rohan <rohandvivedi@gmail.com>
2021-03-14 14:57:03 +0530

    --

--- inc/page_list.h
+++ inc/page_list.h
@@ -12,34 +12,46 @@
 #define NULL_PAGE_REFERENCE (~0U)
 
 #define NEXT_PAGE_REFERENCE_INDEX  0
-#define PREV_PAGE_REFERENCE_INDEX  1
+//#define PREV_PAGE_REFERENCE_INDEX  1
 
-typedef struct page_list page_list;
-struct page_list
+// for reading tuples inside the page_list you need a page cursor
+typedef struct page_cursor page_cursor;
+struct page_cursor
 {
-	rwlock page_list_lock;
+	// the page of the page_list that this page_cursor is reading
+	// this is the page that this cursor has locked for the lock_type (as READER or WRITER)
+	void* page;
+	uint32_t page_id;		// page_id of the page
 
-	// the head page of the page_list
-	uint32_t head_page_id;
-
-	// the head page of the page_list
-	uint32_t tail_page_id;
-
-	// the tuple definition of the data at each of the pages
-	tuple_def* tuple_definition;
+	// the tuple that this page_cursor is pointing to at this instance
+	void* tuple;
+	uint16_t tuple_index;	// tuple_index of the tuple in the page
 };
 
-// page_list functions
+// creates a new page_list and returns the id to head page of the the newly created page_list
+uint32_t get_new_page_list(const data_access_methods* dam_p);
 
-int initialize_page_list(page_list* pl_p, uint32_t head_page_id, uint32_t tail_page_id, tuple_def* tpl_d);
+// page_cursor functions
 
-int insert_in_page_list(page_list* pl_p, const void* tuple_like, const data_access_methods* dam_p);
+void initialize_cursor(page_cursor* pc_p, uint32_t page_list_page_id, const data_access_methods* dam_p);
 
-int deinitialize_page_list(page_list* pl_p);
+// page_cursor points to the next tuple in the page_list, and return 1
+// else it returns 0 (if the end of page_list is reached)
+int seek_to_next_tuple_in_page_list(page_cursor* pc_p, const data_access_methods* dam_p);
+
+// if deleted successfully, page_cursor points to the next tuple in the page_list, and returns 1
+// else it returns 0 (if the end of page_list is reached)
+int delete_tuple_at_the_cursor(page_cursor* pc_p, const void* tuple_like, const data_access_methods* dam_p);
+
+// if deleted successfully, page_cursor points to the newlu inserted tuple in the page_list, and returns 1
+// else it returns 0 (if the end of page_list is reached)
+int insert_tuple_after_the_cursor(page_cursor* pc_p, const void* tuple_like, const data_access_methods* dam_p);
+
+void deinitialize_cursor(page_cursor* pc_p);
 
 // external merge sort
 
 // here : key_elements_count denote the number of elements to sort on
-void external_merge_sort_the_page_list(page_list* pl_p, uint16_t key_elements_count, const data_access_methods* dam_p);
+void external_merge_sort_the_page_list(uint32_t page_list_head_page_id, uint16_t key_elements_count, const data_access_methods* dam_p);
 
 #endif
--- src/page_list.c
+++ src/page_list.c
@@ -1,72 +1,24 @@
 #include<page_list.h>
 
-static int is_head_page(page_list* pl_p, uint32_t page_id)
+uint32_t get_new_page_list(const data_access_methods* dam_p);
+
+void initialize_cursor(page_cursor* pc_p, uint32_t page_list_page_id, const data_access_methods* dam_p)
 {
-	return (pl_p->head_page_id != NULL_PAGE_REFERENCE) && (pl_p->head_page_id == page_id);
+	pc_p->page_id = page_list_page_id;
+	pc_p->page = NULL;
+	pc_p->tuple_index = 0;
+	pc_p->tuple = NULL;
 }
 
-int initialize_page_list(page_list* pl_p, uint32_t head_page_id, uint32_t tail_page_id, tuple_def* tpl_d)
+int seek_to_next_tuple_in_page_list(page_cursor* pc_p, const data_access_methods* dam_p);
+
+int delete_tuple_at_the_cursor(page_cursor* pc_p, const void* tuple_like, const data_access_methods* dam_p);
+
+int insert_tuple_after_the_cursor(page_cursor* pc_p, const void* tuple_like, const data_access_methods* dam_p);
+
+void deinitialize_cursor(page_cursor* pc_p)
 {
-	initialize_rwlock(&(pl_p->page_list_lock));
-	pl_p->head_page_id = head_page_id;
-	pl_p->tail_page_id = tail_page_id;
-	pl_p->tuple_definition = tpl_d;
+
 }
 
-int insert_in_page_list(page_list* pl_p, const void* tuple_like, const data_access_methods* dam_p)
-{
-	int inserted = 0;
-	write_lock(&(pl_p->page_list_lock));
-
-	void* old_tail_page = NULL;
-
-	if(pl_p->tail_page_id != NULL_PAGE_REFERENCE)
-	{
-		void* tail_page = dam_p->acquire_page_with_writer_lock(dam_p->context, pl_p->tail_page_id);
-		inserted = insert_tuple(tail_page, dam_p->page_size, pl_p->tuple_definition, tuple_like);
-
-		if(inserted)
-			dam_p->release_writer_lock_on_page(dam_p->context, tail_page);
-		else
-			old_tail_page = tail_page;
-	}
-
-	if(!inserted)
-	{
-		uint32_t new_page_id = NULL_PAGE_REFERENCE;
-		void* new_page = dam_p->get_new_page_with_write_lock(dam_p->context, &new_page_id);
-
-		int initialized = init_page(new_page, dam_p->page_size, 0, 2, pl_p->tuple_definition);
-		if(initialized)
-		{
-			inserted = insert_tuple(new_page, dam_p->page_size, pl_p->tuple_definition, tuple_like);
-
-			set_reference_page_id(new_page, NEXT_PAGE_REFERENCE_INDEX, NULL_PAGE_REFERENCE);
-			set_reference_page_id(new_page, PREV_PAGE_REFERENCE_INDEX, pl_p->tail_page_id);
-
-			dam_p->release_writer_lock_on_page(dam_p->context, new_page);
-
-			pl_p->tail_page_id = new_page_id;
-			if(pl_p->head_page_id == NULL_PAGE_REFERENCE)
-				pl_p->head_page_id = new_page_id;
-		}
-		else
-			dam_p->release_writer_lock_and_free_page(dam_p->context, new_page);
-
-		if(old_tail_page != NULL)
-		{
-			set_reference_page_id(old_tail_page, NEXT_PAGE_REFERENCE_INDEX, new_page_id);
-			dam_p->release_writer_lock_on_page(dam_p->context, old_tail_page);
-		}
-	}
-
-	write_unlock(&(pl_p->page_list_lock));
-	return inserted;
-}
-
-int deinitialize_page_list(page_list* pl_p)
-{
-	deinitialize_rwlock(&(pl_p->page_list_lock));
-}
-
-void external_merge_sort_the_page_list(page_list* pl_p, uint16_t key_elements_count, const data_access_methods* dam_p);
+void external_merge_sort_the_page_list(uint32_t page_list_head_page_id, uint16_t key_elements_count, const data_access_methods* dam_p);
