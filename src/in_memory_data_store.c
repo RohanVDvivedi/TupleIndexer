3a14c9670138ab6027bf752309a7e268c2acc793
rohan <rohandvivedi@gmail.com>
2021-03-15 19:37:07 +0530

    lock acquisition functions built

--- src/in_memory_data_store.c
+++ src/in_memory_data_store.c
@@ -8,6 +8,16 @@
 #include<stdlib.h>
 #include<sys/mman.h>
 
+#define IS_FREE_FLAG 0x00000001
+
+typedef struct page_state page_state;
+struct page_state
+{
+	int flags;
+
+	rwlock reader_writer_page_lock;
+};
+
 typedef struct memory_store_context memory_store_context;
 struct memory_store_context
 {
@@ -20,12 +30,12 @@ struct memory_store_context
 	// count
 	void* memory;	// points to mmap-ed memory of size (page_size * page_count)
 
-	// this lock protects free_pages_list
+	// this lock protects free_pages_list and the flags inside each of the page_state
 	pthread_mutex_t free_pages_lock;
 
 	linkedlist free_pages;
 
-	rwlock reader_writer_page_locks[];
+	page_state page_states[];
 };
 
 static int open_data_file(void* context)
@@ -40,7 +50,8 @@ static int open_data_file(void* context)
 		void* mem = cntxt->memory + (cntxt->page_size * i);
 		initialize_llnode(mem);
 		insert_tail(&(cntxt->free_pages), mem);
-		initialize_rwlock(&(cntxt->reader_writer_page_locks[i]));
+		cntxt->page_states[i].flags |= IS_FREE_FLAG;
+		initialize_rwlock(&(cntxt->page_states[i].reader_writer_page_lock));
 	}
 
 	return 1;
@@ -50,36 +61,103 @@ static void* get_new_page_with_write_lock(void* context, uint32_t* page_id_retur
 {
 	memory_store_context* cntxt = context;
 
+	// we have to find this free page and return it
+	void* free_page_p = NULL;
+	uint32_t free_page_id = 0;
+
 	pthread_mutex_lock(&(cntxt->free_pages_lock));
 
-	void* mem = (void*) get_head(&(cntxt->free_pages));
-	remove_head(&(cntxt->free_pages));
+		// if the free_pages list is not empty, then get a free_page from it
+		if(!is_empty_linkedlist(&(cntxt->free_pages)))
+		{
+			// get a free page from the head of the free_pages list, and remove it
+			free_page_p = (void*) get_head(&(cntxt->free_pages));
+			remove_head(&(cntxt->free_pages));
+			
+			// calculate its index
+			uint32_t free_page_id = ((uintptr_t)(cntxt-> memory - free_page_p)) / cntxt->page_size;
+
+			// clear the free flag bit
+			cntxt->page_states[free_page_id].flags &= (~IS_FREE_FLAG);
+		}
 
 	pthread_mutex_unlock(&(cntxt->free_pages_lock));
 
-	(*page_id_returned) = ((uintptr_t)(cntxt->memory - mem)) / cntxt->page_size;
+	// attempt to acquire a write lock and return free_page_id, if free_page_p is found
+	if(free_page_p != NULL)
+	{
+		(*page_id_returned) = free_page_id;
 
-	write_lock(&(cntxt->reader_writer_page_locks[(*page_id_returned)]));
+		write_lock(&(cntxt->page_states[free_page_id].reader_writer_page_lock));
+	}
 
-	return mem;
+	return free_page_p;
 }
 
 static void* acquire_page_with_reader_lock(void* context, uint32_t page_id)
 {
 	memory_store_context* cntxt = context;
 
-	read_lock(&(cntxt->reader_writer_page_locks[page_id]));
+	// page_id is out of bounds
+	if(page_id >= cntxt->pages_count)
+		return 0;
 
-	return cntxt->memory + (page_id * cntxt->page_size);
+	// to check if the page that you requested is not free
+	// i.e. you can not directly acquire a lock to the free page (use the methof above)
+	int is_free_page = 0;
+
+	// calculate the page pointer, that you might have to return
+	void* page = cntxt->memory + (page_id * cntxt->page_size);
+
+	pthread_mutex_lock(&(cntxt->free_pages_lock));
+
+		// check if the page is free
+		is_free_page = (cntxt->page_states[page_id].flags & IS_FREE_FLAG);
+
+	pthread_mutex_unlock(&(cntxt->free_pages_lock));
+
+	// you may acquire the lock only if it is not free
+	if(!is_free_page)
+	{
+		read_lock(&(cntxt->page_states[page_id].reader_writer_page_lock));
+
+		return page;
+	}
+	else
+		return NULL;
 }
 
 static void* acquire_page_with_writer_lock(void* context, uint32_t page_id)
 {
 	memory_store_context* cntxt = context;
 
-	write_lock(&(cntxt->reader_writer_page_locks[page_id]));
+	// page_id is out of bounds
+	if(page_id >= cntxt->pages_count)
+		return 0;
 
-	return cntxt->memory + (page_id * cntxt->page_size);
+	// to check if the page that you requested is not free
+	// i.e. you can not directly acquire a lock to the free page (use the methof above)
+	int is_free_page = 0;
+
+	// calculate the page pointer, that you might have to return
+	void* page = cntxt->memory + (page_id * cntxt->page_size);
+
+	pthread_mutex_lock(&(cntxt->free_pages_lock));
+
+		// check if the page is free
+		is_free_page = (cntxt->page_states[page_id].flags & IS_FREE_FLAG);
+
+	pthread_mutex_unlock(&(cntxt->free_pages_lock));
+
+	// you may acquire the lock only if it is not free
+	if(!is_free_page)
+	{
+		write_lock(&(cntxt->page_states[page_id].reader_writer_page_lock));
+
+		return page;
+	}
+	else
+		return NULL;
 }
 
 static int downgrade_writer_lock_to_reader_lock_on_page(void* context, void* pg_ptr)
@@ -174,7 +252,7 @@ data_access_methods* get_new_in_memory_data_store(uint32_t page_size, uint32_t p
 	dam_p->force_write_to_disk = force_write_to_disk;
 	dam_p->close_data_file = close_data_file;
 	dam_p->page_size = page_size;
-	dam_p->context = malloc(sizeof(memory_store_context) + (pages_count * sizeof(rwlock)));
+	dam_p->context = malloc(sizeof(memory_store_context) + (pages_count * sizeof(page_state)));
 	
 	((memory_store_context*)(dam_p->context))->page_size = page_size;
 	((memory_store_context*)(dam_p->context))->pages_count = pages_count;
