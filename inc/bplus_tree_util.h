60dbc14215889fecac00a64689fd9aa2ea400abd
rohan <rohandvivedi@gmail.com>
2021-03-20 21:41:00 +0530

    we will need to implement a tuple definitions creator function

--- inc/bplus_tree_util.h
+++ inc/bplus_tree_util.h
@@ -13,4 +13,25 @@
 #define NEXT_PAGE_REF       0
 #define PREV_PAGE_REF       1
 
+typedef struct bplus_tree_tuple_defs bplus_tree_tuple_defs;
+struct bplus_tree_tuple_defs
+{
+	// tuple definition to compare keys in the bplus_tree
+	tuple_def* key_def;
+
+	// tuple definition of the interior pages in the bplus_tree
+	// this is equal to { key_def : page_id(u4)) }
+	tuple_def* index_def;
+
+	// tuple definition of the leaf pages in the bplus_tree
+	// this is equal to { key_def : value_def }
+	tuple_def* record_def;
+};
+
+// you may generate tuple definitions before performing any operations
+const bplus_tree_tuple_defs* get_bplus_tree_tuple_defs(tuple_def* record_def, uint16_t key_element_count);
+
+// and then discard when your work with bplus tree is completed
+void del_bplus_tree_tuple_defs(bplus_tree_tuple_defs* bpttds);
+
 #endif
