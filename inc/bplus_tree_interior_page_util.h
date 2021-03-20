f0aba7f7c96b5f5ccb21b213987e0627c322b606
rohan <rohandvivedi@gmail.com>
2021-03-20 19:46:18 +0530

    added util functions

---
+++ inc/bplus_tree_interior_page_util.h
@@ -0,0 +1,8 @@
+#ifndef BPLUS_TREE_INTERIOR_PAGE_UTIL_H
+#define BPLUS_TREE_INTERIOR_PAGE_UTIL_H
+
+#include<bplus_tree_util.h>
+
+int is_interior_node_page(const void* page);
+
+#endif
---
+++ inc/bplus_tree_leaf_page_util.h
@@ -0,0 +1,8 @@
+#ifndef BPLUS_TREE_LEAF_PAGE_UTIL_H
+#define BPLUS_TREE_LEAF_PAGE_UTIL_H
+
+#include<bplus_tree_util.h>
+
+int is_leaf_node_page(const void* page);
+
+#endif
