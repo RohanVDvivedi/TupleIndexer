6ed9288d3783b60c6134bfd29afbe3526b5aba03
rohan <rohandvivedi@gmail.com>
2021-03-21 11:07:37 +0530

    added simple internal apis for handling find and inserts

--- inc/bplus_tree_interior_page_util.h
+++ inc/bplus_tree_interior_page_util.h
@@ -5,4 +5,14 @@
 
 int is_interior_node_page(const void* page);
 
+// returns page_id, to search for find_like tuple
+uint32_t find_in_interior_node_page(const void* page, const void* like_key);
+
+// inserts a new_index_tuple to the intr_page
+// if the insert fails due to the page being out of space
+// then this interior page is split and the non-NULL index tuple is created
+// now this non Null index tuple needs to be inserted into the parent interior page
+// the retunred tuple is allocated using malloc and the calling function has to deallocate it after use/insert
+const void* insert_or_split_interior_node_page(const void* intr_page, const void* new_index_tuple);
+
 #endif
--- inc/bplus_tree_leaf_page_util.h
+++ inc/bplus_tree_leaf_page_util.h
@@ -8,4 +8,14 @@ int is_leaf_node_page(const void* page);
 uint32_t get_next_leaf_node_page(const void* page);
 uint32_t get_prev_leaf_node_node(const void* page);
 
+// returns index to the record tuple in the page, that equals the find_like tuple
+uint16_t find_in_interior_node_page(const void* page, const void* like_key);
+
+// inserts a new_record_tuple to the leaf_page
+// if the insert fails due to the page being out of space
+// then this leaf page is split and the non-NULL index tuple is created
+// now this non Null index tuple needs to be inserted into the parent interior page
+// the retunred tuple is allocated using malloc and the calling function has to deallocate it after use/insert
+const void* insert_or_split_leaf_node_page(const void* intr_page, const void* new_record_tuple);
+
 #endif
