8ea7e26dad7f1c14fd7e0c64506c1324571e7dc6
rohan <rohandvivedi@gmail.com>
2021-03-21 12:21:57 +0530

    added declarations for api methods

--- inc/bplus_tree.h
+++ inc/bplus_tree.h
@@ -11,9 +11,12 @@
 
 uint32_t create_new_bplus_tree(const data_access_methods* dam_p);
 
-const void* search_in_bplus_tree(const data_access_methods* dam_p);
+const void* find_in_bplus_tree(const void* key_like, const bplus_tree_tuple_defs* bpttds, const data_access_methods* dam_p);
 
-insert_in_bpus_tree(const data_access_methods* dam_p);
+void insert_in_bplus_tree(const void* record, const bplus_tree_tuple_defs* bpttds, const data_access_methods* dam_p);
 
+int insert_if_not_exists_in_bplus_tree(const void* record, const bplus_tree_tuple_defs* bpttds, const data_access_methods* dam_p);
+
+int delete_in_bplus_tree(const void* key_like, const bplus_tree_tuple_defs* bpttds, const data_access_methods* dam_p);
 
 #endif
