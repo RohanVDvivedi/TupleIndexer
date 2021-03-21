f1f7446fb966bd30cf880e5e784c40c2e5384d5c
rohan <rohandvivedi@gmail.com>
2021-03-21 11:48:34 +0530

    added more utility function definition in bplus_tree

--- inc/bplus_tree_interior_page_util.h
+++ inc/bplus_tree_interior_page_util.h
@@ -3,16 +3,29 @@
 
 #include<bplus_tree_util.h>
 
-int is_interior_node_page(const void* page);
+int is_interior_page(const void* page);
 
-// returns page_id, to search for find_like tuple
-uint32_t find_in_interior_node_page(const void* page, const void* like_key);
+/*
+**
+**	each tuple in the interior page is of the form 
+**		: { Key : page_id(u4) }
+**			where all the records greater than equal to Key and lesser than next Key are stored at the corresponding page_id
+**
+**	all the records lesser than the first (0th) key are stored at the page_references[ALL_LEAST_REF]
+**
+*/
+
+// returns a pointer to the tuple at index-th position that has tuple definition of index_def
+const void* get_index_entry_from_interior_page(const void* page, uint16_t index, const bplus_tree_tuple_defs* bpttds);
+
+// returns page_id, to search for the least of the tuples that has key equivalent to like_key
+uint32_t find_in_interior_page(const void* page, const void* like_key, const bplus_tree_tuple_defs* bpttds);
 
 // inserts a new_index_tuple to the intr_page
 // if the insert fails due to the page being out of space
 // then this interior page is split and the non-NULL index tuple is created
 // now this non Null index tuple needs to be inserted into the parent interior page
 // the retunred tuple is allocated using malloc and the calling function has to deallocate it after use/insert
-const void* insert_or_split_interior_node_page(const void* intr_page, const void* new_index_tuple);
+const void* insert_or_split_interior_page(const void* intr_page, const void* new_index_tuple, const bplus_tree_tuple_defs* bpttds);
 
 #endif
--- inc/bplus_tree_leaf_page_util.h
+++ inc/bplus_tree_leaf_page_util.h
@@ -3,19 +3,25 @@
 
 #include<bplus_tree_util.h>
 
-int is_leaf_node_page(const void* page);
+int is_leaf_page(const void* page);
 
-uint32_t get_next_leaf_node_page(const void* page);
-uint32_t get_prev_leaf_node_node(const void* page);
+uint32_t get_next_leaf_page(const void* page);
+uint32_t get_prev_leaf_page(const void* page);
 
-// returns index to the record tuple in the page, that equals the find_like tuple
-uint16_t find_in_interior_node_page(const void* page, const void* like_key);
+// returns number of records in the leaf page
+uint32_t get_record_count_in_leaf_page(const void* page, const bplus_tree_tuple_defs* bpttds);
+
+// returns a pointer in the page that points to index-th record, and has tuple definition of that of a record_def
+const void* get_record_from_leaf_page(const void* page, uint16_t index, const bplus_tree_tuple_defs* bpttds);
+
+// returns index to the first record tuple in the page, that has key that equals the like_key
+uint16_t find_in_leaf_page(const void* page, const void* like_key, const bplus_tree_tuple_defs* bpttds);
 
 // inserts a new_record_tuple to the leaf_page
 // if the insert fails due to the page being out of space
 // then this leaf page is split and the non-NULL index tuple is created
 // now this non Null index tuple needs to be inserted into the parent interior page
 // the retunred tuple is allocated using malloc and the calling function has to deallocate it after use/insert
-const void* insert_or_split_leaf_node_page(const void* intr_page, const void* new_record_tuple);
+const void* insert_or_split_leaf_page(const void* intr_page, const void* new_record_tuple, const bplus_tree_tuple_defs* bpttds);
 
 #endif
