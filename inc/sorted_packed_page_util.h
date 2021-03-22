a4882e805dea021d491a27c98a32538066a1dae3
rohan <rohandvivedi@gmail.com>
2021-03-22 12:04:40 +0530

    added method declarations for managing sorted_packed pages

--- inc/sorted_packed_page_util.h
+++ inc/sorted_packed_page_util.h
@@ -2,9 +2,37 @@
 #define SORTED_PACKED_PAGE_UTIL_H
 
 // find using binary search in sorted page
+// returns index of the tuple found if, return value == 1
+int search_in_sorted_packed_page(
+									const void* page, uint32_t page_size, 
+									const tuple_def* key_def, const tuple_def* tuple_def, 
+									const void* key, 
+									uint16_t* index
+								);
 
-// insert with record and key definition
+// insert tuple
+// returns the index of the inserted tuple if, return value == 1
+int insert_to_sorted_packed_page(
+									void* page, uint32_t page_size, 
+									const tuple_def* key_def, const tuple_def* tuple_def, 
+									const void* tuple, 
+									uint16_t* index
+								);
 
-// delete with recors and key definition
+// delete tuple at given index
+// returns 1, if the tuple was deleted
+int delete_in_sorted_packed_page(
+									void* page, uint32_t page_size, 
+									const tuple_def* tuple_def, 
+									uint16_t index
+								);
+
+// move n tuples from src page to dest page
+// effectively the tuples moved, will be deleted in src_page and appear in dest_page
+int transfer_to_sorted_packed_page(
+									void* src_page, void* dest_page, uint32_t page_size, 
+									const tuple_def* tuple_def, 
+									uint16_t start_index, uint16_t end_index
+								);
 
 #endif
