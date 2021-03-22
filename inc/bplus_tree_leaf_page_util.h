595614ccb998f5eb64e74ae4a148079f0f3005cd
rohan <rohandvivedi@gmail.com>
2021-03-22 10:44:43 +0530

    method declarations change as per requirement

--- inc/bplus_tree_leaf_page_util.h
+++ inc/bplus_tree_leaf_page_util.h
@@ -9,13 +9,13 @@ uint32_t get_next_leaf_page(const void* page);
 uint32_t get_prev_leaf_page(const void* page);
 
 // returns number of records in the leaf page
-uint16_t get_record_count_in_leaf_page(const void* page, const bplus_tree_tuple_defs* bpttds);
+uint16_t get_record_count_in_leaf_page(const void* page);
 
 // returns a pointer in the page that points to index-th record, and has tuple definition of that of a record_def
-const void* get_record_from_leaf_page(const void* page, uint16_t index, const bplus_tree_tuple_defs* bpttds);
+const void* get_record_from_leaf_page(const void* page, uint32_t page_size, uint16_t index, const bplus_tree_tuple_defs* bpttds);
 
 // returns index to the first record tuple in the page, that has key that equals the like_key
-uint16_t find_in_leaf_page(const void* page, const void* like_key, const bplus_tree_tuple_defs* bpttds);
+uint16_t find_in_leaf_page(const void* page, uint32_t page_size, const void* like_key, const bplus_tree_tuple_defs* bpttds);
 
 // inserts a new_record_tuple to the leaf_page
 // if the insert fails due to the page being out of space
