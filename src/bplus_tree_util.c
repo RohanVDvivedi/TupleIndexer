762c67eb5472e468a6ae6a1a75f00513996bc05c
rohan <rohandvivedi@gmail.com>
2021-03-23 11:59:57 +0530

    -- bug fix in creating tuple definitions for bplus tree

--- src/bplus_tree_util.c
+++ src/bplus_tree_util.c
@@ -15,12 +15,12 @@ bplus_tree_tuple_defs* get_bplus_tree_tuple_defs_from_record_def(tuple_def* reco
 	bpttds->key_def->element_count = key_element_count;
 
 	bpttds->index_def = malloc(size_of_tuple_def(key_element_count + 1));
-	memcpy(bpttds->key_def, record_def, size_of_tuple_def(key_element_count));
-	bpttds->key_def->element_count = key_element_count;
+	memcpy(bpttds->index_def, record_def, size_of_tuple_def(key_element_count));
+	bpttds->index_def->element_count = key_element_count;
 	insert_element_def(bpttds->index_def, UINT, 4);
 
 	bpttds->record_def = malloc(size_of_tuple_def(record_def->element_count));
-	memcpy(bpttds->key_def, record_def, size_of_tuple_def(record_def->element_count));
+	memcpy(bpttds->record_def, record_def, size_of_tuple_def(record_def->element_count));
 	
 	return bpttds;
 }
