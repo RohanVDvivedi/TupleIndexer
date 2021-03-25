5071d16cd5df163e1c146a40fb9f9324e048f54f
rohan <rohandvivedi@gmail.com>
2021-03-25 10:31:47 +0530

    implemented test cases for search in indirections of the interior page

--- test/test_ip.c
+++ test/test_ip.c
@@ -191,6 +191,9 @@ int main()
 
 	// ---------------  SEARCHES
 
+	for(int64_t i = -5; i < 60; i++)
+		printf("%lld => %d\n", i, find_in_interior_page(page, PAGE_SIZE, &i, bpttds));
+
 	// ---------------  DESTROY TUPLE DEFINITION
 
 	del_bplus_tree_tuple_defs(bpttds);
