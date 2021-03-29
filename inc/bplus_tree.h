d198bca8fcd78e4f2e77e4d95ab4e91c5187193c
rohan <rohandvivedi@gmail.com>
2021-03-29 17:47:22 +0530

    changes to the point query api

--- inc/bplus_tree.h
+++ inc/bplus_tree.h
@@ -13,12 +13,10 @@
 
 uint32_t create_new_bplus_tree(const bplus_tree_tuple_defs* bpttds, const data_access_methods* dam_p);
 
-const void* find_in_bplus_tree(const uint32_t* root, const void* key_like, const bplus_tree_tuple_defs* bpttds, const data_access_methods* dam_p);
+const void* find_in_bplus_tree(uint32_t root, const void* key, const bplus_tree_tuple_defs* bpttds, const data_access_methods* dam_p);
 
-void insert_in_bplus_tree(const uint32_t* root, const void* record, const bplus_tree_tuple_defs* bpttds, const data_access_methods* dam_p);
+int insert_in_bplus_tree(uint32_t* root, const void* record, const bplus_tree_tuple_defs* bpttds, const data_access_methods* dam_p);
 
-int insert_if_not_exists_in_bplus_tree(const uint32_t* root, const void* record, const bplus_tree_tuple_defs* bpttds, const data_access_methods* dam_p);
-
-int delete_in_bplus_tree(const uint32_t* root, const void* key_like, const bplus_tree_tuple_defs* bpttds, const data_access_methods* dam_p);
+int delete_in_bplus_tree(uint32_t* root, const void* key, const bplus_tree_tuple_defs* bpttds, const data_access_methods* dam_p);
 
 #endif
