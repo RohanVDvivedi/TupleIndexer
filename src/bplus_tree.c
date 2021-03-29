e4202d5ac18e7f3fb33e1ce44427e39ce06c95ba
rohan <rohandvivedi@gmail.com>
2021-03-29 18:12:59 +0530

    implemented basic find functionality

--- src/bplus_tree.c
+++ src/bplus_tree.c
@@ -16,3 +16,42 @@ uint32_t create_new_bplus_tree(const bplus_tree_tuple_defs* bpttds, const data_a
 	
 	return root_page_id;
 }
+
+const void* find_in_bplus_tree(uint32_t root, const void* key, const bplus_tree_tuple_defs* bpttds, const data_access_methods* dam_p)
+{
+	void* curr_page = dam_p->acquire_page_with_reader_lock(dam_p->context, root);
+
+	while(curr_page != NULL)
+	{
+		switch(get_page_type(curr_page))
+		{
+			case LEAF_PAGE_TYPE :
+			{
+				// find record for the given key in b+tree
+
+				// release lock and cleanup
+				dam_p->release_reader_lock_on_page(dam_p->context, curr_page);
+				curr_page = NULL
+				break;
+			}
+			case INTERIOR_PAGE_TYPE :
+			{
+				// find next page_id to jump to the next level in the b+tree
+				int32_t next_indirection_index = find_in_interior_page(curr_page, dam_p->page_size, key, 0, bpttds);
+				uint32_t next_page_id = get_index_page_id_from_interior_page(curr_page, dam_p->page_size, next_indirection_index, bpttds);
+
+				// lock next page, prior to releasing the lock on the current page
+				void* next_page = dam_p->acquire_page_with_reader_lock(dam_p->context, next_page_id);
+				dam_p->release_reader_lock_on_page(dam_p->context, curr_page);
+
+				// reiterate until you reach the leaf page
+				curr_page = next_page;
+				break;
+			}
+		}
+	}
+}
+
+int insert_in_bplus_tree(uint32_t* root, const void* record, const bplus_tree_tuple_defs* bpttds, const data_access_methods* dam_p);
+
+int delete_in_bplus_tree(uint32_t* root, const void* key, const bplus_tree_tuple_defs* bpttds, const data_access_methods* dam_p);
