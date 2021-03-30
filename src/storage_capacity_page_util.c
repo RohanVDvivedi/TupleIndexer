9b6abf477ee23e181cd25aa1dd4a339af9a9189e
rohan <rohandvivedi@gmail.com>
2021-03-30 18:43:24 +0530

    added capacity estimation and efficiency related function

--- src/storage_capacity_page_util.c
+++ src/storage_capacity_page_util.c
@@ -22,6 +22,15 @@ int is_page_more_than_half_full(const void* page, uint32_t page_size, const tupl
 	return get_space_occupied_by_all_tuples(page, page_size, def) >= (get_space_allotted_to_all_tuples(page, page_size, def)/2);
 }
 
-float get_storage_capacity_used_for_page(const void* page, uint32_t page_size, const tuple_def* def);
+float get_storage_capacity_used_for_page(const void* page, uint32_t page_size, const tuple_def* def)
+{
+	return ((float)get_space_occupied_by_all_tuples(page, page_size, def)) / get_space_allotted_to_all_tuples(page, page_size, def);
+}
 
-float get_storage_efficiency_for_page(const void* page, uint32_t page_size, const tuple_def* def);
+float get_storage_efficiency_for_page(const void* page, uint32_t page_size, const tuple_def* def)
+{
+	uint32_t space_occupied_by_all_tuples = get_space_occupied_by_all_tuples(page, page_size, def);
+	uint32_t space_used_by_all_tuples = get_space_allotted_to_all_tuples(page, page_size, def) - get_free_space_in_page(page, page_size, def);
+
+	return ((float)space_occupied_by_all_tuples) / space_used_by_all_tuples;
+}
