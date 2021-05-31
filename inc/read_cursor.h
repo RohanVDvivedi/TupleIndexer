7d5daac9b80c72ee81487933d60d693a05c3e93b
rohan <rohandvivedi@gmail.com>
2021-05-31 19:45:17 +0530

    removed const qualifier since it is library managers responsibility to write good code

--- inc/read_cursor.h
+++ inc/read_cursor.h
@@ -13,7 +13,7 @@ typedef struct read_cursor read_cursor;
 struct read_cursor
 {
 	// the page that is locked for reading
-	void const * read_page;
+	void* read_page;
 
 	// definition of each record on the page
 	tuple_def const * record_def;
@@ -24,7 +24,7 @@ struct read_cursor
 
 // call open_read_cursor, only while read_page_id is read locked
 int lock_page_and_open_read_cursor(read_cursor* rc, uint32_t read_page_id, uint32_t record_id, const tuple_def* record_def, const data_access_methods* dam_p);
-int open_read_cursor(read_cursor* rc, const void* read_page, uint32_t record_id, const tuple_def* record_def);
+int open_read_cursor(read_cursor* rc, void* read_page, uint32_t record_id, const tuple_def* record_def);
 
 // seek returns 1, if the seek was success full
 // it fails with 0, if we reached the end of b+tree
