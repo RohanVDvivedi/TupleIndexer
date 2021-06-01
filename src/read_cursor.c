4e85386a4954701fd140935b4b26223954ed6dba
rohan <rohandvivedi@gmail.com>
2021-06-01 09:57:55 +0530

    using page list methods in the read_cursor

--- src/read_cursor.c
+++ src/read_cursor.c
@@ -1,6 +1,6 @@
 #include<read_cursor.h>
 
-#include<page_list_util.h>
+#include<page_list_page_util.h>
 
 #include<page_layout.h>
 
@@ -24,17 +24,17 @@ int open_read_cursor(read_cursor* rc, void* read_page, uint32_t record_id, const
 
 int seek_next_read_cursor(read_cursor* rc, const data_access_methods* dam_p)
 {
-	if(get_record_count_in_leaf_page(rc->read_page) > 0 && 
-		rc->record_id < get_record_count_in_leaf_page(rc->read_page) - 1)
+	if(get_record_count_in_list_page(rc->read_page) > 0 && 
+		rc->record_id < get_record_count_in_list_page(rc->read_page) - 1)
 	{
 		rc->record_id++;
-		if(rc->record_id < get_record_count_in_leaf_page(rc->read_page))
+		if(rc->record_id < get_record_count_in_list_page(rc->read_page))
 			return 1;
 	}
 
 	do
 	{
-		uint32_t next_sibling_page_id = get_next_sibling_leaf_page(rc->read_page);
+		uint32_t next_sibling_page_id = get_next_list_page(rc->read_page);
 		if(next_sibling_page_id != NULL_PAGE_REF)
 		{
 			void* next_sibling_page = dam_p->acquire_page_with_reader_lock(dam_p->context, next_sibling_page_id);
@@ -45,7 +45,7 @@ int seek_next_read_cursor(read_cursor* rc, const data_access_methods* dam_p)
 		else
 			return 0;	// i.e. end of leaf pages
 	}
-	while(get_record_count_in_leaf_page(rc->read_page) == 0);
+	while(get_record_count_in_list_page(rc->read_page) == 0);
 	// if pointing to empty page then re iterate
 
 	return 1;
