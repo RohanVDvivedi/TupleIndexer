4d474f467a31b744cb47aebe820fb3d956ed92ef
rohan <rohandvivedi@gmail.com>
2021-06-01 10:42:52 +0530

    making the page_refs NULL on init in the page list pages

--- src/page_list_page_util.c
+++ src/page_list_page_util.c
@@ -4,7 +4,10 @@
 
 int init_list_page(void* page, uint32_t page_size, const tuple_def* record_def)
 {
-	return init_page(page, page_size, PAGE_LIST_PAGE_TYPE, 1, record_def);
+	int inited = init_page(page, page_size, PAGE_LIST_PAGE_TYPE, 1, record_def);
+	if(inited)
+		set_reference_page_id(page, NEXT_PAGE_ID, NULL_PAGE_REF);
+	return inited;
 }
 
 uint32_t get_next_list_page(const void* page)
