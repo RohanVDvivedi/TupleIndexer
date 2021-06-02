ce5f953b0ea6fd13a5ab57039924cd3b6636d15a
rohan <rohandvivedi@gmail.com>
2021-06-02 20:44:04 +0530

    started with implementation for page_list_writable_handle

--- inc/page_list_writable_handle.h
+++ inc/page_list_writable_handle.h
@@ -1,6 +1,10 @@
 #ifndef PAGE_LIST_WRITABLE_HANDLE_H
 #define PAGE_LIST_WRITABLE_HANDLE_H
 
+#include<data_access_methods.h>
+
+#include<page_list_page_util.h>
+
 // to write to the page list you need a writable_handle
 // you effectively lock two pages of the list
 
@@ -15,10 +19,10 @@ struct page_list_writable_handle
 	int is_curr_the_first_page;
 
 	// null if this is the first page in the page_list
-	void* prev;
+	void* prev_page;
 
 	// pointer to the current locked page in the page_list
-	void* curr;
+	void* curr_page;
 };
 
 void* get_curr_page(page_list_writable_handle* plwh);
@@ -26,16 +30,16 @@ void* get_curr_page(page_list_writable_handle* plwh);
 void* get_prev_page(page_list_writable_handle* plwh);
 
 // returns 1 if the seek succeeds, else fails with 0, if you are pointing to the end of the page_list
-int seek_to_next_page(page_list_writable_handle* plwh);
+int seek_to_next_page(page_list_writable_handle* plwh, const data_access_methods* dam_p);
 
 // inserts a new page before or after the curr_page
-int insert_new_page_after_curr_page(page_list_writable_handle* plwh);
-int insert_new_page_before_curr_page(page_list_writable_handle* plwh);	// the new page becomes the curr_page
+int insert_new_page_after_curr_page(page_list_writable_handle* plwh, const data_access_methods* dam_p);
+int insert_new_page_before_curr_page(page_list_writable_handle* plwh, const data_access_methods* dam_p);	// the new page becomes the curr_page
 
 // deletes the page that the curr_page points to
-int delete_curr_page(page_list_writable_handle* plwh);
+int delete_curr_page(page_list_writable_handle* plwh, const data_access_methods* dam_p);
 
 // releases all the locks held, and closes the writable handle
-int close_page_list_writable_handle(page_list_writable_handle* plwh);
+int close_page_list_writable_handle(page_list_writable_handle* plwh, const data_access_methods* dam_p);
 
 #endif
--- src/page_list_writable_handle.c
+++ src/page_list_writable_handle.c
@@ -1,1 +1,21 @@
 #include<page_list_writable_handle.h>
+
+void* get_curr_page(page_list_writable_handle* plwh)
+{
+	return plwh->curr_page;
+}
+
+void* get_prev_page(page_list_writable_handle* plwh)
+{
+	return plwh->prev_page;
+}
+
+int seek_to_next_page(page_list_writable_handle* plwh, const data_access_methods* dam_p);
+
+int insert_new_page_after_curr_page(page_list_writable_handle* plwh, const data_access_methods* dam_p);
+
+int insert_new_page_before_curr_page(page_list_writable_handle* plwh, const data_access_methods* dam_p);
+
+int delete_curr_page(page_list_writable_handle* plwh, const data_access_methods* dam_p);
+
+int close_page_list_writable_handle(page_list_writable_handle* plwh, const data_access_methods* dam_p);
