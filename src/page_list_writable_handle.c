877d4f3f66601fd1f2099782824662330c47295c
rohan <rohandvivedi@gmail.com>
2021-06-04 11:11:20 +0530

    added necessary comments

--- src/page_list_writable_handle.c
+++ src/page_list_writable_handle.c
@@ -20,30 +20,43 @@ int seek_to_next_page(page_list_writable_handle* plwh, const data_access_methods
 	if(plwh->curr_page == NULL || get_reference_page_id(plwh->curr_page, NEXT_PAGE_REF) == NULL_PAGE_REF)
 		return 0;
 
+	// release lock on what ever is previous
 	if(plwh->is_curr_the_first_page)
 		write_unlock(&(plwh->parent_page_list->handle_lock));
 	else
 		dam_p->release_writer_lock_on_page(dam_p->context, plwh->prev_page);
 
+	// now we are not pointing to the first page
 	plwh->is_curr_the_first_page = 0;
 
+	// lock the nea page and the curr_page becomes the prev_page
 	plwh->prev_page = plwh->curr_page;
 	plwh->curr_page = dam_p->acquire_page_with_writer_lock(dam_p->context, get_reference_page_id(plwh->curr_page, NEXT_PAGE_REF));
 
 	return 1;
 }
 
-int insert_new_page_after_curr_page(page_list_writable_handle* plwh, const data_access_methods* dam_p);
+int insert_new_page_after_curr_page(page_list_writable_handle* plwh, const data_access_methods* dam_p)
+{
+	if(plwh->curr_page == NULL)
+		return 0;
+}
 
-int insert_new_page_before_curr_page(page_list_writable_handle* plwh, const data_access_methods* dam_p);
+int insert_new_page_before_curr_page(page_list_writable_handle* plwh, const data_access_methods* dam_p)
+{
+	if(plwh->curr_page == NULL)
+		return 0;
+}
 
 int delete_curr_page(page_list_writable_handle* plwh, const data_access_methods* dam_p)
 {
-
+	if(plwh->curr_page == NULL)
+		return 0;
 }
 
 int close_page_list_writable_handle(page_list_writable_handle* plwh, const data_access_methods* dam_p)
 {
+	// release previous (previous can either be the page_list itself or the prev_page)
 	if(plwh->is_curr_the_first_page)
 	{
 		write_unlock(&(plwh->parent_page_list->handle_lock));
@@ -57,6 +70,7 @@ int close_page_list_writable_handle(page_list_writable_handle* plwh, const data_
 
 	plwh->is_curr_the_first_page = 0;
 
+	// release current page
 	if(plwh->curr_page != NULL)
 		dam_p->release_writer_lock_on_page(dam_p->context, plwh->curr_page);
 	plwh->curr_page = NULL;
