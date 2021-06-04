2abbc4f67cdd61a633291060a43a1682faa0c72b
rohan <rohandvivedi@gmail.com>
2021-06-04 10:14:12 +0530

    implementation for page_list api completed

--- src/page_list.c
+++ src/page_list.c
@@ -7,12 +7,11 @@ int create_new_page_list(page_list_handle* plh)
 	return 1;
 }
 
-int init_read_cursor(page_list_handle* plh, read_cursor* rc, const data_access_methods* dam_p)
+int init_read_cursor(page_list_handle* plh, read_cursor* rc, const tuple_def* record_def, const data_access_methods* dam_p)
 {
 	if(plh->head_id == NULL_PAGE_REF)
 		return 0;
-	// open read cursor
-	return 1;
+	return lock_page_and_open_read_cursor(rc, plh->head_id, 0, record_def, dam_p);
 }
 
 int init_writable_handle(page_list_handle* plh, page_list_writable_handle* plwh, const data_access_methods* dam_p)
@@ -20,5 +19,20 @@ int init_writable_handle(page_list_handle* plh, page_list_writable_handle* plwh,
 	if(plh->head_id == NULL_PAGE_REF)
 		return 0;
 	// open writable handle
+	plwh->parent_page_list = plh;
+	write_lock(&(plh->handle_lock));
+
+	plwh->prev_page = NULL;
+	plwh->is_curr_the_first_page = 1;
+	plwh->curr_page = dam_p->acquire_page_with_writer_lock(dam_p->context, plh->head_id);
+
+	// if getting lock fails, do cleanup
+	if(plwh->curr_page == NULL)
+	{
+		write_unlock(&(plh->handle_lock));
+		plwh->is_curr_the_first_page = 0;
+		return 0;
+	}
+
 	return 1;
 }
