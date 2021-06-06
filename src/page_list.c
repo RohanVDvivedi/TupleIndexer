91639a0368574f196eeb7250aec15b4101e0b0e0
rohan <rohandvivedi@gmail.com>
2021-06-06 19:16:35 +0530

    page_list holds its own record def

--- src/page_list.c
+++ src/page_list.c
@@ -1,9 +1,14 @@
 #include<page_list.h>
 
-int create_new_page_list(page_list_handle* plh, uint32_t head_id)
+#include<stdlib.h>
+#include<string.h>
+
+int create_new_page_list(page_list_handle* plh, tuple_def* record_def, uint32_t head_id)
 {
 	initialize_rwlock(&(plh->handle_lock));
 	plh->head_id = head_id;
+	plh->record_def = malloc(size_of_tuple_def(record_def->element_count));
+	memcpy(plh->record_def, record_def, size_of_tuple_def(record_def->element_count));
 	return 1;
 }
 
@@ -14,11 +19,11 @@ int deinit_page_list(page_list_handle* plh)
 	return 1;
 }
 
-int init_read_cursor(page_list_handle* plh, read_cursor* rc, const tuple_def* record_def, const data_access_methods* dam_p)
+int init_read_cursor(page_list_handle* plh, read_cursor* rc, const data_access_methods* dam_p)
 {
 	if(plh->head_id == NULL_PAGE_REF)
 		return 0;
-	return lock_page_and_open_read_cursor(rc, plh->head_id, 0, record_def, dam_p);
+	return lock_page_and_open_read_cursor(rc, plh->head_id, 0, plh->record_def, dam_p);
 }
 
 int init_writable_handle(page_list_handle* plh, page_list_writable_handle* plwh, const data_access_methods* dam_p)
