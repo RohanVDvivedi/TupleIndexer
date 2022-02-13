e070a29c16b253c7d6e2b4271642bd46102b4945
rohan <rohandvivedi@gmail.com>
2022-02-12 19:46:18 -0700

    changes to sorted packed page as required

--- src/sorted_packed_page_util.c
+++ src/sorted_packed_page_util.c
@@ -3,133 +3,41 @@
 #include<tuple.h>
 #include<page_layout.h>
 
-int search_in_sorted_packed_page(
-									const void* page, uint32_t page_size, 
-									const tuple_def* key_def, const tuple_def* key_val_def, 
-									const void* key, 
-									uint16_t* index
-								)
-{
-	uint16_t count = get_tuple_count(page);
-	if(count == 0)
-		return 0;
-
-	uint16_t low = 0;
-	uint16_t high = count - 1;
-
-	uint16_t mid = (low + high) / 2;
-	if(index != NULL)
-		(*index) = mid;
-
-	const void* tup_mid;
-	int compare;
-
-	while(low < high)
-	{
-		mid = (low + high) / 2;
-		if(index != NULL)
-			(*index) = mid;
-
-		tup_mid = get_nth_tuple(page, page_size, key_val_def, mid);
-		compare = compare_tuples(tup_mid, key, key_def);
-
-		if(compare > 0)
-		{
-			if(mid == 0)
-				break;
-			high = mid - 1;
-		}
-		else if(compare < 0)
-		{
-			if(mid == 65535)
-				break;
-			low = mid + 1;
-		}
-		else
-			return 1;
-	}
-
-	mid = (low + high) / 2;
-	if(index != NULL)
-		(*index) = mid;
-
-	tup_mid = get_nth_tuple(page, page_size, key_val_def, mid);
-	compare = compare_tuples(tup_mid, key, key_def);
-
-	if(compare == 0)
-		return 1;
-
-	return 0;
-}
-
 int insert_to_sorted_packed_page(
 									void* page, uint32_t page_size, 
-									const tuple_def* key_def, const tuple_def* key_val_def, 
-									const void* key_val, 
-									uint16_t* index
+									const tuple_def* tpl_def, uint32_t* keys_to_compare, uint32_t keys_count,
+									const void* tuple, 
+									uint32_t* index
 								)
 {
-	// handle edge case when number of elements in the page are 0
-	if(get_tuple_count(page) == 0)
+	// case when number of elements in the page are 0
+	if(get_tuple_count(page, page_size, tpl_def) == 0)
 	{
 		if(index != NULL)
 			(*index) = 0;
-		return insert_tuple(page, page_size, key_val_def, key_val);
+		return insert_tuple(page, page_size, tpl_def, tuple);
 	}
 
-	// search for a viable index that is closer to that one, which we are planning to insert
-	uint16_t index_searched = 0;
-	search_in_sorted_packed_page(page, page_size, key_def, key_val_def, key_val, &index_searched);
+	// search for a viable index for the new tuple to insert
+	uint32_t new_index = NOT_FOUND;
 
-	const void* tup = get_nth_tuple(page, page_size, key_val_def, index_searched);
-	int compare = compare_tuples(tup, key_val, key_def);
-	if(compare > 0)
-	{
-		while(compare > 0)
-		{
-			if(index_searched == 0)
-				break;
-
-			index_searched--;
-
-			tup = get_nth_tuple(page, page_size, key_val_def, index_searched);
-			compare = compare_tuples(tup, key_val, key_def);
-		}
-		if(compare <= 0)
-			index_searched++;
-	}
-	else
-	{
-		uint16_t tuple_count = get_tuple_count(page);
-		while(compare <= 0)
-		{
-			index_searched++;
-
-			if(index_searched == tuple_count)
-				break;
-
-			tup = get_nth_tuple(page, page_size, key_val_def, index_searched);
-			compare = compare_tuples(tup, key_val, key_def);
-		}
-	}
+	// find the new_index using binary search
+	// TODO
 
 	// this is the final index for the newly inserted element
-	uint16_t new_index = index_searched;
-
-	// set the return value
 	if(index != NULL)
 		(*index) = new_index;
 
 	// insert tuple to the end of the page
-	int inserted = insert_tuple(page, page_size, key_val_def, key_val);
-	if(!inserted)
+	if(!insert_tuple(page, page_size, tpl_def, tuple))
 		return 0;
 
-	uint16_t tuple_count = get_tuple_count(page);
-	uint16_t temp_index = tuple_count - 1;	// current temporary index of the inserted tuple
+	// swap until the new tuple is not at new_index
+	uint32_t tuple_count = get_tuple_count(page, page_size, tpl_def);
+	uint32_t temp_index = tuple_count - 1;
 	while(new_index < temp_index)
 	{
-		swap_tuples(page, page_size, key_val_def, temp_index - 1, temp_index);
+		swap_tuples(page, page_size, tpl_def, temp_index - 1, temp_index);
 		temp_index--;
 	}
 
@@ -139,10 +47,10 @@ int insert_to_sorted_packed_page(
 int delete_in_sorted_packed_page(
 									void* page, uint32_t page_size, 
 									const tuple_def* tpl_def,
-									uint16_t index
+									uint32_t index
 								)
 {
-	if(!delete_tuple(page, page_size, key_val_def, index))
+	if(!delete_tuple(page, page_size, tpl_def, index))
 		return 0;
 
 	run_page_compaction(page, page_size, tpl_def, 1, 0);
@@ -152,60 +60,93 @@ int delete_in_sorted_packed_page(
 
 int delete_all_in_sorted_packed_page(
 									void* page, uint32_t page_size, 
-									const tuple_def* key_val_def, 
-									uint16_t start_index, uint16_t end_index
+									const tuple_def* tpl_def, 
+									uint32_t start_index, uint32_t end_index
 								)
 {
-	uint16_t count = get_tuple_count(page);
+	uint32_t count = get_tuple_count(page, page_size, tpl_def);
 	if(count == 0 || start_index > end_index || end_index >= count)
 		return 0;
 
-	for(uint16_t i = start_index; i <= end_index; i++)
-		delete_tuple(page, page_size, key_val_def, i);
+	for(uint32_t i = start_index; i <= end_index; i++)
+		delete_tuple(page, page_size, tpl_def, i);
 
-	// move tombstones to the end
-	for(uint16_t i = start_index, j = end_index + 1; j < count; i++, j++)
-		swap_tuples(page, page_size, key_val_def, i, j);
+	run_page_compaction(page, page_size, tpl_def, 1, 0);
 
 	return 1;
 }
 
-uint16_t insert_all_from_sorted_packed_page(
+uint32_t insert_all_from_sorted_packed_page(
 									void* page_dest, const void* page_src, uint32_t page_size, 
-									const tuple_def* key_def, const tuple_def* key_val_def, 
-									uint16_t start_index, uint16_t end_index
+									const tuple_def* tpl_def, uint32_t* keys_to_compare, uint32_t keys_count,
+									uint32_t start_index, uint32_t end_index
 								)
 {
-	uint16_t count = get_tuple_count(page_src);
-	if(count == 0 || start_index > end_index || end_index >= count)
+	uint32_t src_count = get_tuple_count(page_src, page_size, tpl_def);
+	if(src_count == 0 || start_index > end_index || end_index >= src_count)
 		return 0;
 
-	// if the dest page does not have cany tuples then we can perform a direct insert and that will be in order
-	// i.e. no comparisons needed
-	if(get_tuple_count(page_dest) == 0)
-		return insert_tuples_from_page(page_dest, page_size, key_val_def, page_src, start_index, end_index);
+	// if the dest page is empty, insert all no comparisons needed
+	uint32_t dest_count = get_tuple_count(page_dest, page_size, tpl_def);
+	if(dest_count == 0)
+		return insert_tuples_from_page(page_dest, page_size, tpl_def, page_src, start_index, end_index);
 
 	// compare the last tuple of the dest page and first tuple of the src page
-	const void* last_tuple_dest = get_nth_tuple(page_dest, page_size, key_val_def, get_tuple_count(page_dest) - 1);
-	const void* first_tuple_src = get_nth_tuple(page_src, page_size, key_val_def, 0);
+	const void* last_tuple_dest = get_nth_tuple(page_dest, page_size, tpl_def, dest_count - 1);
+	const void* first_tuple_src = get_nth_tuple(page_src, page_size, tpl_def, 0);
 
 	// if they are in order then perform a direct copy
-	int compare_last_first = compare_tuples(last_tuple_dest, first_tuple_src, key_def);
+	int compare_last_first = compare_tuples(last_tuple_dest, first_tuple_src, tpl_def, keys_count, keys_to_compare);
 	if(compare_last_first <= 0)
-		return insert_tuples_from_page(page_dest, page_size, key_val_def, page_src, start_index, end_index);
+		return insert_tuples_from_page(page_dest, page_size, tpl_def, page_src, start_index, end_index);
 
-	uint16_t inserted_count = 0;
+	uint32_t inserted_count = 0;
 
 	// insert using stupstom api from start_index to end_index
-	for(uint16_t index = start_index; index <= end_index; index++)
+	for(uint32_t index = start_index; index <= end_index; index++)
 	{
-		const void* tup = get_nth_tuple(page_src, page_size, key_val_def, index);
-		int inserted = insert_to_sorted_packed_page(page_dest, page_size, key_def, key_val_def, tup, NULL);
-		if(inserted)
+		const void* tup = get_nth_tuple(page_src, page_size, tpl_def, index);
+		if(insert_to_sorted_packed_page(page_dest, page_size, tpl_def, keys_to_compare, keys_count, tup, NULL))
 			inserted_count++;
 		else
 			break;
 	}
 
 	return inserted_count;
+}
+
+uint32_t search_in_sorted_packed_page(
+									const void* page, uint32_t page_size, 
+									const tuple_def* tpl_def, uint32_t* keys_to_compare, uint32_t keys_count,
+									const void* tuple
+								)
+{
+	uint32_t count = get_tuple_count(page, page_size, tpl_def);
+	if(count == 0)
+		return 0;
+
+	// if the provided tuple is lesser than the first tuple
+	const void* tup_first = get_nth_tuple(page, page_size, tpl_def, 0);
+	if(compare_tuples(tup_first, tuple, tpl_def, keys_count, keys_to_compare) > 0)
+		return NOT_FOUND;
+
+	uint32_t low = 0;
+	uint32_t high = count - 1;
+
+	while(low <= high)
+	{
+		uint32_t mid = low + (high - low) / 2;
+
+		const void* tup_mid = get_nth_tuple(page, page_size, tpl_def, mid);
+		int compare = compare_tuples(tup_mid, tuple, tpl_def, keys_count, keys_to_compare);
+
+		if(compare > 0)
+			high = mid - 1;
+		else if(compare < 0)
+			low = mid + 1;
+		else
+			return mid;
+	}
+
+	return NOT_FOUND;
 }
