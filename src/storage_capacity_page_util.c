f24cbe21bdf26cf7cc0c304faf3b35883daab8e4
rohan <rohandvivedi@gmail.com>
2022-02-14 03:31:18 -0700

    implemented may split function

--- src/storage_capacity_page_util.c
+++ src/storage_capacity_page_util.c
@@ -29,3 +29,16 @@ int is_page_more_than_half_full(const void* page, uint32_t page_size, const tupl
 	uint32_t used_space = get_space_occupied_by_all_tuples(page, page_size, def);
 	return used_space > (allotted_space / 2);
 }
+
+int may_split_on_insert_for_bplus_tree(const void* page, uint32_t page_size, const tuple_def* def)
+{
+	uint32_t allotted_space = get_space_allotted_to_all_tuples(page, page_size, def);
+	uint32_t used_space = get_space_occupied_by_all_tuples(page, page_size, def);
+	
+	// page will be surely split if the unused space is lesser than incoming tuple size for fixed sized tuple
+	if(is_fixed_sized_tuple_def(def))
+		return (allotted_space - used_space) < def->size;
+	// for a variable sized tuple the page must be more than half full to may require spliting
+	else
+		return used_space > (allotted_space / 2);
+}
