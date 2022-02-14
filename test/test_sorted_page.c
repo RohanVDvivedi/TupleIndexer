334c644ccbe15a47fa181d22bc794cb22d42ca4a
rohan <rohandvivedi@gmail.com>
2022-02-13 17:35:28 -0700

    test cases passed

--- test/test_sorted_page.c
+++ test/test_sorted_page.c
@@ -386,13 +386,12 @@ int main()
 			printf("PRECEDING EQUALS = %u\n", index);
 
 		printf("\n");
-/*
+
 		index = find_succeeding_equals_in_sorted_packed_page(page, PAGE_SIZE, def, NULL, 1, &i);
 		if(index == NOT_FOUND)
 			printf("SUCCEEDING EQUALS = %s\n", "NOT_FOUND");
 		else
 			printf("SUCCEEDING EQUALS = %u\n", index);
-*/
 
 		index = find_succeeding_in_sorted_packed_page(page, PAGE_SIZE, def, NULL, 1, &i);
 		if(index == NOT_FOUND)
