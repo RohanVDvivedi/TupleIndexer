da3f998da06d8d811965b70584ceb38fc40c0428
rohan <rohandvivedi@gmail.com>
2022-02-14 03:31:18 -0700

    implemented may split function

--- inc/storage_capacity_page_util.h
+++ inc/storage_capacity_page_util.h
@@ -11,4 +11,10 @@ int is_page_more_than_or_equal_to_half_full(const void* page, uint32_t page_size
 
 int is_page_more_than_half_full(const void* page, uint32_t page_size, const tuple_def* def);
 
+// method only valid for bplus tree and variants
+// returns 1 -> MAY require split
+// returns 0 -> will SURELY not require any split
+// it may give false positive for splitting
+int may_split_on_insert_for_bplus_tree(const void* page, uint32_t page_size, const tuple_def* def);
+
 #endif
