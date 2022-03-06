eec976a02e8f4e25b20952c959461a51dfd55b9a
rohan <rohandvivedi@gmail.com>
2022-03-05 21:53:54 -0700

    added basic functions for leaf page header

--- inc/bplus_tree_leaf_page.h
+++ inc/bplus_tree_leaf_page.h
@@ -1,6 +1,9 @@
 #ifndef BPLUS_TREE_LEAF_PAGE_H
 #define BPLUS_TREE_LEAF_PAGE_H
 
+#include<common_page_header.h>
+#include<bplus_tree_page.h>
+
 typedef struct bplus_tree_leaf_page_header bplus_tree_leaf_page_header;
 struct bplus_tree_leaf_page_header
 {
@@ -8,10 +11,10 @@ struct bplus_tree_leaf_page_header
 	uint32_t next_page;	// towards all greater data
 };
 
-#define sizeof_LEAF_PAGE_HEADER
+#define sizeof_LEAF_PAGE_HEADER (sizeof(page_header) + sizeof(bplustree_page_header) + sizeof(bplus_tree_leaf_page_header))
 
-uint32_t get_next_of_bplus_tree_leaf_page(void* page);
+uint32_t get_next_of_bplus_tree_leaf_page(const void* page, uint32_t page_size);
 
-uint32_t get_prev_of_bplus_tree_leaf_page(void* page);
+uint32_t get_prev_of_bplus_tree_leaf_page(const void* page, uint32_t page_size);
 
 #endif
