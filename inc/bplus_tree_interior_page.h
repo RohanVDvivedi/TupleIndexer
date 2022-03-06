4e4202c3c5d5d96949ac621d95f87b96675d3685
rohan <rohandvivedi@gmail.com>
2022-03-05 22:00:20 -0700

    implemented getter functions for all page headers

--- inc/bplus_tree_interior_page.h
+++ inc/bplus_tree_interior_page.h
@@ -1,14 +1,19 @@
 #ifndef BPLUS_TREE_INTERIOR_PAGE_H
 #define BPLUS_TREE_INTERIOR_PAGE_H
 
+#include<stdint.h>
+
+#include<common_page_header.h>
+#include<bplus_tree_page.h>
+
 typedef struct bplus_tree_interior_page_header bplus_tree_interior_page_header;
 struct bplus_tree_interior_page_header
 {
 	uint32_t least_keys_page;	// link to child page having keys lesser than the least key on this page
 };
 
-#define sizeof_INTERIOR_PAGE_HEADER
+#define sizeof_INTERIOR_PAGE_HEADER (sizeof(page_header) + sizeof(bplus_tree_page_header) + sizeof(bplus_tree_interior_page_header))
 
-uint32_t get_least_keys_page_id_of_bplus_tree_interior_page(void* page);
+uint32_t get_least_keys_page_id_of_bplus_tree_interior_page(const void* page, uint32_t page_size);
 
 #endif
--- src/bplus_tree_leaf_page.c
+++ src/bplus_tree_leaf_page.c
@@ -2,13 +2,13 @@
 
 #include<page_layout.h>
 
-uint32_t get_next_of_bplus_tree_leaf_page(const void* page, uint32_t page_size)
+uint32_t get_next_page_id_of_bplus_tree_leaf_page(const void* page, uint32_t page_size)
 {
 	const bplus_tree_leaf_page_header* ph = get_page_header((void*)page, page_size) + sizeof(page_header) + sizeof(bplus_tree_page_header);
 	return ph->next_page;
 }
 
-uint32_t get_prev_of_bplus_tree_leaf_page(const void* page, uint32_t page_size)
+uint32_t get_prev_page_id_of_bplus_tree_leaf_page(const void* page, uint32_t page_size)
 {
 	const bplus_tree_leaf_page_header* ph = get_page_header((void*)page, page_size) + sizeof(page_header) + sizeof(bplus_tree_page_header);
 	return ph->prev_page;
