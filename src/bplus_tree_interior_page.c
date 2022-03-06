0e63ceeeaca49ce36dbda6a26fa1ae81b7a3abe9
rohan <rohandvivedi@gmail.com>
2022-03-05 22:00:49 -0700

    implemented getter functions for all page headers

---
+++ src/bplus_tree_interior_page.c
@@ -0,0 +1,9 @@
+#include<bplus_tree_interior_page.h>
+
+#include<page_layout.h>
+
+uint32_t get_least_keys_page_id_of_bplus_tree_interior_page(const void* page, uint32_t page_size)
+{
+	const bplus_tree_interior_page_header* ph = get_page_header((void*)page, page_size) + sizeof(page_header) + sizeof(bplus_tree_page_header);
+	return ph->least_keys_page;
+}
