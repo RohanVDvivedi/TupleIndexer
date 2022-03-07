275a2efbc8f4796dbab8e6d7047fc2cbce2e0dec
rohan <rohandvivedi@gmail.com>
2022-03-07 15:31:16 -0700

    added get tuple definition function to the project copied from old project version

---
+++ inc/bplus_tree_tuple_definitions.h
@@ -0,0 +1,25 @@
+#ifndef BPLUS_TREE_TUPLE_DEFINITIONS_H
+#define BPLUS_TREE_TUPLE_DEFINITIONS_H
+
+#include<tuple_def.h>
+
+typedef struct bplus_tree_tuple_defs bplus_tree_tuple_defs;
+struct bplus_tree_tuple_defs
+{
+	// tuple definition to compare keys in the bplus_tree
+	tuple_def* key_def;
+
+	// tuple definition of the interior pages in the bplus_tree
+	// this is equal to { key_def : page_id(u4)) }
+	tuple_def* index_def;
+
+	// tuple definition of the leaf pages in the bplus_tree
+	// this is equal to { key_def : value_def }
+	tuple_def* record_def;
+};
+
+void init_bplus_tree_tuple_definitions(bplus_tree_tuple_defs* bpttd_p, const tuple_def* record_def, uint32_t key_element_count);
+
+void deinit_bplus_tree_tuple_definitions(bplus_tree_tuple_defs* bpttd_p);
+
+#endif
