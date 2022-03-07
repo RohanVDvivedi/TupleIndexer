a0cafbce883c933110725c9073746d36bc5f15f7
rohan <rohandvivedi@gmail.com>
2022-03-07 15:51:46 -0700

    implemented may require merge and redistribution functions

--- inc/storage_capacity_page_util.h
+++ inc/storage_capacity_page_util.h
@@ -15,6 +15,12 @@ int is_page_more_than_half_full(const void* page, uint32_t page_size, const tupl
 // returns 1 -> MAY require split
 // returns 0 -> will SURELY not require any split
 // it may give false positive for splitting
-int may_split_on_insert_for_bplus_tree(const void* page, uint32_t page_size, const tuple_def* def);
+int may_require_split_for_insert_for_bplus_tree(const void* page, uint32_t page_size, const tuple_def* def);
+
+// method only valid for bplus tree and variants
+// returns 1 -> MAY require merge or redistribution
+// returns 0 -> will SURELY not require any merge or redistribution
+// it may give false positive for merging or redistribution
+int may_require_merge_or_redistribution_for_delete_for_bplus_tree(const void* page, uint32_t page_size, const tuple_def* def, uint32_t deletion_index);
 
 #endif
--- src/storage_capacity_page_util.c
+++ src/storage_capacity_page_util.c
@@ -30,7 +30,7 @@ int is_page_more_than_half_full(const void* page, uint32_t page_size, const tupl
 	return used_space > (allotted_space / 2);
 }
 
-int may_split_on_insert_for_bplus_tree(const void* page, uint32_t page_size, const tuple_def* def)
+int may_require_split_for_insert_for_bplus_tree(const void* page, uint32_t page_size, const tuple_def* def)
 {
 	uint32_t allotted_space = get_space_allotted_to_all_tuples(page, page_size, def);
 	uint32_t used_space = get_space_occupied_by_all_tuples(page, page_size, def);
@@ -41,4 +41,23 @@ int may_split_on_insert_for_bplus_tree(const void* page, uint32_t page_size, con
 	// for a variable sized tuple the page must be more than half full to may require spliting
 	else
 		return used_space > (allotted_space / 2);
+}
+
+int may_require_merge_or_redistribution_for_delete_for_bplus_tree(const void* page, uint32_t page_size, const tuple_def* def, uint32_t deletion_index)
+{
+	uint32_t allotted_space = get_space_allotted_to_all_tuples(page, page_size, def);
+	uint32_t used_space = get_space_occupied_by_all_tuples(page, page_size, def);
+
+	// if the page is already lesser than half full, then ti may require merging or redistribution
+	if(used_space < (allotted_space / 2))
+		return 1;
+
+	uint32_t new_used_space = used_space;
+
+	// find new used space after deleting the tuple at the current index (and after full compaction and tomb stone removal)
+	if(deletion_index < get_tuple_count(page, page_size, def))
+		new_used_space -= get_space_occupied_by_tuples(page, page_size, def, deletion_index, deletion_index);
+
+	// if the new used space is lesser than half full then it may required merging/redistribution
+	return new_used_space < (allotted_space / 2);
 }
