b7ea31cab779f2c3df3fa481a078c872b0c085aa
rohan <rohandvivedi@gmail.com>
2022-04-05 20:00:40 -0700

    added appropriate conditions to update function

--- src/sorted_packed_page_util.c
+++ src/sorted_packed_page_util.c
@@ -73,7 +73,7 @@ int insert_at_in_sorted_packed_page(
 	// in the below conditions we ensure that the tuple order is maintained
 
 	// the tuple compares greater than the tuple at index, we fail
-	if(index < tuple_count && tuple_count > 0)
+	if(tuple_count > 0 && index < tuple_count)
 	{
 		const void* ith_tuple = get_nth_tuple(page, page_size, tpl_def, index);
 		if( compare_tuples(tuple, tpl_def, keys_to_compare, ith_tuple, tpl_def, keys_to_compare, keys_count) > 0)
@@ -81,7 +81,7 @@ int insert_at_in_sorted_packed_page(
 	}
 
 	// the tuple compares lesser than the tuple at (index - 1), we fail
-	if(index > 0 && tuple_count > 0)
+	if(tuple_count > 0 && index > 0)
 	{
 		const void* i_1_th_tuple = get_nth_tuple(page, page_size, tpl_def, index - 1);
 		if( compare_tuples(tuple, tpl_def, keys_to_compare, i_1_th_tuple, tpl_def, keys_to_compare, keys_count) < 0)
@@ -90,6 +90,42 @@ int insert_at_in_sorted_packed_page(
 
 	// insert tuple to the page at the desired index
 	return insert_at_in_page(page, page_size, tpl_def, tuple, index);
+}
+
+int update_resiliently_at_in_sorted_packed_page(
+									void* page, uint32_t page_size, 
+									const tuple_def* tpl_def, uint32_t* keys_to_compare, uint32_t keys_count,
+									const void* tuple, 
+									uint32_t index
+								)
+{
+	uint32_t tuple_count = get_tuple_count(page, page_size, tpl_def);
+
+	// if the index is not valid we fail the update
+	if( !(0 <= index && index < tuple_count) )
+		return 0;
+
+	// in the below conditions we ensure that the tuple order is maintained
+
+	// the tuple compares greater than the tuple at (index + 1), we fail
+	if(tuple_count > 0 && index != tuple_count - 1)
+	{
+		const void* i_1_th_tuple = get_nth_tuple(page, page_size, tpl_def, index + 1);
+		if( compare_tuples(tuple, tpl_def, keys_to_compare, i_1_th_tuple, tpl_def, keys_to_compare, keys_count) > 0)
+			return 0;
+	}
+
+	// the tuple compares lesser than the tuple at (index - 1), we fail
+	if(tuple_count > 0 && index > 0)
+	{
+		const void* i_1_th_tuple = get_nth_tuple(page, page_size, tpl_def, index - 1);
+		if( compare_tuples(tuple, tpl_def, keys_to_compare, i_1_th_tuple, tpl_def, keys_to_compare, keys_count) < 0)
+			return 0;
+	}
+
+	int update_successfull = 1;
+
+	return update_successfull;
 }
 
 int delete_in_sorted_packed_page(
