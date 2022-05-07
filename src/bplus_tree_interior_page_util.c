147e51fe1fd2dd26b3c5ba41b0d47855c3bf6fbf
RohanVDvivedi <rohandvivedi@gmail.com>
2022-05-07 02:28:03 -0700

    added page_util source files

---
+++ src/bplus_tree_interior_page_util.c
@@ -0,0 +1,11 @@
+#include<bplus_tree_interior_page_util.h>
+
+uint32_t find_child_index_for_key(const void* page, const void* key, const bplus_tree_tuple_defs* bpttd_p)
+{
+
+}
+
+uint64_t find_child_page_id_by_child_index(const void* page, uint32_t index, const bplus_tree_tuple_defs* bpttd_p)
+{
+
+}
---
+++ src/bplus_tree_leaf_page_util.c
@@ -0,0 +1,11 @@
+#include<bplus_tree_leaf_page_util.h>
+
+uint32_t find_greater_equals_for_key(const void* page, const void* key, const bplus_tree_tuple_defs* bpttd_p)
+{
+
+}
+
+uint32_t find_lesser_equals_for_key(const void* page, const void* key, const bplus_tree_tuple_defs* bpttd_p)
+{
+
+}
