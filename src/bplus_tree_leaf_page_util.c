8504b2814531245b01a6cc68fe5ef10fb03ac66b
RohanVDvivedi <rohandvivedi@gmail.com>
2022-05-07 03:04:59 -0700

    implemented find functions for leaf page util source file

--- src/bplus_tree_leaf_page_util.c
+++ src/bplus_tree_leaf_page_util.c
@@ -1,11 +1,21 @@
 #include<bplus_tree_leaf_page_util.h>
 
+#include<sorted_packed_page_util.h>
+
 uint32_t find_greater_equals_for_key(const void* page, const void* key, const bplus_tree_tuple_defs* bpttd_p)
 {
-
+	return find_succeeding_equals_in_sorted_packed_page(
+									page, bpttd_p->page_size, 
+									bpttd_p->record_def, bpttd_p->key_element_ids, bpttd_p->key_element_count,
+									key, bpttd_p->key_def, NULL
+								);
 }
 
 uint32_t find_lesser_equals_for_key(const void* page, const void* key, const bplus_tree_tuple_defs* bpttd_p)
 {
-
+	return find_preceding_equals_in_sorted_packed_page(
+									page, bpttd_p->page_size, 
+									bpttd_p->record_def, bpttd_p->key_element_ids, bpttd_p->key_element_count,
+									key, bpttd_p->key_def, NULL
+								);
 }
