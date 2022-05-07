8a2b5d85d5eb71e87b571fb813dfbd632a10d569
RohanVDvivedi <rohandvivedi@gmail.com>
2022-05-06 22:56:43 -0700

    added declarations for find methods for page util header files

--- inc/bplus_tree_interior_page_util.h
+++ inc/bplus_tree_interior_page_util.h
@@ -1,4 +1,17 @@
 #ifndef BPLUS_TREE_INTERIOR_PAGE_UTIL_H
 #define BPLUS_TREE_INTERIOR_PAGE_UTIL_H
 
+/*
+*	child index start from (-1) and end at (tuple_count - 1)
+*	child page_id at index = (-1) => is same as the "least_keys_page_id" as given in the bplus_tree_interior_page_header
+*	all other child page_id are stored as the last attributes of the corresponding tuples
+*/
+
+// this the index of the tuple in the interior page that you should follow
+// you may cache this, it may help in case of a split
+uint32_t find_child_index_for_key(const void* page, const void* key, const bplus_tree_tuple_defs* bpttd_p);
+
+// returns the page_id stored with the corresponding tuple at index, in its attribute "child_page_id" 
+uint64_t find_child_page_id_by_child_index(const void* page, uint32_t index, const bplus_tree_tuple_defs* bpttd_p);
+
 #endif
--- inc/bplus_tree_leaf_page_util.h
+++ inc/bplus_tree_leaf_page_util.h
@@ -1,4 +1,10 @@
 #ifndef BPLUS_TREE_LEAF_PAGE_UTIL_H
 #define BPLUS_TREE_LEAF_PAGE_UTIL_H
 
+// when you want tuples in descending order from the key
+uint32_t find_greater_equals_for_key(const void* page, const void* key, const bplus_tree_tuple_defs* bpttd_p);
+
+// when you want tuples in ascending order from the key
+uint32_t find_lesser_equals_for_key(const void* page, const void* key, const bplus_tree_tuple_defs* bpttd_p);
+
 #endif
