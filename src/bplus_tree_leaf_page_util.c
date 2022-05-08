3a53ced3eb0a964cc597c2266892ea045fd77eb8
RohanVDvivedi <rohandvivedi@gmail.com>
2022-05-07 22:37:18 -0700

    started with implementing the space fullfillment logic for the leaf page split

--- src/bplus_tree_leaf_page_util.c
+++ src/bplus_tree_leaf_page_util.c
@@ -27,13 +27,68 @@ uint32_t find_lesser_equals_for_key_bplus_tree_leaf_page(const void* page, const
 }
 
 // this will the tuples that will remain in the page_info after after the complete split operation
-static uint32_t calculate_final_tuple_count_of_page_to_be_split(locked_page_info* page_info, const void* tuple_to_insert, const bplus_tree_tuple_defs* bpttds)
+static uint32_t calculate_final_tuple_count_of_page_to_be_split(locked_page_info* page_info, const void* tuple_to_insert, uint32_t tuple_to_insert_at, const bplus_tree_tuple_defs* bpttds)
 {
+	uint32_t tuple_count = get_tuple_count(page_info->page, bpttds->page_size, bpttds->record_def);
+
 	if(is_fixed_sized_tuple_def(bpttds->record_def))
 	{
+		// if it is the last leaf page
+		if(get_next_page_id_of_bplus_tree_leaf_page(page_info->page, bpttds) == 0)
+			return tuple_count;	// i.e. only 1 tuple goes to the new page
+		else // else
+			return (tuple_count + 1) / 2;	// equal split
 	}
 	else
 	{
+		uint32_t total_tuple_count = tuple_count + 1;
+		uint32_t* cumulative_tuple_sizes = malloc(sizeof(uint32_t) * (total_tuple_count + 1));
+
+		// space required in addition to its size on the page
+		// this is assumed to be fixed
+		const void* first_tuple = get_nth_tuple(page_info->page, bpttds->page_size, bpttds->record_def, 0);
+		uint32_t per_tuple_over_head = get_space_occupied_by_tuples(page_info->page, bpttds->page_size, bpttds->record_def, 0, 0) - get_tuple_size(bpttds->record_def, first_tuple);
+
+		cumulative_tuple_sizes[0] = 0;
+		for(uint32_t i = 0, k = 1; i < tuple_count; i++)
+		{
+			// if the new tuple is suppossed to be inserted at i then process it first
+			if(i == tuple_to_insert_at)
+			{
+				uint32_t space_occupied_by_new_tuple = get_tuple_size(bpttds->record_def, tuple_to_insert) + per_tuple_over_head;
+				cumulative_tuple_sizes[k] = space_occupied_by_new_tuple + cumulative_tuple_sizes[k-1];
+				k++;
+			}
+
+			// process the ith tuple
+			uint32_t space_occupied_by_ith_tuple = get_space_occupied_by_tuples(page_info->page, bpttds->page_size, bpttds->record_def, i, i);
+			cumulative_tuple_sizes[k] = space_occupied_by_ith_tuple + cumulative_tuple_sizes[k-1];
+			k++;
+		}
+
+		// now we have the cumulative space requirement of all the tupl
+		// i.e. the first n tuples will occupy cumulative_tuple_sizes[n] amount of space
+
+		// this is the total space available to you to store the tuples
+		uint32_t space_allotted_to_tuples = get_space_allotted_to_all_tuples(page_info->page, bpttds->page_size, bpttds->record_def);
+
+		// this is the result number of tuple that should stay on this page
+		uint32_t result;
+
+		// if it is the last leaf page
+		if(get_next_page_id_of_bplus_tree_leaf_page(page_info->page, bpttds) == 0)
+		{
+
+		}
+		else // else
+		{
+
+		}
+
+		// free cumulative tuple sizes
+		free(cumulative_tuple_sizes);
+
+		return result;
 	}
 }
 
@@ -60,7 +115,7 @@ const void* split_insert_bplus_tree_leaf_page(locked_page_info* page_info, const
 	// lingo for variables page1 => page to be split, page2 => page that will be allocated to handle the split
 
 	// final tuple count of the page that will be split
-	uint32_t final_tuple_count_page1 = calculate_final_tuple_count_of_page_to_be_split(page_info, tuple_to_insert, bpttds);
+	uint32_t final_tuple_count_page1 = calculate_final_tuple_count_of_page_to_be_split(page_info, tuple_to_insert, tuple_to_insert_at, bpttds);
 
 	// final tuple count of the page that will be newly allocated
 	//uint32_t final_tuple_count_page2 = total_tuple_count - final_tuple_count_page1;
