d78fbcfc30c0475c5b0aba214e1915b4813c98bf
RohanVDvivedi <rohandvivedi@gmail.com>
2022-05-07 19:35:35 -0700

    started with implementation of split and merge for leaf pages

--- inc/bplus_tree_leaf_page_util.h
+++ inc/bplus_tree_leaf_page_util.h
@@ -6,9 +6,24 @@
 #include<bplus_tree_interior_page_header.h>
 
 // when you want tuples in descending order from the key
-uint32_t find_greater_equals_for_key(const void* page, const void* key, const bplus_tree_tuple_defs* bpttd_p);
+uint32_t find_greater_equals_for_key_bplus_tree_leaf_page(const void* page, const void* key, const bplus_tree_tuple_defs* bpttd_p);
 
 // when you want tuples in ascending order from the key
-uint32_t find_lesser_equals_for_key(const void* page, const void* key, const bplus_tree_tuple_defs* bpttd_p);
+uint32_t find_lesser_equals_for_key_bplus_tree_leaf_page(const void* page, const void* key, const bplus_tree_tuple_defs* bpttd_p);
+
+// it performs a split insert to the leaf page provided
+// and returns the tuple that needs to be inserted to the parent page
+// it returns NULL, on failure if the tuple was not inserted, and the split was not performed
+// This function MUST be called only if the direct insert to this page fails
+// the failure results from following reason:
+// failure to allocate a new page OR failure to get reference to the next page of the page_info
+const void* split_insert_bplus_tree_leaf_page(locked_page_info* page_info, const void* tuple_to_insert, uint32_t tuple_to_insert_at, const bplus_tree_tuple_defs* bpttds, data_access_methods* dam_p);
+
+// it performs merge of the 2 leaf pages
+// the page_info1 and page_info2 MUST be adjacent pages sharing a parent node
+// this function will perform page compaction on the page_info1 if required
+// it fails with a 0 if the pages can not be merged (this may be due to their used spaces greater than the allotted size on the page_info1)
+// you may call this function regardless of you knowing that the pages may be merged
+int merge_bplus_tree_leaf_pages(locked_page_info* page_info1, locked_page_info* page_info2, bplus_tree_tuple_defs* bpttds, data_access_methods* dam_p);
 
 #endif
--- src/bplus_tree_leaf_page_util.c
+++ src/bplus_tree_leaf_page_util.c
@@ -2,7 +2,7 @@
 
 #include<sorted_packed_page_util.h>
 
-uint32_t find_greater_equals_for_key(const void* page, const void* key, const bplus_tree_tuple_defs* bpttd_p)
+uint32_t find_greater_equals_for_key_bplus_tree_leaf_page(const void* page, const void* key, const bplus_tree_tuple_defs* bpttd_p)
 {
 	return find_succeeding_equals_in_sorted_packed_page(
 									page, bpttd_p->page_size, 
@@ -11,11 +11,21 @@ uint32_t find_greater_equals_for_key(const void* page, const void* key, const bp
 								);
 }
 
-uint32_t find_lesser_equals_for_key(const void* page, const void* key, const bplus_tree_tuple_defs* bpttd_p)
+uint32_t find_lesser_equals_for_key_bplus_tree_leaf_page(const void* page, const void* key, const bplus_tree_tuple_defs* bpttd_p)
 {
 	return find_preceding_equals_in_sorted_packed_page(
 									page, bpttd_p->page_size, 
 									bpttd_p->record_def, bpttd_p->key_element_ids, bpttd_p->key_element_count,
 									key, bpttd_p->key_def, NULL
 								);
+}
+
+const void* split_insert_bplus_tree_leaf_page(locked_page_info* page_info, const void* tuple_to_insert, uint32_t tuple_to_insert_at, const bplus_tree_tuple_defs* bpttds, data_access_methods* dam_p)
+{
+
+}
+
+int merge_bplus_tree_leaf_pages(locked_page_info* page_info1, locked_page_info* page_info2, bplus_tree_tuple_defs* bpttds, data_access_methods* dam_p)
+{
+
 }
