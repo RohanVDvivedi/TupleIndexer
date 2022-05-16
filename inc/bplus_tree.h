e648424aaa386366cc3d55725312d1213cb7dab9
RohanVDvivedi <rohandvivedi@gmail.com>
2022-05-15 19:57:55 -0700

    added declarations for bplus_tree_cursor and the core api

--- inc/bplus_tree.h
+++ inc/bplus_tree.h
@@ -9,9 +9,14 @@
 // returns pointer to the root page of the bplus_tree
 uint64_t get_new_bplus_tree(const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p);
 
-// returns a bplus_tree_cursor to read from key_start to key_last both inclusive
-// the direction of the cursor is decided on the basis of ordering of key_start and key_last
-bplus_tree_cursor* find_range_in_bplus_tree(uint64_t root_page_id, const void* key_start, const void* key_last, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p);
+// returns a bplus_tree_cursor to read from key (key inclusive)
+// if record with given key is absent from the bplus_tree 
+// 	then if scan_dir == -1, then the cursor points to a record lesser than the key
+// 	then if scan_dir == +1, then the cursor points to a record greater than the key
+// if the key is NULL
+// 	then if the scan_dir == -1, then the cursor points to the greatest record in the bplus_tree
+// 	then if the scan_dir == +1, then the cursor points to the least record in the bplus_tree
+bplus_tree_cursor* find_in_bplus_tree(uint64_t root_page_id, const void* key, int scan_dir, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p);
 
 // insert record in bplus_tree
 int insert_in_bplus_tree(uint64_t root_page_id, const void* record, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p);
--- inc/bplus_tree_cursor.h
+++ inc/bplus_tree_cursor.h
@@ -3,23 +3,36 @@
 
 #include<stdint.h>
 
-typedef enum bplus_tree_cursor_direction bplus_tree_cursor_direction;
-enum bplus_tree_cursor_direction
-{
-	TOWARDS_NEXT,
-	TOWARDS_PREV,
-};
+#include<data_access_methods.h>
+#include<bplus_tree_tuple_definitions.h>
 
 typedef struct bplus_tree_cursor bplus_tree_cursor;
 struct bplus_tree_cursor
 {
+	void* curr_page;
+
 	uint64_t curr_page_id;
 
 	uint32_t curr_tuple_index;
 
-	int curr_tuple_seen;	// 1 if the tuple pointed to by curr_page_id and curr_tuple_index has been returned to the user
+	const bplus_tree_tuple_defs* bpttd_p;
 
-	bplus_tree_cursor_direction direction;
+	const data_access_methods* dam_p;
 };
+
+bplus_tree_cursor* get_new_bplus_tree_cursor(void* curr_page, uint64_t curr_page_id, uint32_t curr_tuple_index, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p);
+
+// it moves the cursor forward by a tuple
+// returns 1 for success, it returns 0, if there are no records to move to
+int next_bplus_tree_cursor(bplus_tree_cursor* cursor);
+
+// returns pointer to the current tuple that the cursor points to
+const void* get_curr_bplus_tree_cursor(bplus_tree_cursor* cursor);
+
+// it moves the cursor backward by a tuple
+// returns 1 for success, it returns 0, if there are no records to move to
+int prev_bplus_tree_cursor(bplus_tree_cursor* cursor);
+
+int destroy_bplus_tree_cursor(bplus_tree_cursor* cursor);
 
 #endif
