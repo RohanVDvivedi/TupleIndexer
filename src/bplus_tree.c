25e2f9e7be3cf73eba2b9b44ae4f2ea1f4c11933
RohanVDvivedi <rohandvivedi@gmail.com>
2022-05-16 23:18:19 -0700

    handling of root split when root is an interior page

--- src/bplus_tree.c
+++ src/bplus_tree.c
@@ -183,6 +183,29 @@ int insert_in_bplus_tree(uint64_t root_page_id, const void* record, const bplus_
 				// if it still fails then call the split insert
 				if(!inserted)
 				{
+					if(curr_locked_page->is_root)
+					{
+						// get a new page to insert between the root and its children
+						uint64_t root_least_keys_child_id;
+						void* root_least_keys_child = dam_p->get_new_page_with_write_lock(dam_p->context, &root_least_keys_child_id);
+
+						// clone root page contents into the new root_least_keys_child
+						clone_page(root_least_keys_child, bpttd_p->page_size, bpttd_p->record_def, 1, curr_locked_page->page);
+
+						// re intialize root page as an interior page
+						init_bplus_tree_interior_page(curr_locked_page->page, ++curr_locked_page->level, bpttd_p);
+						set_least_keys_page_id_of_bplus_tree_interior_page(curr_locked_page->page, root_least_keys_child_id, bpttd_p);
+
+						// create new locked_page_info for the root_least_keys_child and push it onto the stack
+						locked_page_info* root_least_keys_child_info = get_new_locked_page_info(root_least_keys_child, root_least_keys_child_id, 1, 0, bpttd_p);
+						root_least_keys_child_info->child_index = curr_locked_page->child_index;
+						curr_locked_page->child_index = -1; // the root page only has a single child at the moment
+						push_stack_bplus_tree_locked_pages_stack(&locked_pages_stack, root_least_keys_child_info);
+
+						// get new top of the stack
+						curr_locked_page = get_top_stack_bplus_tree_locked_pages_stack(&locked_pages_stack);
+					}
+
 					new_parent_insert = split_insert_bplus_tree_interior_page(curr_locked_page->page, curr_locked_page->page_id, record, insertion_point, bpttd_p, dam_p);
 					
 					if(new_parent_insert != NULL)
