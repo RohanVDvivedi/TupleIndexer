b82ff6715d225d1ea1f8788a2656d2a147e996ee
RohanVDvivedi <rohandvivedi@gmail.com>
2022-05-17 14:25:15 -0700

    implemented print for bplus_tree page header

--- src/bplus_tree_interior_page_header.c
+++ src/bplus_tree_interior_page_header.c
@@ -140,7 +140,7 @@ void set_next_page_id_of_bplus_tree_interior_page(void* page, uint64_t page_id, 
 
 void print_bplus_tree_interior_page_header(const void* page, const bplus_tree_tuple_defs* bpttd_p)
 {
-	print_bplus_tree_page_header(page, bpttd_p);
+	print_bplus_tree_page_header(page, bpttd_p->page_size);
 	printf("next_page_id : %llu\n", (unsigned long long int)get_next_page_id_of_bplus_tree_interior_page(page, bpttd_p));
 	printf("least_keys_page_id : %llu\n", (unsigned long long int)get_least_keys_page_id_of_bplus_tree_interior_page(page, bpttd_p));
 }
--- src/bplus_tree_page.c
+++ src/bplus_tree_page.c
@@ -15,3 +15,9 @@ void set_level_of_bplus_tree_page(void* page, uint32_t page_size, uint32_t level
 	bplus_tree_page_header* ph = get_page_header(page, page_size) + sizeof(page_header);
 	ph->level = level;
 }
+
+void print_bplus_tree_page_header(const void* page, uint32_t page_size)
+{
+	print_common_page_header(page, page_size);
+	printf("level : %u\n", get_level_of_bplus_tree_page(page, page_size));
+}
