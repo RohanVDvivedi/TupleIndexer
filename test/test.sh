4e4d06e5eeca7e306ce70671a5b37ae4dbffe672
RohanVDvivedi <rohandvivedi@gmail.com>
2022-06-14 13:20:20 -0700

    configured shell script to run valgrind and time for test files

--- test/test.sh
+++ test/test.sh
@@ -1,1 +1,14 @@
-gcc -Wall ./test_$1.c -o test_$1.out -ltupleindexer -ltuplestore -lrwlock -lcutlery && sudo valgrind -v --track-origins=yes ./test_$1.out && sudo time -v ./test_$1.out
+gcc -Wall ./test_$1.c -o test_$1.out -ltupleindexer -ltuplestore -lrwlock -lcutlery
+if [ $? -eq 0 ]
+then
+	if [ $2 = "vald" ]
+	then
+		sudo valgrind -v --track-origins=yes ./test_$1.out
+	elif [ $2 = "time" ]
+	then
+		sudo time -v ./test_$1.out
+	else
+		./test_$1.out
+	fi
+	echo $2
+fi
