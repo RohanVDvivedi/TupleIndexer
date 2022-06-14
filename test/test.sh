7c1dd319143e4b48b921494fd08a9df956983257
RohanVDvivedi <rohandvivedi@gmail.com>
2022-06-14 13:26:15 -0700

    minor changes to shell script

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,10 @@
 gcc -Wall ./test_$1.c -o test_$1.out -ltupleindexer -ltuplestore -lrwlock -lcutlery
 if [ $? -eq 0 ]
 then
-	if [ $2 = "vald" ]
+	if [ "$2" = "vald" ]
 	then
 		sudo valgrind -v --track-origins=yes ./test_$1.out
-	elif [ $2 = "time" ]
+	elif [ "$2" = "time" ]
 	then
 		sudo time -v ./test_$1.out
 	else
