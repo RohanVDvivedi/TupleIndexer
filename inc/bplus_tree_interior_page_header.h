c9bebe14fb358a28499a0546ebe774851db79a65
RohanVDvivedi <rohandvivedi@gmail.com>
2022-06-27 09:48:19 -0700

    implemented functions to allow is_last_page_of_level as 1 byte field, reducing header size

--- inc/bplus_tree_interior_page_header.h
+++ inc/bplus_tree_interior_page_header.h
@@ -10,8 +10,8 @@ uint32_t sizeof_INTERIOR_PAGE_HEADER(const bplus_tree_tuple_defs* bpttd_p);
 uint64_t get_least_keys_page_id_of_bplus_tree_interior_page(const void* page, const bplus_tree_tuple_defs* bpttd_p);
 void set_least_keys_page_id_of_bplus_tree_interior_page(void* page, uint64_t page_id, const bplus_tree_tuple_defs* bpttd_p);
 
-uint64_t get_next_page_id_of_bplus_tree_interior_page(const void* page, const bplus_tree_tuple_defs* bpttd_p);
-void set_next_page_id_of_bplus_tree_interior_page(void* page, uint64_t page_id, const bplus_tree_tuple_defs* bpttd_p);
+int is_last_page_of_level_of_bplus_tree_interior_page(const void* page, const bplus_tree_tuple_defs* bpttd_p);
+void set_is_last_page_of_level_of_bplus_tree_interior_page(void* page, int is_last_page_of_level, const bplus_tree_tuple_defs* bpttd_p);
 
 // prints header of bplus_tree interior page
 void print_bplus_tree_interior_page_header(const void* page, const bplus_tree_tuple_defs* bpttd_p);
--- src/bplus_tree_interior_page_header.c
+++ src/bplus_tree_interior_page_header.c
@@ -3,9 +3,15 @@
 #include<page_layout.h>
 #include<int_accesses.h>
 
+// number of bytes in the flags field of the header
+#define FLAGS_BYTE_SIZE 1
+
+// bit position of is_last_page_of_level
+#define IS_LAST_PAGE_OF_LEVEL_FLAG_POS 0
+
 uint32_t sizeof_INTERIOR_PAGE_HEADER(const bplus_tree_tuple_defs* bpttd_p)
 {
-	return get_size_of_page_type_header() + get_size_of_bplus_tree_page_level_header() + bpttd_p->page_id_width * 2;
+	return get_size_of_page_type_header() + get_size_of_bplus_tree_page_level_header() + bpttd_p->page_id_width + FLAGS_BYTE_SIZE;
 }
 
 uint64_t get_least_keys_page_id_of_bplus_tree_interior_page(const void* page, const bplus_tree_tuple_defs* bpttd_p)
@@ -24,20 +30,26 @@ void set_least_keys_page_id_of_bplus_tree_interior_page(void* page, uint64_t pag
 	return write_uint64(least_keys_page_id, bpttd_p->page_id_width, page_id);
 }
 
-uint64_t get_next_page_id_of_bplus_tree_interior_page(const void* page, const bplus_tree_tuple_defs* bpttd_p)
+int is_last_page_of_level_of_bplus_tree_interior_page(const void* page, const bplus_tree_tuple_defs* bpttd_p)
 {
 	const void* page_header = get_page_header((void*)page, bpttd_p->page_size);
 	const void* leaf_page_header = page_header + get_size_of_page_type_header() + get_size_of_bplus_tree_page_level_header();
-	const void* next_page_id = leaf_page_header + bpttd_p->page_id_width;
-	return read_uint64(next_page_id, bpttd_p->page_id_width);
+	const void* flags = leaf_page_header + bpttd_p->page_id_width;
+	int64_t flags_val = read_int64(flags, FLAGS_BYTE_SIZE);
+	return (flags_val >> IS_LAST_PAGE_OF_LEVEL_FLAG_POS) & INT64_C(1);
 }
 
-void set_next_page_id_of_bplus_tree_interior_page(void* page, uint64_t page_id, const bplus_tree_tuple_defs* bpttd_p)
+void set_is_last_page_of_level_of_bplus_tree_interior_page(void* page, int is_last_page_of_level, const bplus_tree_tuple_defs* bpttd_p)
 {
 	void* page_header = get_page_header((void*)page, bpttd_p->page_size);
 	void* leaf_page_header = page_header + get_size_of_page_type_header() + get_size_of_bplus_tree_page_level_header();
-	void* next_page_id = leaf_page_header + bpttd_p->page_id_width;
-	return write_uint64(next_page_id, bpttd_p->page_id_width, page_id);
+	void* flags = leaf_page_header + bpttd_p->page_id_width;
+	int64_t flags_val = read_int64(flags, FLAGS_BYTE_SIZE);
+	if(is_last_page_of_level)
+		flags_val |= (INT64_C(1) << IS_LAST_PAGE_OF_LEVEL_FLAG_POS);
+	else
+		flags_val &= (~(INT64_C(1) << IS_LAST_PAGE_OF_LEVEL_FLAG_POS));
+	return write_int64(flags, FLAGS_BYTE_SIZE, flags_val);
 }
 
 #include<stdio.h>
@@ -45,6 +57,6 @@ void set_next_page_id_of_bplus_tree_interior_page(void* page, uint64_t page_id, 
 void print_bplus_tree_interior_page_header(const void* page, const bplus_tree_tuple_defs* bpttd_p)
 {
 	print_bplus_tree_page_header(page, bpttd_p->page_size);
-	printf("next_page_id : %llu\n", (unsigned long long int)get_next_page_id_of_bplus_tree_interior_page(page, bpttd_p));
 	printf("least_keys_page_id : %llu\n", (unsigned long long int)get_least_keys_page_id_of_bplus_tree_interior_page(page, bpttd_p));
+	printf("is_last_page_of_level : %d\n", is_last_page_of_level_of_bplus_tree_interior_page(page, bpttd_p));
 }
