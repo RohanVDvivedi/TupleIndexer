82b2e04d340bf6685faee9588f77a660d3205327
RohanVDvivedi <rohandvivedi@gmail.com>
2022-07-13 14:21:42 -0700

    minor change to allow use of is_NULL of user_value instead of two different functions

--- src/bplus_tree_index_tuple_functions_util.c
+++ src/bplus_tree_index_tuple_functions_util.c
@@ -6,10 +6,12 @@ uint64_t get_child_page_id_from_index_tuple(const void* index_tuple, const bplus
 {
 	uint64_t child_page_id = bpttd_p->NULL_PAGE_ID;
 
-	if(is_NULL_in_tuple(bpttd_p->index_def, bpttd_p->index_def->element_count - 1, index_tuple))
-		return child_page_id;
+	// get user value for the child_page_id element from the tuple
+	user_value child_page_id_value = get_value_from_element_from_tuple(bpttd_p->index_def, bpttd_p->index_def->element_count - 1, index_tuple);
 
-	child_page_id = get_value_from_element_from_tuple(bpttd_p->index_def, bpttd_p->index_def->element_count - 1, index_tuple).uint_value;
+	// if it is not NULL then update it with the value as found in the tuple
+	if(!child_page_id_value.is_NULL)
+		child_page_id = child_page_id_value.uint_value;
 
 	return child_page_id;
 }
