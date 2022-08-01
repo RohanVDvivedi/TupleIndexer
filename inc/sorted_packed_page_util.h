98ba97c855274194cf2df6a5c23bbb28fa0a5236
RohanVDvivedi <rohandvivedi@gmail.com>
2022-07-31 21:06:02 -0700

    added more comments to the declarations of the sorted_packed_page functions

--- inc/sorted_packed_page_util.h
+++ inc/sorted_packed_page_util.h
@@ -11,13 +11,16 @@
 #include<tuple_def.h>
 
 // returns index at which a new tuple may be inserted
+// returns an index in range from (0 to tuple_count) where the tuple can be inserted, keeping the sorted page sorted
+// if a tuple/tuples with the given key as in "tuple" parameter exists
+// than the index returns is right after these tuples that compare equals to the "tuple" parameter
 uint32_t find_insertion_point_in_sorted_packed_page(
 									const void* page, uint32_t page_size, 
 									const tuple_def* tpl_def, const uint32_t* tuple_keys_to_compare, uint32_t keys_count,
 									const void* tuple
 								);
 
-// insert tuple
+// insert tuple (the tuple will be inserted at the index given by find_insertion_point_in_sorted_packed_page() function)
 // returns 1 tuple was inserted
 // returns the index the new tuple is inserted on
 int insert_to_sorted_packed_page(
@@ -85,28 +88,30 @@ uint32_t find_last_in_sorted_packed_page(
 									const void* key, const tuple_def* key_def, const uint32_t* key_elements_to_compare
 								);
 
-// returns index of the tuple found
+// returns index of the tuple found at the greatest index that is lesser than the key
 uint32_t find_preceding_in_sorted_packed_page(
 									const void* page, uint32_t page_size, 
 									const tuple_def* tpl_def, const uint32_t* tuple_keys_to_compare, uint32_t keys_count,
 									const void* key, const tuple_def* key_def, const uint32_t* key_elements_to_compare
 								);
 
-// returns index of the tuple found
+// returns index of the tuple found at the greatest index that is lesser than or equal to the key
+// if there are tuples that compare equal to the key, then the last index of the tuple that compare equal to the key is returned
 uint32_t find_preceding_equals_in_sorted_packed_page(
 									const void* page, uint32_t page_size, 
 									const tuple_def* tpl_def, const uint32_t* tuple_keys_to_compare, uint32_t keys_count,
 									const void* key, const tuple_def* key_def, const uint32_t* key_elements_to_compare
 								);
 
-// returns index of the tuple found
+// returns index of the tuple found at the least index that is greater than or equal to the key
+// if there are tuples that compare equal to the key, then the first index of the tuple that compare equal to the key is returned
 uint32_t find_succeeding_equals_in_sorted_packed_page(
 									const void* page, uint32_t page_size, 
 									const tuple_def* tpl_def, const uint32_t* tuple_keys_to_compare, uint32_t keys_count,
 									const void* key, const tuple_def* key_def, const uint32_t* key_elements_to_compare
 								);
 
-// returns index of the tuple found
+// returns index of the tuple found at the least index that is greater than the key
 uint32_t find_succeeding_in_sorted_packed_page(
 									const void* page, uint32_t page_size, 
 									const tuple_def* tpl_def, const uint32_t* tuple_keys_to_compare, uint32_t keys_count,
