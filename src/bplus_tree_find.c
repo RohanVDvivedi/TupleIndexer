98a565305c0f8eda4ba57ab2b4b063485d988171
RohanVDvivedi <rohandvivedi@gmail.com>
2022-08-13 16:04:38 -0700

    implemented find_type to decide how to process find

--- src/bplus_tree_find.c
+++ src/bplus_tree_find.c
@@ -1,5 +1,16 @@
 #include<bplus_tree.h>
 
+typedef enum find_type find_type;
+enum find_type
+{
+	MIN_TUPLE,
+	LESSER_THAN_KEY,
+	LESSER_THAN_EQUAL_TO_KEY,
+	GREATER_THAN_EQUAL_TO_KEY,
+	GREATER_THAN_KEY,
+	MAX_TUPLE,
+};
+
 bplus_tree_iterator* find_in_bplus_tree(uint64_t root_page_id, const void* key, find_position find_pos, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p)
 {
 	// get lock on the root page of the bplus_tree
