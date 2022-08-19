f9e1f4658269853e21626ad66278a551a63bbc9b
RohanVDvivedi <rohandvivedi@gmail.com>
2022-08-19 12:00:41 -0700

    common_page_header fixed

--- inc/common_page_header.h
+++ inc/common_page_header.h
@@ -1,6 +1,8 @@
 #ifndef COMMON_PAGE_HEADER_H
 #define COMMON_PAGE_HEADER_H
 
+#include<bplus_tree_tuple_definitions.h>
+
 #include<stdint.h>
 
 typedef enum page_type page_type;
@@ -15,9 +17,9 @@ uint32_t get_offset_of_page_type_header(const bplus_tree_tuple_defs* bpttd_p);
 uint32_t get_size_of_page_type_header();
 
 // getter and setter for page type of the page
-page_type get_type_of_page(const void* page, uint32_t page_size);
-void set_type_of_page(void* page, uint32_t page_size, page_type type);
+page_type get_type_of_page(const void* page, const bplus_tree_tuple_defs* bpttd_p);
+void set_type_of_page(void* page, page_type type, const bplus_tree_tuple_defs* bpttd_p);
 
-void print_common_page_header(const void* page, uint32_t page_size);
+void print_common_page_header(const void* page, const bplus_tree_tuple_defs* bpttd_p);
 
 #endif
--- src/common_page_header.c
+++ src/common_page_header.c
@@ -21,19 +21,19 @@ uint32_t get_size_of_page_type_header()
 	return BYTES_FOR_PAGE_TYPE;
 }
 
-page_type get_type_of_page(const void* page, uint32_t page_size)
+page_type get_type_of_page(const void* page, const bplus_tree_tuple_defs* bpttd_p)
 {
-	const void* page_type_header = get_page_header((void*)page, page_size) + get_offset_of_page_type_header(page, page_size);
+	const void* page_type_header = get_page_header((void*)page, bpttd_p->page_size) + get_offset_of_page_type_header(bpttd_p);
 	return (page_type)read_uint16(page_type_header, BYTES_FOR_PAGE_TYPE);
 }
 
-void set_type_of_page(void* page, uint32_t page_size, page_type type)
+void set_type_of_page(void* page, page_type type, const bplus_tree_tuple_defs* bpttd_p)
 {
-	void* page_type_header = get_page_header((void*)page, page_size) + get_offset_of_page_type_header(page, page_size);
+	void* page_type_header = get_page_header((void*)page, bpttd_p->page_size) + get_offset_of_page_type_header(bpttd_p);
 	write_uint16(page_type_header, BYTES_FOR_PAGE_TYPE, ((uint16_t)type));
 }
 
-void print_common_page_header(const void* page, uint32_t page_size)
+void print_common_page_header(const void* page, const bplus_tree_tuple_defs* bpttd_p)
 {
-	printf("page_type : %s\n", page_type_string[get_type_of_page(page, page_size)]);
+	printf("page_type : %s\n", page_type_string[get_type_of_page(page, bpttd_p)]);
 }
