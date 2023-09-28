7740d32748c71a9f9b1702988f1536f37a52bf7c
RohanVDvivedi <rohandvivedi@gmail.com>
2023-09-27 23:11:34 -0700

    sorted packed pages will never have tomb stones

--- src/bplus_tree_leaf_page_util.c
+++ src/bplus_tree_leaf_page_util.c
@@ -316,10 +316,9 @@ int merge_bplus_tree_leaf_pages(void* page1, uint64_t page1_id, const bplus_tree
 	uint32_t total_space_page1 = get_space_allotted_to_all_tuples_on_page(page1, bpttd_p->page_size, &(bpttd_p->record_def->size_def));
 	uint32_t space_in_use_page1 = get_space_occupied_by_all_tuples_on_page(page1, bpttd_p->page_size, &(bpttd_p->record_def->size_def));
 	uint32_t space_in_use_page2 = get_space_occupied_by_all_tuples_on_page(page2, bpttd_p->page_size, &(bpttd_p->record_def->size_def));
-	uint32_t space_in_tomb_stones_page2 = get_space_occupied_by_all_tomb_stones_on_page(page2, bpttd_p->page_size, &(bpttd_p->record_def->size_def));
 
 	// inserting tuples from one page to the another is done while discarding tomb_stones during the insertion
-	if(total_space_page1 < space_in_use_page1 + space_in_use_page2 - space_in_tomb_stones_page2)
+	if(total_space_page1 < space_in_use_page1 + space_in_use_page2)
 	{
 		// release writer lock on the page, and we did not modify it
 		dam_p->release_writer_lock_on_page(dam_p->context, page2, 0);
