e253691427b8ee8699f32080c225dffb4da5069f
RohanVDvivedi <rohandvivedi@gmail.com>
2023-09-27 18:27:45 -0700

    refactored bplus_tree_tuple_definitions.c to use new tuple store api

--- src/bplus_tree_tuple_definitions.c
+++ src/bplus_tree_tuple_definitions.c
@@ -12,7 +12,7 @@
 int init_bplus_tree_tuple_definitions(bplus_tree_tuple_defs* bpttd_p, uint32_t default_common_header_size, const tuple_def* record_def, const uint32_t* key_element_ids, uint32_t key_element_count, uint32_t page_size, uint8_t page_id_width, uint64_t NULL_PAGE_ID)
 {
 	// basic parameter check
-	if(key_element_count == 0 || key_element_ids == NULL || record_def == NULL || record_def->element_count == 0)
+	if(key_element_count == 0 || key_element_ids == NULL || record_def == NULL || get_element_def_count_tuple_def(record_def) == 0)
 		return 0;
 
 	// bytes required to store page id
@@ -38,12 +38,12 @@ int init_bplus_tree_tuple_definitions(bplus_tree_tuple_defs* bpttd_p, uint32_t d
 	memcpy(bpttd_p->key_element_ids, key_element_ids, sizeof(uint32_t) * bpttd_p->key_element_count);
 
 	bpttd_p->record_def = clone_tuple_def(record_def);
-	finalize_tuple_def(bpttd_p->record_def, page_size);
+	finalize_tuple_def(bpttd_p->record_def);
 
 	// initialize index_def
 
 	// allocate memory for index def and initialize it
-	bpttd_p->index_def = get_new_tuple_def("temp_index_def", key_element_count + 1);
+	bpttd_p->index_def = get_new_tuple_def("temp_index_def", key_element_count + 1, page_size);
 
 	// result of inserting element_definitions to index def
 	int res = 1;
@@ -64,12 +64,12 @@ int init_bplus_tree_tuple_definitions(bplus_tree_tuple_defs* bpttd_p, uint32_t d
 	}
 
 	// end step
-	finalize_tuple_def(bpttd_p->index_def, page_size);
+	finalize_tuple_def(bpttd_p->index_def);
 
 	// initialize key def
 
 	// allocate memory for key_def and initialize it
-	bpttd_p->key_def = get_new_tuple_def("temp_key_def", key_element_count);
+	bpttd_p->key_def = get_new_tuple_def("temp_key_def", key_element_count, page_size);
 
 	// result of inserting element_definitions to key_def
 	res = 1;
@@ -86,7 +86,7 @@ int init_bplus_tree_tuple_definitions(bplus_tree_tuple_defs* bpttd_p, uint32_t d
 	}
 
 	// end step
-	finalize_tuple_def(bpttd_p->key_def, page_size);
+	finalize_tuple_def(bpttd_p->key_def);
 
 	if(get_maximum_insertable_record_size(bpttd_p) < get_minimum_tuple_size(bpttd_p->record_def))
 	{
@@ -127,7 +127,7 @@ void build_index_entry_from_record_tuple(const bplus_tree_tuple_defs* bpttd_p, c
 		set_element_in_tuple_from_tuple(bpttd_p->index_def, i, index_entry, bpttd_p->record_def, bpttd_p->key_element_ids[i], record_tuple);
 
 	// copy the child_page_id to the last element in the index entry
-	set_element_in_tuple(bpttd_p->index_def, bpttd_p->index_def->element_count - 1, index_entry, &((const user_value){.uint_value = child_page_id}));
+	set_element_in_tuple(bpttd_p->index_def, get_element_def_count_tuple_def(bpttd_p->index_def) - 1, index_entry, &((const user_value){.uint_value = child_page_id}));
 }
 
 void build_index_entry_from_key(const bplus_tree_tuple_defs* bpttd_p, const void* key, uint64_t child_page_id, void* index_entry)
@@ -140,7 +140,7 @@ void build_index_entry_from_key(const bplus_tree_tuple_defs* bpttd_p, const void
 		set_element_in_tuple_from_tuple(bpttd_p->index_def, i, index_entry, bpttd_p->key_def, i, key);
 
 	// copy the child_page_id to the last element in the index entry
-	set_element_in_tuple(bpttd_p->index_def, bpttd_p->index_def->element_count - 1, index_entry, &((user_value){.uint_value = child_page_id}));
+	set_element_in_tuple(bpttd_p->index_def, get_element_def_count_tuple_def(bpttd_p->index_def) - 1, index_entry, &((user_value){.uint_value = child_page_id}));
 }
 
 void deinit_bplus_tree_tuple_definitions(bplus_tree_tuple_defs* bpttd_p)
@@ -167,14 +167,14 @@ void deinit_bplus_tree_tuple_definitions(bplus_tree_tuple_defs* bpttd_p)
 uint32_t get_maximum_insertable_record_size(const bplus_tree_tuple_defs* bpttd_p)
 {
 	uint32_t min_record_tuple_count = 2;
-	uint32_t total_available_space_in_leaf_page = get_space_to_be_allotted_to_all_tuples(sizeof_LEAF_PAGE_HEADER(bpttd_p), bpttd_p->page_size, bpttd_p->record_def);
+	uint32_t total_available_space_in_leaf_page = get_space_to_be_allotted_to_all_tuples_on_page(sizeof_LEAF_PAGE_HEADER(bpttd_p), bpttd_p->page_size, &(bpttd_p->record_def->size_def));
 	uint32_t total_available_space_in_leaf_page_per_min_tuple_count = total_available_space_in_leaf_page / min_record_tuple_count;
-	uint32_t max_record_size_per_leaf_page = total_available_space_in_leaf_page_per_min_tuple_count - get_additional_space_overhead_per_tuple(bpttd_p->page_size, bpttd_p->record_def);
+	uint32_t max_record_size_per_leaf_page = total_available_space_in_leaf_page_per_min_tuple_count - get_additional_space_overhead_per_tuple_on_page(bpttd_p->page_size, &(bpttd_p->record_def->size_def));
 
 	uint32_t min_index_record_tuple_count = 2;
-	uint32_t total_available_space_in_interior_page = get_space_to_be_allotted_to_all_tuples(sizeof_INTERIOR_PAGE_HEADER(bpttd_p), bpttd_p->page_size, bpttd_p->index_def);
+	uint32_t total_available_space_in_interior_page = get_space_to_be_allotted_to_all_tuples_on_page(sizeof_INTERIOR_PAGE_HEADER(bpttd_p), bpttd_p->page_size, &(bpttd_p->index_def->size_def));
 	uint32_t total_available_space_in_interior_page_per_min_tuple_count = total_available_space_in_interior_page / min_index_record_tuple_count;
-	uint32_t max_key_size_per_interior_page = total_available_space_in_interior_page_per_min_tuple_count - get_additional_space_overhead_per_tuple(bpttd_p->page_size, bpttd_p->index_def) - bpttd_p->page_id_width - 1; 
+	uint32_t max_key_size_per_interior_page = total_available_space_in_interior_page_per_min_tuple_count - get_additional_space_overhead_per_tuple_on_page(bpttd_p->page_size, &(bpttd_p->index_def->size_def)) - bpttd_p->page_id_width - 1; 
 	// this 1 at the end is important believe me, it represents a spill over bit from storing the is_null bit for the child page pointer
 
 	#define min(a,b) (((a)<(b))?(a):(b))
