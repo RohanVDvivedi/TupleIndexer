9211c0786ab2300a0c7f36ebcfb288d37d565754
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-02 16:48:24 -0700

    implemented set for bplus_tree_page_header

--- src/bplus_tree_page_header.c
+++ src/bplus_tree_page_header.c
@@ -3,6 +3,8 @@
 #include<page_layout_unaltered.h>
 #include<int_accesses.h>
 
+#include<stdlib.h>
+
 // number of bytes to store level of the page
 // 2 is more than enough for curent computer systems, and allows us to store large amount of data
 // values in range 1 to 4 both inclusive
@@ -53,7 +55,24 @@ bplus_tree_page_header get_bplus_tree_page_header(const void* page, const bplus_
 
 void serialize_bplus_tree_page_header(void* hdr_serial, const bplus_tree_page_header* bptph_p, const bplus_tree_tuple_defs* bpttd_p);
 
-void set_bplus_tree_page_header(persistent_page ppage, const bplus_tree_page_header* bptph_p, const bplus_tree_tuple_defs* bpttd_p, const page_modification_methods* pmm_p);
+void set_bplus_tree_page_header(persistent_page ppage, const bplus_tree_page_header* bptph_p, const bplus_tree_tuple_defs* bpttd_p, const page_modification_methods* pmm_p)
+{
+	uint32_t page_header_size = get_page_header_size(ppage.page, bpttd_p->page_size);
+
+	// allocate memory, to hold complete page_header
+	void* hdr_serial = malloc(page_header_size);
+
+	// copy the old page_header to it
+	memory_move(hdr_serial, get_page_header_ua(ppage.page, bpttd_p->page_size), page_header_size);
+
+	// serialize bptph_p on the hdr_serial
+	serialize_bplus_tree_page_header(hdr_serial, bptph_p, bpttd_p);
+
+	// write hdr_serial to the new header position
+	pmm_p->set_page_header(pmm_p->context, ppage, bpttd_p->page_size, hdr_serial);
+
+	free(hdr_serial);
+}
 
 void print_bplus_tree_page_header(const void* page, const bplus_tree_tuple_defs* bpttd_p)
 {
