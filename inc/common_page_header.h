4a5faaa625137f58739379afe6b3ffe4d544e3dc
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-02 13:07:13 -0700

    started refactoring with common_page_header

--- inc/common_page_header.h
+++ inc/common_page_header.h
@@ -2,6 +2,7 @@
 #define COMMON_PAGE_HEADER_H
 
 #include<bplus_tree_tuple_definitions.h>
+#include<page_modification_methods.h>
 
 #include<stdint.h>
 
@@ -10,20 +11,31 @@ enum page_type
 {
 	BPLUS_TREE_LEAF_PAGE,
 	BPLUS_TREE_INTERIOR_PAGE,
+	// add more page_types as you wish at most ((2^16) - 1) page_types can be supported
+};
+
+typedef struct common_page_header common_page_header;
+struct common_page_header
+{
+	page_type type;
 };
 
 /*
-**		default common header is prefix to even the common page header
-**		size of this default common header is equal to the what was specified in bplus_tree_tuple_definitions object
+**		system header is prefix to even the common page header
+**		size of this system header is equal to the what was specified in bplus_tree_tuple_definitions object
 */
 
 uint32_t get_offset_of_page_type_header(const bplus_tree_tuple_defs* bpttd_p);
 
 uint32_t get_size_of_page_type_header();
 
-// getter and setter for page type of the page
 page_type get_type_of_page(const void* page, const bplus_tree_tuple_defs* bpttd_p);
-void set_type_of_page(void* page, page_type type, const bplus_tree_tuple_defs* bpttd_p);
+
+common_page_header get_common_page_header(const void* page, const bplus_tree_tuple_defs* bpttd_p);
+
+void serialize_common_page_header(void* hdr_serial, const common_page_header* cph_p);
+
+void set_common_page_header(persistent_page ppage, const common_page_header* cph_p, const bplus_tree_tuple_defs* bpttd_p, const page_modification_methods* pmm_p);
 
 void print_common_page_header(const void* page, const bplus_tree_tuple_defs* bpttd_p);
 
--- src/common_page_header.c
+++ src/common_page_header.c
@@ -1,7 +1,11 @@
 #include<common_page_header.h>
 
-#include<page_layout.h>
+#include<page_layout_unaltered.h>
 #include<int_accesses.h>
+
+#include<cutlery_stds.h>
+
+#include<stdlib.h>
 
 const char* page_type_string[] = {
 	"BPLUS_TREE_LEAF_PAGE",
@@ -13,7 +17,7 @@ const char* page_type_string[] = {
 
 uint32_t get_offset_of_page_type_header(const bplus_tree_tuple_defs* bpttd_p)
 {
-	return bpttd_p->default_common_header_size;
+	return bpttd_p->system_header_size;
 }
 
 uint32_t get_size_of_page_type_header()
@@ -21,6 +25,7 @@ uint32_t get_size_of_page_type_header()
 	return BYTES_FOR_PAGE_TYPE;
 }
 
+/*
 page_type get_type_of_page(const void* page, const bplus_tree_tuple_defs* bpttd_p)
 {
 	const void* page_type_header = get_page_header((void*)page, bpttd_p->page_size) + get_offset_of_page_type_header(bpttd_p);
@@ -31,6 +36,41 @@ void set_type_of_page(void* page, page_type type, const bplus_tree_tuple_defs* b
 {
 	void* page_type_header = get_page_header((void*)page, bpttd_p->page_size) + get_offset_of_page_type_header(bpttd_p);
 	write_uint16(page_type_header, BYTES_FOR_PAGE_TYPE, ((uint16_t)type));
+}
+*/
+
+page_type get_type_of_page(const void* page, const bplus_tree_tuple_defs* bpttd_p)
+{
+	return get_common_page_header(page, bpttd_p).type;
+}
+
+common_page_header get_common_page_header(const void* page, const bplus_tree_tuple_defs* bpttd_p)
+{
+	const void* page_type_header = get_page_header_ua((void*)page, bpttd_p->page_size) + get_offset_of_page_type_header(bpttd_p);
+	return (common_page_header){
+		.type = (page_type) read_uint16(page_type_header, BYTES_FOR_PAGE_TYPE),
+	};
+}
+
+void serialize_common_page_header(void* hdr_serial, const common_page_header* cph_p);
+
+void set_common_page_header(persistent_page ppage, const common_page_header* cph_p, const bplus_tree_tuple_defs* bpttd_p, const page_modification_methods* pmm_p)
+{
+	uint32_t page_header_size = get_page_header_size(ppage.page, bpttd_p->page_size);
+
+	// allocate memory, to hold complete page_header
+	void* hdr_serial = malloc(page_header_size);
+
+	// copy the old page_header to it
+	memory_move(hdr_serial, get_page_header_ua(ppage.page, bpttd_p->page_size), page_header_size);
+
+	// serialize cph_p on the hdr_serial
+	serialize_common_page_header(hdr_serial, cph_p);
+
+	// write hdr_serial to the new header position
+	pmm_p->set_page_header(pmm_p->context, ppage, bpttd_p->page_size, hdr_serial);
+
+	free(hdr_serial);
 }
 
 void print_common_page_header(const void* page, const bplus_tree_tuple_defs* bpttd_p)
