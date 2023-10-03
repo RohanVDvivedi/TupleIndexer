b135fd5fb8f677fd27fb8d97e976a167dfaa6c6f
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-02 18:02:28 -0700

    finalized api of bplus_tree_interior_page_header

--- inc/bplus_tree_interior_page_header.h
+++ inc/bplus_tree_interior_page_header.h
@@ -1,19 +1,35 @@
 #ifndef BPLUS_TREE_INTERIOR_PAGE_HEADER_H
 #define BPLUS_TREE_INTERIOR_PAGE_HEADER_H
 
-#include<common_page_header.h>
-#include<bplus_tree_common_page_header.h>
+#include<bplus_tree_page_header.h>
 #include<bplus_tree_tuple_definitions.h>
+#include<page_modification_methods.h>
 
-// size of the complete header for a BPLUS_TREE_INTERIOR_PAGE type
-// this includes default_common_header_size + common_page_header + bplus_tree_level_page_header + interior_page_header
-uint32_t sizeof_INTERIOR_PAGE_HEADER(const bplus_tree_tuple_defs* bpttd_p);
+typedef struct bplus_tree_interior_page_header bplus_tree_interior_page_header;
+struct bplus_tree_interior_page_header
+{
+	bplus_tree_page_header parent;
+
+	// all leaf pages are doubly linked, with pointers (page_id-s) to next and prev pages
+	uint64_t least_keys_page_id;
+
+	// flag that suggests if this interior page is last one on this bplus_tree level
+	int is_last_page_of_level;
+};
+
+#define sizeof_INTERIOR_PAGE_HEADER get_offset_to_end_of_bplus_tree_interior_page_header
+
+uint32_t get_offset_to_end_of_bplus_interior_leaf_page_header(const bplus_tree_tuple_defs* bpttd_p);
 
 uint64_t get_least_keys_page_id_of_bplus_tree_interior_page(const void* page, const bplus_tree_tuple_defs* bpttd_p);
-void set_least_keys_page_id_of_bplus_tree_interior_page(void* page, uint64_t page_id, const bplus_tree_tuple_defs* bpttd_p);
 
 int is_last_page_of_level_of_bplus_tree_interior_page(const void* page, const bplus_tree_tuple_defs* bpttd_p);
-void set_is_last_page_of_level_of_bplus_tree_interior_page(void* page, int is_last_page_of_level, const bplus_tree_tuple_defs* bpttd_p);
+
+bplus_tree_interior_page_header get_bplus_tree_interior_page_header(const void* page, const bplus_tree_tuple_defs* bpttd_p);
+
+void serialize_bplus_tree_interior_page_header(void* hdr_serial, const bplus_tree_interior_page_header* bptlph_p, const bplus_tree_tuple_defs* bpttd_p);
+
+void set_bplus_tree_interior_page_header(persistent_page ppage, const bplus_tree_interior_page_header* bptlph_p, const bplus_tree_tuple_defs* bpttd_p, const page_modification_methods* pmm_p);
 
 // prints header of bplus_tree interior page
 void print_bplus_tree_interior_page_header(const void* page, const bplus_tree_tuple_defs* bpttd_p);
