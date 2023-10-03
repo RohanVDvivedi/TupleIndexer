a40402456cfa5029064076ee57ee8e2cdfd72027
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-02 18:21:38 -0700

    all headers implemented

--- src/bplus_tree_interior_page_header.c
+++ src/bplus_tree_interior_page_header.c
@@ -3,6 +3,8 @@
 #include<page_layout.h>
 #include<int_accesses.h>
 
+#include<stdlib.h>
+
 // number of bytes in the flags field of the header
 #define FLAGS_BYTE_SIZE 1
 
@@ -44,8 +46,8 @@ void serialize_bplus_tree_interior_page_header(void* hdr_serial, const bplus_tre
 	serialize_bplus_tree_page_header(hdr_serial, &(bptiph_p->parent), bpttd_p);
 
 	void* bplus_tree_interior_page_header_serial = hdr_serial + get_offset_to_bplus_tree_interior_page_header_locals(bpttd_p);
-	write_uint64(bplus_tree_interior_page_header_serial, bpttd_p->page_id_width, bptlph_p->least_keys_page_id);
-	write_uint64(bplus_tree_interior_page_header_serial + bpttd_p->page_id_width, FLAGS_BYTE_SIZE, (bptlph_p->is_last_page_of_level << IS_LAST_PAGE_OF_LEVEL_FLAG_POS));
+	write_uint64(bplus_tree_interior_page_header_serial, bpttd_p->page_id_width, bptiph_p->least_keys_page_id);
+	write_uint64(bplus_tree_interior_page_header_serial + bpttd_p->page_id_width, FLAGS_BYTE_SIZE, (bptiph_p->is_last_page_of_level << IS_LAST_PAGE_OF_LEVEL_FLAG_POS));
 }
 
 void set_bplus_tree_interior_page_header(persistent_page ppage, const bplus_tree_interior_page_header* bptiph_p, const bplus_tree_tuple_defs* bpttd_p, const page_modification_methods* pmm_p)
