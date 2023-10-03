a77f16c22ebf9adf4f22a4831c2c506980652377
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-03 08:03:40 -0700

    in_memory_data_store refactored to use uint64_max

--- src/in_memory_data_store.c
+++ src/in_memory_data_store.c
@@ -478,7 +478,7 @@ static int close_data_file(void* context)
 data_access_methods* get_new_in_memory_data_store(uint32_t page_size, uint8_t page_id_width)
 {
 	// check for invalud page_width
-	if(page_id_width == 0 || page_id_width > 8)
+	if(page_id_width == 0 || page_id_width > sizeof(uint64_t))
 		return NULL;
 
 	data_access_methods* dam_p = malloc(sizeof(data_access_methods));
@@ -499,11 +499,7 @@ data_access_methods* get_new_in_memory_data_store(uint32_t page_size, uint8_t pa
 	dam_p->page_size = page_size;
 	dam_p->page_id_width = page_id_width;
 
-	uint8_t bits_wanted1 = dam_p->page_id_width * 8;
-	if(bits_wanted1 < 64)
-		dam_p->NULL_PAGE_ID = (((uint64_t)1) << bits_wanted1) - ((uint64_t)1);
-	else
-		dam_p->NULL_PAGE_ID = -1;
+	dam_p->NULL_PAGE_ID = UINT64_MAX >> ((sizeof(uint64_t) - dam_p->page_id_width) * CHAR_BIT);
 	
 	dam_p->context = malloc(sizeof(memory_store_context));
 	if(dam_p->context == NULL)
