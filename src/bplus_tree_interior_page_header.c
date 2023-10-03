72ecb1910b8665f243e15fc02bb5c6e5666e676d
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-02 18:08:40 -0700

    implemented basic functions for bplus_tree_interior_page_header api

--- src/bplus_tree_interior_page_header.c
+++ src/bplus_tree_interior_page_header.c
@@ -9,54 +9,26 @@
 // bit position of is_last_page_of_level
 #define IS_LAST_PAGE_OF_LEVEL_FLAG_POS 0
 
-static uint32_t get_offset_of_bplus_tree_interior_page_header(const bplus_tree_tuple_defs* bpttd_p)
+uint32_t get_offset_to_end_of_bplus_interior_leaf_page_header(const bplus_tree_tuple_defs* bpttd_p)
 {
-	return get_offset_of_bplus_tree_page_level_header(bpttd_p) + get_size_of_bplus_tree_page_level_header();
-}
-
-static uint32_t get_size_of_bplus_tree_interior_page_header(const bplus_tree_tuple_defs* bpttd_p)
-{
-	return bpttd_p->page_id_width + FLAGS_BYTE_SIZE;
-}
-
-uint32_t sizeof_INTERIOR_PAGE_HEADER(const bplus_tree_tuple_defs* bpttd_p)
-{
-	return get_offset_of_bplus_tree_interior_page_header(bpttd_p) + get_size_of_bplus_tree_interior_page_header(bpttd_p);
+	return get_offset_to_end_of_bplus_tree_page_header(bpttd_p) + bpttd_p->page_id_width + 1;
 }
 
 uint64_t get_least_keys_page_id_of_bplus_tree_interior_page(const void* page, const bplus_tree_tuple_defs* bpttd_p)
 {
-	const void* leaf_page_header = get_page_header((void*)page, bpttd_p->page_size) + get_offset_of_bplus_tree_interior_page_header(bpttd_p);
-	const void* least_keys_page_id = leaf_page_header + 0;
-	return read_uint64(least_keys_page_id, bpttd_p->page_id_width);
-}
-
-void set_least_keys_page_id_of_bplus_tree_interior_page(void* page, uint64_t page_id, const bplus_tree_tuple_defs* bpttd_p)
-{
-	void* leaf_page_header = get_page_header((void*)page, bpttd_p->page_size) + get_offset_of_bplus_tree_interior_page_header(bpttd_p);
-	void* least_keys_page_id = leaf_page_header + 0;
-	return write_uint64(least_keys_page_id, bpttd_p->page_id_width, page_id);
+	return get_bplus_tree_interior_page_header(page, bpttd_p).least_keys_page_id;
 }
 
 int is_last_page_of_level_of_bplus_tree_interior_page(const void* page, const bplus_tree_tuple_defs* bpttd_p)
 {
-	const void* leaf_page_header = get_page_header((void*)page, bpttd_p->page_size) + get_offset_of_bplus_tree_interior_page_header(bpttd_p);
-	const void* flags = leaf_page_header + bpttd_p->page_id_width;
-	int64_t flags_val = read_int64(flags, FLAGS_BYTE_SIZE);
-	return (flags_val >> IS_LAST_PAGE_OF_LEVEL_FLAG_POS) & INT64_C(1);
+	return get_bplus_tree_interior_page_header(page, bpttd_p).is_last_page_of_level;
 }
 
-void set_is_last_page_of_level_of_bplus_tree_interior_page(void* page, int is_last_page_of_level, const bplus_tree_tuple_defs* bpttd_p)
-{
-	void* leaf_page_header = get_page_header((void*)page, bpttd_p->page_size) + get_offset_of_bplus_tree_interior_page_header(bpttd_p);
-	void* flags = leaf_page_header + bpttd_p->page_id_width;
-	int64_t flags_val = read_int64(flags, FLAGS_BYTE_SIZE);
-	if(is_last_page_of_level)
-		flags_val |= (INT64_C(1) << IS_LAST_PAGE_OF_LEVEL_FLAG_POS);
-	else
-		flags_val &= (~(INT64_C(1) << IS_LAST_PAGE_OF_LEVEL_FLAG_POS));
-	return write_int64(flags, FLAGS_BYTE_SIZE, flags_val);
-}
+bplus_tree_interior_page_header get_bplus_tree_interior_page_header(const void* page, const bplus_tree_tuple_defs* bpttd_p);
+
+void serialize_bplus_tree_interior_page_header(void* hdr_serial, const bplus_tree_interior_page_header* bptlph_p, const bplus_tree_tuple_defs* bpttd_p);
+
+void set_bplus_tree_interior_page_header(persistent_page ppage, const bplus_tree_interior_page_header* bptlph_p, const bplus_tree_tuple_defs* bpttd_p, const page_modification_methods* pmm_p);
 
 #include<stdio.h>
 
