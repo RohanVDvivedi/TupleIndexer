549368a6f5dd609e6d395fd992781483c0978470
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-05 17:34:58 -0700

    implemented persistent page unaltered to resiliently append or update a tuple

---
+++ inc/persistent_page_altered_util.h
@@ -0,0 +1,12 @@
+#ifndef PERSISTENT_PAGE_UNALTERED_UTIL_H
+#define PERSISTENT_PAGE_UNALTERED_UTIL_H
+
+#include<persistent_page.h>
+
+#include<opaque_page_modification_methods.h>
+
+int append_tuple_on_persistent_page_resiliently(const page_modification_methods* pmm_p, persistent_page* ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d, const void* external_tuple);
+
+int update_tuple_on_persistent_page_resiliently(const page_modification_methods* pmm_p, persistent_page* ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d, uint32_t index, const void* external_tuple);
+
+#endif
