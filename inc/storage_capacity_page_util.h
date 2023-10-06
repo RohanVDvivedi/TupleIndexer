3788c488232367a82f41c7dc061cdcbfa0fd3e70
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-05 18:04:24 -0700

    refactored storage capacity page util to use persistent page

--- inc/storage_capacity_page_util.h
+++ inc/storage_capacity_page_util.h
@@ -1,23 +1,24 @@
 #ifndef STORAGE_CAPACITY_PAGE_UTIL_H
 #define STORAGE_CAPACITY_PAGE_UTIL_H
 
+#include<persistent_page_functions.h>
 #include<tuple_def.h>
 
 // The below functions only read the page provided and must not and will not modify the page
 
-int is_page_lesser_than_half_full(const void* page, uint32_t page_size, const tuple_def* def);
+int is_page_lesser_than_half_full(const persistent_page* ppage, uint32_t page_size, const tuple_def* def);
 
-int is_page_lesser_than_or_equal_to_half_full(const void* page, uint32_t page_size, const tuple_def* def);
+int is_page_lesser_than_or_equal_to_half_full(const persistent_page* ppage, uint32_t page_size, const tuple_def* def);
 
-int is_page_more_than_or_equal_to_half_full(const void* page, uint32_t page_size, const tuple_def* def);
+int is_page_more_than_or_equal_to_half_full(const persistent_page* ppage, uint32_t page_size, const tuple_def* def);
 
-int is_page_more_than_half_full(const void* page, uint32_t page_size, const tuple_def* def);
+int is_page_more_than_half_full(const persistent_page* ppage, uint32_t page_size, const tuple_def* def);
 
 // method only valid for bplus tree and variants
 // returns 1 -> MAY require split
 // returns 0 -> will SURELY not require any split
 // it may give false positive for splitting
-int may_require_split_for_insert_for_bplus_tree(const void* page, uint32_t page_size, const tuple_def* def);
+int may_require_split_for_insert_for_bplus_tree(const persistent_page* ppage, uint32_t page_size, const tuple_def* def);
 
 // method only valid for bplus tree and variants
 // returns 1 -> MAY require merge or redistribution
@@ -25,6 +26,6 @@ int may_require_split_for_insert_for_bplus_tree(const void* page, uint32_t page_
 // it may give false positive for merging or redistribution
 // child_index is the index in the page, that we would be following
 // child_index = -1 when you are following the least_keys_page_id
-int may_require_merge_or_redistribution_for_delete_for_bplus_tree_interior_page(const void* page, uint32_t page_size, const tuple_def* index_def, uint32_t child_index);
+int may_require_merge_or_redistribution_for_delete_for_bplus_tree_interior_page(const persistent_page* ppage, uint32_t page_size, const tuple_def* index_def, uint32_t child_index);
 
 #endif
--- src/storage_capacity_page_util.c
+++ src/storage_capacity_page_util.c
@@ -1,40 +1,39 @@
 #include<storage_capacity_page_util.h>
 
-#include<page_layout_unaltered.h>
 #include<tuple.h>
 
-int is_page_lesser_than_half_full(const void* page, uint32_t page_size, const tuple_def* def)
+int is_page_lesser_than_half_full(const persistent_page* ppage, uint32_t page_size, const tuple_def* def)
 {
-	uint32_t allotted_space = get_space_allotted_to_all_tuples_on_page(page, page_size, &(def->size_def));
-	uint32_t used_space = get_space_occupied_by_all_tuples_on_page(page, page_size, &(def->size_def));
+	uint32_t allotted_space = get_space_allotted_to_all_tuples_on_persistent_page(ppage, page_size, &(def->size_def));
+	uint32_t used_space = get_space_occupied_by_all_tuples_on_persistent_page(ppage, page_size, &(def->size_def));
 	return used_space < (allotted_space / 2);
 }
 
-int is_page_lesser_than_or_equal_to_half_full(const void* page, uint32_t page_size, const tuple_def* def)
+int is_page_lesser_than_or_equal_to_half_full(const persistent_page* ppage, uint32_t page_size, const tuple_def* def)
 {
-	uint32_t allotted_space = get_space_allotted_to_all_tuples_on_page(page, page_size, &(def->size_def));
-	uint32_t used_space = get_space_occupied_by_all_tuples_on_page(page, page_size, &(def->size_def));
+	uint32_t allotted_space = get_space_allotted_to_all_tuples_on_persistent_page(ppage, page_size, &(def->size_def));
+	uint32_t used_space = get_space_occupied_by_all_tuples_on_persistent_page(ppage, page_size, &(def->size_def));
 	return used_space <= (allotted_space / 2);
 }
 
-int is_page_more_than_or_equal_to_half_full(const void* page, uint32_t page_size, const tuple_def* def)
+int is_page_more_than_or_equal_to_half_full(const persistent_page* ppage, uint32_t page_size, const tuple_def* def)
 {
-	uint32_t allotted_space = get_space_allotted_to_all_tuples_on_page(page, page_size, &(def->size_def));
-	uint32_t used_space = get_space_occupied_by_all_tuples_on_page(page, page_size, &(def->size_def));
+	uint32_t allotted_space = get_space_allotted_to_all_tuples_on_persistent_page(ppage, page_size, &(def->size_def));
+	uint32_t used_space = get_space_occupied_by_all_tuples_on_persistent_page(ppage, page_size, &(def->size_def));
 	return used_space >= (allotted_space / 2);
 }
 
-int is_page_more_than_half_full(const void* page, uint32_t page_size, const tuple_def* def)
+int is_page_more_than_half_full(const persistent_page* ppage, uint32_t page_size, const tuple_def* def)
 {
-	uint32_t allotted_space = get_space_allotted_to_all_tuples_on_page(page, page_size, &(def->size_def));
-	uint32_t used_space = get_space_occupied_by_all_tuples_on_page(page, page_size, &(def->size_def));
+	uint32_t allotted_space = get_space_allotted_to_all_tuples_on_persistent_page(ppage, page_size, &(def->size_def));
+	uint32_t used_space = get_space_occupied_by_all_tuples_on_persistent_page(ppage, page_size, &(def->size_def));
 	return used_space > (allotted_space / 2);
 }
 
-int may_require_split_for_insert_for_bplus_tree(const void* page, uint32_t page_size, const tuple_def* def)
+int may_require_split_for_insert_for_bplus_tree(const persistent_page* ppage, uint32_t page_size, const tuple_def* def)
 {
-	uint32_t allotted_space = get_space_allotted_to_all_tuples_on_page(page, page_size, &(def->size_def));
-	uint32_t used_space = get_space_occupied_by_all_tuples_on_page(page, page_size, &(def->size_def));
+	uint32_t allotted_space = get_space_allotted_to_all_tuples_on_persistent_page(ppage, page_size, &(def->size_def));
+	uint32_t used_space = get_space_occupied_by_all_tuples_on_persistent_page(ppage, page_size, &(def->size_def));
 
 	// the space that will become free memory -> after defragmentation and discarding tomb stones (ther should be no tomb stones, though)
 	uint32_t available_space_after_compaction = allotted_space - used_space;
@@ -52,23 +51,23 @@ int may_require_split_for_insert_for_bplus_tree(const void* page, uint32_t page_
 	}
 }
 
-int may_require_merge_or_redistribution_for_delete_for_bplus_tree_interior_page(const void* page, uint32_t page_size, const tuple_def* index_def, uint32_t child_index)
+int may_require_merge_or_redistribution_for_delete_for_bplus_tree_interior_page(const persistent_page* ppage, uint32_t page_size, const tuple_def* index_def, uint32_t child_index)
 {
-	uint32_t allotted_space = get_space_allotted_to_all_tuples_on_page(page, page_size, &(index_def->size_def));
-	uint32_t used_space = get_space_occupied_by_all_tuples_on_page(page, page_size, &(index_def->size_def));
+	uint32_t allotted_space = get_space_allotted_to_all_tuples_on_persistent_page(ppage, page_size, &(index_def->size_def));
+	uint32_t used_space = get_space_occupied_by_all_tuples_on_persistent_page(ppage, page_size, &(index_def->size_def));
 
 	// if the page is already lesser than half full, then it may require merging or redistribution
 	if(used_space <= (allotted_space / 2))
 		return 1;
 
-	uint32_t tuple_count = get_tuple_count_on_page(page, page_size, &(index_def->size_def));
+	uint32_t tuple_count = get_tuple_count_on_persistent_page(ppage, page_size, &(index_def->size_def));
 
 	// if we would require deleting the index_entry at child_index
 	// i.e. if your child merges with the previous
 	if(child_index < tuple_count)
 	{
 		// find new used space after deleting the tuple at the child_index (and after full compaction and tomb stone removal)
-		uint32_t new_used_space = used_space - get_space_occupied_by_tuples_on_page(page, page_size, &(index_def->size_def), child_index, child_index);
+		uint32_t new_used_space = used_space - get_space_occupied_by_tuples_on_persistent_page(ppage, page_size, &(index_def->size_def), child_index, child_index);
 
 		// if the new used space is lesser than half full then it may required merging/redistribution
 		if(new_used_space <= (allotted_space / 2))
@@ -80,7 +79,7 @@ int may_require_merge_or_redistribution_for_delete_for_bplus_tree_interior_page(
 	if(child_index + 1 < tuple_count)
 	{
 		// find new used space after deleting the tuple at the child_index (and after full compaction and tomb stone removal)
-		uint32_t new_used_space = used_space - get_space_occupied_by_tuples_on_page(page, page_size, &(index_def->size_def), child_index + 1, child_index + 1);
+		uint32_t new_used_space = used_space - get_space_occupied_by_tuples_on_persistent_page(ppage, page_size, &(index_def->size_def), child_index + 1, child_index + 1);
 
 		// if the new used space is lesser than half full then it may required merging/redistribution
 		if(new_used_space <= (allotted_space / 2))
