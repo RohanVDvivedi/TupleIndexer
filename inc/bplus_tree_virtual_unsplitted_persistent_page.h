324b31cd037001af196298d1269b0549e1789880
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-14 17:27:58 -0700

    minor description in the comments

--- inc/bplus_tree_virtual_unsplitted_persistent_page.h
+++ inc/bplus_tree_virtual_unsplitted_persistent_page.h
@@ -24,22 +24,27 @@ struct virtual_unsplitted_persistent_page
 	uint32_t insertion_index;
 
 	// the tuple_definition to use for the ppage, and the tuple_to_insert
+	// bpttd->record_def for leaf pages and bpttd_p->index_def for interior pages
 	const tuple_def* tpl_d;
 
 	// above attributes are for get_nth_tuple, get_space_occupied_by_tuples and get_tuple_count for the persistent_page
 	// below attributes along with the tpl_d are used for the contexted_comparator, to compare tuples on the page
 
 	// element_ids of the tpl_d, that are key to this bplus_tree's (sorted_packed_page) page
+	// must be same as in bpttd_p->key_element_ids for leaf pages, and NULL for interior pages
 	const uint32_t* key_element_ids;
 
 	// the sort direction of the keys
+	// same as bpttd_p->key_compare_direction
 	const compare_direction* key_compare_direction;
 
-	// number of elements in the key_element_ids
+	// number of elements in the key_element_ids and key_compare_direction
+	// must be same as bpttd_p->key_element_count
 	uint32_t key_element_count;
 };
 
-virtual_unsplitted_persistent_page get_virtual_unsplitted_persistent_page(const persistent_page* ppage, const void* tuple_to_insert, uint32_t insertion_index, const tuple_def* tpl_d, const uint32_t* key_element_ids, const compare_direction* key_compare_direction, uint32_t key_element_count);
+// The insertion_point will be calculated and returned if it equals NO_TUPLE_FOUND
+virtual_unsplitted_persistent_page get_virtual_unsplitted_persistent_page(const persistent_page* ppage, const void* tuple_to_insert, uint32_t* insertion_index, const tuple_def* tpl_d, const uint32_t* key_element_ids, const compare_direction* key_compare_direction, uint32_t key_element_count);
 
 uint32_t get_tuple_count_on_virtual_unsplitted_persistent_page(const virtual_unsplitted_persistent_page* vupp_p);
 
