0b203270e67f1d6bd9a72625199459471268fb1b
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-17 00:23:16 -0700

    major locking bug fixes

--- src/bplus_tree_update.c
+++ src/bplus_tree_update.c
@@ -8,6 +8,8 @@
 #include<sorted_packed_page_util.h>
 #include<storage_capacity_page_util.h>
 
+#include<stdio.h>
+
 static int walk_down_and_split_insert_util(uint64_t root_page_id, persistent_page* concerned_leaf, const void* new_record, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p, const page_modification_methods* pmm_p)
 {
 	// create a stack of capacity = levels
@@ -50,30 +52,27 @@ static int walk_down_and_split_insert_util(uint64_t root_page_id, persistent_pag
 // root_pages can not merge
 static int walk_down_and_merge_util(uint64_t root_page_id, persistent_page* concerned_leaf, const void* old_record, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p, const page_modification_methods* pmm_p)
 {
-	if(concerned_leaf->page_id != root_page_id && is_page_lesser_than_or_equal_to_half_full(concerned_leaf, bpttd_p->page_size, bpttd_p->record_def))
+	// create a stack of capacity = levels
+	locked_pages_stack* locked_pages_stack_p = &((locked_pages_stack){});
+	uint32_t root_page_level;
+
 	{
+		// get lock on the root page of the bplus_tree
+		persistent_page root_page = acquire_persistent_page_with_lock(dam_p, root_page_id, WRITE_LOCK);
+
+		// pre cache level of the root_page
+		root_page_level = get_level_of_bplus_tree_page(&root_page, bpttd_p);
+
 		// create a stack of capacity = levels
-		locked_pages_stack* locked_pages_stack_p = &((locked_pages_stack){});
-		uint32_t root_page_level;
+		initialize_locked_pages_stack(locked_pages_stack_p, root_page_level + 1);
 
-		{
-			// get lock on the root page of the bplus_tree
-			persistent_page root_page = acquire_persistent_page_with_lock(dam_p, root_page_id, WRITE_LOCK);
-
-			// pre cache level of the root_page
-			root_page_level = get_level_of_bplus_tree_page(&root_page, bpttd_p);
-
-			// create a stack of capacity = levels
-			initialize_locked_pages_stack(locked_pages_stack_p, root_page_level + 1);
-
-			// push the root page onto the stack
-			push_to_locked_pages_stack(locked_pages_stack_p, &INIT_LOCKED_PAGE_INFO(root_page));
-		}
-
-		walk_down_locking_parent_pages_for_merge_using_record(root_page_id, 1, locked_pages_stack_p, old_record, bpttd_p, dam_p);
-
-		merge_and_unlock_pages_up(root_page_id, locked_pages_stack_p, bpttd_p, dam_p, pmm_p);
+		// push the root page onto the stack
+		push_to_locked_pages_stack(locked_pages_stack_p, &INIT_LOCKED_PAGE_INFO(root_page));
 	}
+
+	walk_down_locking_parent_pages_for_merge_using_record(root_page_id, 1, locked_pages_stack_p, old_record, bpttd_p, dam_p);
+
+	merge_and_unlock_pages_up(root_page_id, locked_pages_stack_p, bpttd_p, dam_p, pmm_p);
 
 	return 1;
 }
@@ -160,8 +159,10 @@ int inspected_update_in_bplus_tree(uint64_t root_page_id, void* new_record, cons
 						);
 
 		// we need to merge it, if it is not root and is lesser than half full
-		if(deleted)
+		if(deleted && concerned_leaf.page_id != root_page_id && is_page_lesser_than_or_equal_to_half_full(&concerned_leaf, bpttd_p->page_size, bpttd_p->record_def))
 			walk_down_and_merge_util(root_page_id, &concerned_leaf, old_record, bpttd_p, dam_p, pmm_p);
+		else
+			release_lock_on_persistent_page(dam_p, &concerned_leaf, NONE_OPTION);
 
 		return deleted;
 	}
@@ -199,15 +200,22 @@ int inspected_update_in_bplus_tree(uint64_t root_page_id, void* new_record, cons
 
 					updated = walk_down_and_split_insert_util(root_page_id, &concerned_leaf, new_record, bpttd_p, dam_p, pmm_p);
 				}
+				else
+					release_lock_on_persistent_page(dam_p, &concerned_leaf, NONE_OPTION);
 			}
 			else // new_record_size < old_record_size -> may require merge
 			{
-				if(updated) // this must be true here, how can small record can't fit in a bigger one's space
+				// this must be true here, how can small record can't fit in a bigger one's space
+				if(updated && concerned_leaf.page_id != root_page_id && is_page_lesser_than_or_equal_to_half_full(&concerned_leaf, bpttd_p->page_size, bpttd_p->record_def))
 				{
 					walk_down_and_merge_util(root_page_id, &concerned_leaf, old_record, bpttd_p, dam_p, pmm_p);
 				}
+				else
+					release_lock_on_persistent_page(dam_p, &concerned_leaf, NONE_OPTION);
 			}
 		}
+		else
+			release_lock_on_persistent_page(dam_p, &concerned_leaf, NONE_OPTION);
 
 		return updated;
 	}
