54b57d658ea2e806b83518d4e2596189f059ed30
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-16 23:11:08 -0700

    implemented delete case

--- src/bplus_tree_update.c
+++ src/bplus_tree_update.c
@@ -6,6 +6,7 @@
 #include<bplus_tree_page_header.h>
 #include<persistent_page_functions.h>
 #include<sorted_packed_page_util.h>
+#include<storage_capacity_page_util.h>
 
 int inspected_update_in_bplus_tree(uint64_t root_page_id, void* new_record, const update_inspector* ui_p, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p, const page_modification_methods* pmm_p)
 {
@@ -112,7 +113,41 @@ int inspected_update_in_bplus_tree(uint64_t root_page_id, void* new_record, cons
 	}
 	else if(old_record != NULL && new_record == NULL) // delete case
 	{
+		// perform a delete operation on the found index in this page
+		int deleted = delete_in_sorted_packed_page(
+							&(concerned_leaf), bpttd_p->page_size,
+							bpttd_p->record_def,
+							found_index,
+							pmm_p
+						);
 
+		// we need to merge it, if it is not root and is lesser than half full
+		if(deleted && concerned_leaf.page_id != root_page_id && is_page_lesser_than_or_equal_to_half_full(&concerned_leaf, bpttd_p->page_size, bpttd_p->record_def))
+		{
+			// create a stack of capacity = levels
+			locked_pages_stack* locked_pages_stack_p = &((locked_pages_stack){});
+			uint32_t root_page_level;
+
+			{
+				// get lock on the root page of the bplus_tree
+				persistent_page root_page = acquire_persistent_page_with_lock(dam_p, root_page_id, WRITE_LOCK);
+
+				// pre cache level of the root_page
+				root_page_level = get_level_of_bplus_tree_page(&root_page, bpttd_p);
+
+				// create a stack of capacity = levels
+				initialize_locked_pages_stack(locked_pages_stack_p, root_page_level + 1);
+
+				// push the root page onto the stack
+				push_to_locked_pages_stack(locked_pages_stack_p, &INIT_LOCKED_PAGE_INFO(root_page));
+			}
+
+			walk_down_locking_parent_pages_for_merge_using_record(root_page_id, 1, locked_pages_stack_p, old_record, bpttd_p, dam_p);
+
+			merge_and_unlock_pages_up(root_page_id, locked_pages_stack_p, bpttd_p, dam_p, pmm_p);
+		}
+
+		return deleted;
 	}
 	else // update
 	{
