74ce5dbdd42173fe30eca479e7129f0902c78c9f
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-16 21:50:32 -0700

    making utility methods more and more customizatable for supporting updates

--- inc/bplus_tree_merge_util.h
+++ inc/bplus_tree_merge_util.h
@@ -7,10 +7,14 @@
 #include<opaque_page_modification_methods.h>
 
 // you must lock the root page, in the locked_pages_stack_p, before calling this function
-int walk_down_locking_parent_pages_for_merge_using_key(uint64_t root_page_id, locked_pages_stack* locked_pages_stack_p, const void* key, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p);
+// locks will be taken until the parameter until_level
+// you will have the page at until_level in your locked_pages_stack upon successfull return from this function
+int walk_down_locking_parent_pages_for_merge_using_key(uint64_t root_page_id, uint32_t until_level, locked_pages_stack* locked_pages_stack_p, const void* key, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p);
 
 // you must lock the root page, in the locked_pages_stack_p, before calling this function
-int walk_down_locking_parent_pages_for_merge_using_record(uint64_t root_page_id, locked_pages_stack* locked_pages_stack_p, const void* record, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p);
+// locks will be taken until the parameter until_level
+// you will have the page at until_level in your locked_pages_stack upon successfull return from this function
+int walk_down_locking_parent_pages_for_merge_using_record(uint64_t root_page_id, uint32_t until_level, locked_pages_stack* locked_pages_stack_p, const void* record, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p);
 
 // the locked_pages_stack may contain only the pages in the chain that will participate in the merge
 // i.e. the root_page may not be in the locked pages stack
