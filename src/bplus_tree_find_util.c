19519b4a4398c37929a5444d03bae4104b44de26
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-16 22:00:08 -0700

    started with implementation

---
+++ src/bplus_tree_find_util.c
@@ -0,0 +1,11 @@
+#include<bplus_tree_find_util.h>
+
+int read_couple_locks_until_leaf_using_key(uint64_t root_page_id, const void* key, int lock_type, persistent_page* locked_leaf, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p)
+{
+	// TODO
+}
+
+int read_couple_locks_until_leaf_using_record(uint64_t root_page_id, const void* record, int lock_type, persistent_page* locked_leaf, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p)
+{
+	// TODO
+}
