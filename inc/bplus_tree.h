97410432b4c070911620718f8e5eb24bda981e20
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-16 19:16:33 -0700

    added declaration for inspected_update

--- inc/bplus_tree.h
+++ inc/bplus_tree.h
@@ -29,6 +29,24 @@ enum find_position
 // the key == NULL and find_pos == LESSER_THAN, then the iterator will point to the last tuple of the bplus_tree
 bplus_tree_iterator* find_in_bplus_tree(uint64_t root_page_id, const void* key, uint32_t key_element_count_concerned, find_position find_pos, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p);
 
+typedef struct update_inspector update_inspector;
+struct update_inspector
+{
+	const void* context;
+
+	// the operation for an update will be performed only if the update_inspector returns 1, 0 implies no operation
+	// new_record can be modified by this function, (but mind well to keep all the key elements of the record un touched)
+	// upon the return of 1 from this function
+	// if(old_record == NULL && *new_record == NULL) -> do nothing
+	// else if(old_record != NULL && *new_record == NULL) -> do delete of the old_record
+	// else if(old_record == NULL && *new_record != NULL) -> do insert for the new_record
+	// else if(old_record != NULL && *new_record != NULL) -> do update old_record with the new_record
+	int update_inspect(const void* context, const tuple_def* record_def, const void* old_record, void** new_record);
+};
+
+// to find and read a record, then inspect it with the ui_p, and then proceed to update it
+int inspected_update_in_bplus_tree(uint64_t root_page_id, const void* new_record, const update_inspector* ui_p, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p, const page_modification_methods* pmm_p);
+
 // insert record in bplus_tree
 int insert_in_bplus_tree(uint64_t root_page_id, const void* record, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p, const page_modification_methods* pmm_p);
 
