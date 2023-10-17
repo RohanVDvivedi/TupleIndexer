acbc4d2bdf66db60712d1a397fc9a5c2b1295bfb
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-16 20:56:11 -0700

    moving merge_and_unlock_pages to merge util

--- inc/bplus_tree_merge_util.h
+++ inc/bplus_tree_merge_util.h
@@ -12,4 +12,9 @@ int walk_down_locking_parent_pages_for_merge_using_key(uint64_t root_page_id, lo
 // you must lock the root page, in the locked_pages_stack_p, before calling this function
 int walk_down_locking_parent_pages_for_merge_using_record(uint64_t root_page_id, locked_pages_stack* locked_pages_stack_p, const void* record, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p);
 
+// the locked_pages_stack may contain only the pages in the chain that will participate in the merge
+// i.e. the root_page may not be in the locked pages stack
+// all the locks are released by this function, even on a failure
+int merge_and_unlock_pages_up(uint64_t root_page_id, locked_pages_stack* locked_pages_stack_p, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p, const page_modification_methods* pmm_p);
+
 #endif
