3fea8c99c50dbbc69cc6a86c93a48e720448a116
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-16 20:33:22 -0700

    added a record version of the walk_down_ formerge that will be used with record

--- inc/bplus_tree_merge_util.h
+++ inc/bplus_tree_merge_util.h
@@ -9,4 +9,7 @@
 // you must lock the root page, in the locked_pages_stack_p, before calling this function
 int walk_down_locking_parent_pages_for_merge_using_key(uint64_t root_page_id, locked_pages_stack* locked_pages_stack_p, const void* key, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p);
 
+// you must lock the root page, in the locked_pages_stack_p, before calling this function
+int walk_down_locking_parent_pages_for_merge_using_record(uint64_t root_page_id, locked_pages_stack* locked_pages_stack_p, const void* record, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p);
+
 #endif
