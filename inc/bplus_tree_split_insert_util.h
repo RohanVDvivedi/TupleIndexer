020f144f2e43fb581f4b6ba704eeba4a5563fb3b
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-16 20:16:10 -0700

    added comments for the usage of the bplus_tree_split_insert_util

--- inc/bplus_tree_split_insert_util.h
+++ inc/bplus_tree_split_insert_util.h
@@ -6,8 +6,12 @@
 #include<opaque_data_access_methods.h>
 #include<opaque_page_modification_methods.h>
 
+// you must lock the root page, in the locked_pages_stack_p, before calling this function
 int walk_down_locking_parent_pages_for_split_insert(uint64_t root_page_id, locked_pages_stack* locked_pages_stack_p, const void* record, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p);
 
+// the locked_pages_stack may contain only the pages in the chain that will participate in the split insert
+// i.e. the root_page may not be in the locked pages stack
+// all the locks are released by this function, even on a failure
 int split_insert_unlock_pages_up(uint64_t root_page_id, locked_pages_stack* locked_pages_stack_p, const void* record, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p, const page_modification_methods* pmm_p);
 
 #endif
