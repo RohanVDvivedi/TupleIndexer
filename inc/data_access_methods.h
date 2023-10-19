de24a009d5a0ef01fe75a825c58740a02ba40792
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-19 15:50:40 -0700

    transaction id is required by all the pmm and dam methods

--- inc/data_access_methods.h
+++ inc/data_access_methods.h
@@ -12,6 +12,8 @@
 **  0 or NULL impiles an error/a failure with the corresponding operation
 **	a failure to data_access_methods function calls must accompany with an error
 **	this the same error that transaction abort manager will be marked aborted with
+**	any error returned, is expected to abort the transaction
+**	In case of a failure to allocate more memeory for your needs, do not through an error, just call exit() and kill the application
 */
 
 // below are the options that can go with the functions below
@@ -29,19 +31,19 @@ struct data_access_methods
 
 	// a request method to get a new blank page from the page manager with write lock on the page
 	// the page_id_returned is set with the page_id of the new_page
-	void* (*get_new_page_with_write_lock)(void* context, uint64_t* page_id_returned, int* error);
+	void* (*get_new_page_with_write_lock)(void* context, const void* transaction_id, uint64_t* page_id_returned, int* error);
 
 	// locks a page for read or write, if successfull must return pointer to the in-memory of the page
-	void* (*acquire_page_with_reader_lock)(void* context, uint64_t page_id, int* error);
-	void* (*acquire_page_with_writer_lock)(void* context, uint64_t page_id, int* error);
+	void* (*acquire_page_with_reader_lock)(void* context, const void* transaction_id, uint64_t page_id, int* error);
+	void* (*acquire_page_with_writer_lock)(void* context, const void* transaction_id, uint64_t page_id, int* error);
 
 	// downgrade a writer lock to a reader lock
-	int (*downgrade_writer_lock_to_reader_lock_on_page)(void* context, void* pg_ptr, int opts, int* error); // acceptable options : WAS_MODIFIED and FORCE_FLUSH
-	int (*upgrade_reader_lock_to_writer_lock_on_page)(void* context, void* pg_ptr, int* error);
+	int (*downgrade_writer_lock_to_reader_lock_on_page)(void* context, const void* transaction_id, void* pg_ptr, int opts, int* error); // acceptable options : WAS_MODIFIED and FORCE_FLUSH
+	int (*upgrade_reader_lock_to_writer_lock_on_page)(void* context, const void* transaction_id, void* pg_ptr, int* error);
 
 	// releases lock on the page, accordingly, free_page flag will free the page, after releasing the lock
-	int (*release_reader_lock_on_page)(void* context, void* pg_ptr, int opts, int* error); // acceptable options : FREE_PAGE
-	int (*release_writer_lock_on_page)(void* context, void* pg_ptr, int opts, int* error); // acceptable options : WAS_MODIFIED, FORCE_FLUSH and FREE_PAGE
+	int (*release_reader_lock_on_page)(void* context, const void* transaction_id, void* pg_ptr, int opts, int* error); // acceptable options : FREE_PAGE
+	int (*release_writer_lock_on_page)(void* context, const void* transaction_id, void* pg_ptr, int opts, int* error); // acceptable options : WAS_MODIFIED, FORCE_FLUSH and FREE_PAGE
 
 	// the was_modified parameter suggests that the page that we had a writer lock on was modified,
 	// and any disk based system is suppossed to mark this page dirty now and possibly persist this new version of page to disk,
@@ -49,7 +51,7 @@ struct data_access_methods
 
 	// make a page free, you may call this function, even if you don't have lock on the page
 	// fails only if the page is already free
-	int (*free_page)(void* context, uint64_t page_id, int* error);
+	int (*free_page)(void* context, const void* transaction_id, uint64_t page_id, int* error);
 
 	int (*close_data_file)(void* context);
 
--- inc/page_modification_methods.h
+++ inc/page_modification_methods.h
@@ -28,28 +28,28 @@
 typedef struct page_modification_methods page_modification_methods;
 struct page_modification_methods
 {
-	int (*init_page)(void* context, persistent_page ppage, uint32_t page_size, uint32_t page_header_size, const tuple_size_def* tpl_sz_d);
+	int (*init_page)(void* context, const void* transaction_id, persistent_page ppage, uint32_t page_size, uint32_t page_header_size, const tuple_size_def* tpl_sz_d);
 
-	void (*set_page_header)(void* context, persistent_page ppage, uint32_t page_size, const void* hdr);
+	void (*set_page_header)(void* context, const void* transaction_id, persistent_page ppage, uint32_t page_size, const void* hdr);
 
-	int (*append_tuple_on_page)(void* context, persistent_page ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d, const void* external_tuple);
+	int (*append_tuple_on_page)(void* context, const void* transaction_id, persistent_page ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d, const void* external_tuple);
 
-	int (*update_tuple_on_page)(void* context, persistent_page ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d, uint32_t index, const void* external_tuple);
+	int (*update_tuple_on_page)(void* context, const void* transaction_id, persistent_page ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d, uint32_t index, const void* external_tuple);
 
-	int (*discard_tuple_on_page)(void* context, persistent_page ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d, uint32_t index);
+	int (*discard_tuple_on_page)(void* context, const void* transaction_id, persistent_page ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d, uint32_t index);
 
-	void (*discard_all_tuples_on_page)(void* context, persistent_page ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d);
+	void (*discard_all_tuples_on_page)(void* context, const void* transaction_id, persistent_page ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d);
 
-	uint32_t (*discard_trailing_tomb_stones_on_page)(void* context, persistent_page ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d);
+	uint32_t (*discard_trailing_tomb_stones_on_page)(void* context, const void* transaction_id, persistent_page ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d);
 
-	int (*swap_tuples_on_page)(void* context, persistent_page ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d, uint32_t i1, uint32_t i2);
+	int (*swap_tuples_on_page)(void* context, const void* transaction_id, persistent_page ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d, uint32_t i1, uint32_t i2);
 
-	int (*set_element_in_tuple_in_place_on_page)(void* context, persistent_page ppage, uint32_t page_size, const tuple_def* tpl_d, uint32_t tuple_index, uint32_t element_index, const user_value* value);
+	int (*set_element_in_tuple_in_place_on_page)(void* context, const void* transaction_id, persistent_page ppage, uint32_t page_size, const tuple_def* tpl_d, uint32_t tuple_index, uint32_t element_index, const user_value* value);
 
-	void (*clone_page)(void* context, persistent_page ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d, persistent_page ppage_src);
+	void (*clone_page)(void* context, const void* transaction_id, persistent_page ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d, persistent_page ppage_src);
 
 	// the below function, does not change contents of the page, hence a physiologic log entry for this operation is not required in most settings
-	int (*run_page_compaction)(void* context, persistent_page ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d);
+	int (*run_page_compaction)(void* context, const void* transaction_id, persistent_page ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d);
 
 	void* context;
 };
