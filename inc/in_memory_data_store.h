333c32757c28f3ee6beb7dcd4e0e8e1139063295
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-19 16:15:34 -0700

    added comments on you to utilize in_memory_data_store

--- inc/in_memory_data_store.h
+++ inc/in_memory_data_store.h
@@ -3,6 +3,13 @@
 
 #include<data_access_methods.h>
 
+/*
+**	Constructs you a simple in memory, unWALed data store
+**	it either return success or fail with error set to 1
+**	it must be used for testing purposes only
+**	it will not WAL log your page allocations and deallocations, but it is concurrently accessible
+*/
+
 data_access_methods* get_new_in_memory_data_store(uint32_t page_size, uint8_t page_id_width);
 
 int close_and_destroy_in_memory_data_store(data_access_methods* dam_p);
