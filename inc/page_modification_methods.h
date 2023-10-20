f1631911af20915fd1168686f3e1e6fb5bde2c43
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-20 00:23:27 -0700

    added comments to show how page_modification_methods must be implemented

--- inc/page_modification_methods.h
+++ inc/page_modification_methods.h
@@ -7,26 +7,28 @@
 #include<persistent_page.h>
 
 /*
-** This functions act as a middleware allowing you to intercept the functions that modify the page using TupleStore's methods
-** It allows you to Write Ahead Log the operations while modifying the page
-** Note :: They must be backed by the corresponding functions from TupleStore ONLY
-** All the functions here take in persistent_page by value, this is a design choice, so as I not allow the implementer of page_modification_methods to modify the persistent_page
-** You may or may not choose to WAL log the operation, its up to you, the persistent_page given to these functions will always be .is_write_locked = 1, this library will ensure that
-*/
-
-// All the functions of page_modification_methods are analogous to their corresponding counterparts in page_layout.h
-
-/*
+**	This functions act as a middleware allowing you to intercept the functions that modify the page using TupleStore's methods
+**	They should be used to Write Ahead Log the operations while modifying the page
+**	NOTE :: They must be backed by the corresponding functions from TupleStore ONLY
+**
+**	All the functions here take in persistent_page by value, this is a design choice, so as I not allow the implementer of page_modification_methods to modify the persistent_page
+**	You may or may not choose to WAL log the operation, its up to you, the persistent_page given to these functions will always be .is_write_locked = 1, this library will ensure that
+**
+**	All the functions of page_modification_methods are analogous to their corresponding counterparts in page_layout.h
+**
 **	These functions either modify the page OR they dont, It is never expected of them, to give an error other than abort_error
 **	In case, when a transaction for some reason is not able to put a WAL log entry for the corresponding operation on the page, I want you to kill the application using exit().
 **	A database is not a database at all without its WAL, even if you fail to allocate memory to create a WAL record, I want you to exit()
 **	These methods have the sole purpose to intercept the page modification and WAL (Write Ahead Log) it.
 **
-**	A WAL record should not we logged, if a transaction is aborted, in such a case you may return with an abort_error set to the reason of the abort
+**	A WAL record should not be logged, if a transaction is found to be already aborted, in such a case you may return with an abort_error set to the reason of the abort
 **	An abort error can not be 1. failure to allocate memory, OR 2. failure to WAL log -> in those cases you must exit()
 **
-**	Your design may include a shared lock on the transaction's is_aborted bit and its reason for abort, allowing multiple threads to ensure that the transaction is not aborted while you write a log record for one of its changes
-**	And take an exclusive lock to mark it aborted
+**	Your design may include a shared/exclusive lock on the transaction's is_aborted bit and its reason for abort,
+**	allowing multiple threads to get shared lock to ensure that the transaction is not aborted, while they write a log record for one of its changes
+**	And take an exclusive lock to mark it aborted, which these functions may never do
+**
+**	Such a design ensures that no WAL records are inserted into WAL, after a transaction is aborted or commited
 **
 **	As discussed above, the following functions may just ensure that the transaction is not aborted, AND never mark it aborted
 */
