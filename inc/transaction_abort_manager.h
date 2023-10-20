ae7a88b6c8314213cddb18a1b1a7ab5e365c23d6
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-19 19:14:11 -0700

    transaction abort manager takes in an abort error not a reason

--- inc/transaction_abort_manager.h
+++ inc/transaction_abort_manager.h
@@ -1,6 +1,13 @@
 #ifndef TRANSACTION_ABORT_MANAGER_H
 #define TRANSACTION_ABORT_MANAGER_H
 
+/*
+**	Design guidelines
+**	You must protect the transaction's is_abort flag and its reason for abort in a shared/exclusive lock
+**	allowing multiple threads to grab shared lock, read and ensure that the transaction is not aborted, while they work on something
+**	while let only 1 thread to mark a transaction as aborted.
+*/ 
+
 typedef struct transaction_abort_manager_methods transaction_abort_manager_methods;
 struct transaction_abort_manager_methods
 {
@@ -10,11 +17,11 @@ struct transaction_abort_manager_methods
 	// this method must be thread safe, there could be a transaction, using multiple threads
 	int (*is_aborted)(const void* transaction_id, const void* context);
 
-	// abort the transaction, with a reason for abort
+	// abort the transaction, with a reason for the abort, abort_error = reason
 	// this functions first marks the trasaction as aborted in your context
 	// then it must also put a abort log for the transaction
 	// this method must be thread safe, there could be a transaction, using multiple threads
-	void (*mark_aborted)(const void* transaction_id, const void* context, int reason);
+	void (*mark_aborted)(const void* transaction_id, const void* context, int abort_error);
 };
 
 typedef struct transaction_abort_manager transaction_abort_manager;
