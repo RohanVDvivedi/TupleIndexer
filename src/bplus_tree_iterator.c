4a574369c55946afe7392d2e9dc07f1af2b022bc
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-22 19:00:09 -0700

    next and prev are allowed to end

--- src/bplus_tree_iterator.c
+++ src/bplus_tree_iterator.c
@@ -30,6 +30,10 @@ bplus_tree_iterator* get_new_bplus_tree_iterator(persistent_page curr_page, uint
 
 int next_bplus_tree_iterator(bplus_tree_iterator* bpi_p, const void* transaction_id, int* abort_error)
 {
+	// if the page itself is invalid, then quit
+	if(is_persistent_page_NULL(&(bpi_p->curr_page), bpi_p->dam_p))
+		return 0;
+
 	// increment the current tuple count, if the next tuple we point to will be on this page
 	if(bpi_p->curr_tuple_index + 1 < get_tuple_count_on_persistent_page(&(bpi_p->curr_page), bpi_p->bpttd_p->page_size, &(bpi_p->bpttd_p->record_def->size_def)))
 	{
@@ -44,9 +48,7 @@ int next_bplus_tree_iterator(bplus_tree_iterator* bpi_p, const void* transaction
 		uint64_t next_page_id = get_next_page_id_of_bplus_tree_leaf_page(&(bpi_p->curr_page), bpi_p->bpttd_p);
 
 		persistent_page next_page = get_NULL_persistent_page(bpi_p->dam_p);
-		if(next_page_id == bpi_p->bpttd_p->NULL_PAGE_ID) // we reached the end of leaf page list
-			return 0;
-		else
+		if(next_page_id != bpi_p->bpttd_p->NULL_PAGE_ID)
 		{
 			next_page = acquire_persistent_page_with_lock(bpi_p->dam_p, transaction_id, next_page_id, READ_LOCK, abort_error);
 			if(*abort_error) // on an abort, release curr_page lock and exit
@@ -60,7 +62,8 @@ int next_bplus_tree_iterator(bplus_tree_iterator* bpi_p, const void* transaction
 		release_lock_on_persistent_page(bpi_p->dam_p, transaction_id, &(bpi_p->curr_page), NONE_OPTION, abort_error);
 		if(*abort_error)
 		{
-			release_lock_on_persistent_page(bpi_p->dam_p, transaction_id, &next_page, NONE_OPTION, abort_error);
+			if(!is_persistent_page_NULL(&next_page, bpi_p->dam_p))
+				release_lock_on_persistent_page(bpi_p->dam_p, transaction_id, &next_page, NONE_OPTION, abort_error);
 			return 0;
 		}
 
@@ -81,13 +84,17 @@ int next_bplus_tree_iterator(bplus_tree_iterator* bpi_p, const void* transaction
 
 const void* get_tuple_bplus_tree_iterator(bplus_tree_iterator* bpi_p)
 {
-	if(bpi_p->curr_tuple_index >= get_tuple_count_on_persistent_page(&(bpi_p->curr_page), bpi_p->bpttd_p->page_size, &(bpi_p->bpttd_p->record_def->size_def)))
+	if(is_persistent_page_NULL(&(bpi_p->curr_page), bpi_p->dam_p) || bpi_p->curr_tuple_index >= get_tuple_count_on_persistent_page(&(bpi_p->curr_page), bpi_p->bpttd_p->page_size, &(bpi_p->bpttd_p->record_def->size_def)))
 		return NULL;
 	return get_nth_tuple_on_persistent_page(&(bpi_p->curr_page), bpi_p->bpttd_p->page_size, &(bpi_p->bpttd_p->record_def->size_def), bpi_p->curr_tuple_index);
 }
 
 int prev_bplus_tree_iterator(bplus_tree_iterator* bpi_p, const void* transaction_id, int* abort_error)
 {
+	// if the page itself is invalid, then quit
+	if(is_persistent_page_NULL(&(bpi_p->curr_page), bpi_p->dam_p))
+		return 0;
+
 	// increment the current tuple count, if the tuple that we are pointing to is not the first tuple on the page
 	if(bpi_p->curr_tuple_index != 0)
 	{
@@ -102,9 +109,7 @@ int prev_bplus_tree_iterator(bplus_tree_iterator* bpi_p, const void* transaction
 		uint64_t prev_page_id = get_prev_page_id_of_bplus_tree_leaf_page(&(bpi_p->curr_page), bpi_p->bpttd_p);
 
 		persistent_page prev_page = get_NULL_persistent_page(bpi_p->dam_p);
-		if(prev_page_id == bpi_p->bpttd_p->NULL_PAGE_ID) // we reached the end of leaf page list
-			return 0;
-		else
+		if(prev_page_id != bpi_p->bpttd_p->NULL_PAGE_ID)
 		{
 			prev_page = acquire_persistent_page_with_lock(bpi_p->dam_p, transaction_id, prev_page_id, READ_LOCK, abort_error);
 			if(*abort_error) // on an abort, release curr_page lock and exit
@@ -118,7 +123,8 @@ int prev_bplus_tree_iterator(bplus_tree_iterator* bpi_p, const void* transaction
 		release_lock_on_persistent_page(bpi_p->dam_p, transaction_id, &(bpi_p->curr_page), NONE_OPTION, abort_error);
 		if(*abort_error)
 		{
-			release_lock_on_persistent_page(bpi_p->dam_p, transaction_id, &prev_page, NONE_OPTION, abort_error);
+			if(!is_persistent_page_NULL(&prev_page, bpi_p->dam_p))
+				release_lock_on_persistent_page(bpi_p->dam_p, transaction_id, &prev_page, NONE_OPTION, abort_error);
 			return 0;
 		}
 
