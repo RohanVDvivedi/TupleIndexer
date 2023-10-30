e760d0e29cd4c1def8694eac8bf4a30d04ad1034
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-29 20:27:52 -0700

    serint will now be required for testcases

--- test/test.sh
+++ test/test.sh
@@ -1,4 +1,4 @@
-gcc -Wall ./test_$1.c -o test_$1.out -ltupleindexer -ltuplestore -lrwlock -lcutlery
+gcc -Wall ./test_$1.c -o test_$1.out -ltupleindexer -ltuplestore -lrwlock -lserint -lcutlery
 if [ $? -eq 0 ]
 then
 	if [ "$2" = "vald" ]
