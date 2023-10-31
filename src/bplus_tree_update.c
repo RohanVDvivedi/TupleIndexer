431e1dba11e4605ebfce4292b6436c0d9772763a
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-30 22:42:51 -0700

    implementation check point

--- src/bplus_tree_update.c
+++ src/bplus_tree_update.c
@@ -128,13 +128,38 @@ int inspected_update_in_bplus_tree(uint64_t root_page_id, void* new_record, cons
 {
 	if(!check_if_record_can_be_inserted_into_bplus_tree(bpttd_p, new_record))
 		return 0;
-	
-	persistent_page concerned_leaf = get_NULL_persistent_page(dam_p);
-	read_couple_locks_until_leaf_using_record(root_page_id, new_record, bpttd_p->key_element_count, WRITE_LOCK, &concerned_leaf, bpttd_p, dam_p, transaction_id, abort_error);
+
+	// create a stack of capacity = levels
+	locked_pages_stack* locked_pages_stack_p = &((locked_pages_stack){});
+	uint32_t root_page_level;
+
+	{
+		// get lock on the root page of the bplus_tree
+		persistent_page root_page = acquire_persistent_page_with_lock(dam_p, transaction_id, root_page_id, WRITE_LOCK, abort_error);
+		if(*abort_error)
+			return 0;
+
+		// pre cache level of the root_page
+		root_page_level = get_level_of_bplus_tree_page(&root_page, bpttd_p);
+
+		// create a stack of capacity = levels
+		if(!initialize_locked_pages_stack(locked_pages_stack_p, root_page_level + 1))
+			exit(-1);
+
+		// push the root page onto the stack
+		push_to_locked_pages_stack(locked_pages_stack_p, &INIT_LOCKED_PAGE_INFO(root_page));
+	}
+
+	uint32_t release_for_split = 0;
+	uint32_t release_for_merge = 0;
+	walk_down_locking_parent_pages_for_update_using_record(root_page_id, locked_pages_stack_p, new_record, &release_for_split, &release_for_merge, bpttd_p, dam_p, transaction_id, abort_error);
 	if(*abort_error)
 		return 0;
 
-	// find index of last record that compared equal to the new_record
+	// concerned_leaf will always be at the top of this stack
+	persistent_page concerned_leaf = &(get_top_of_locked_pages_stack(locked_pages_stack_p)->ppage);
+
+	// find index of last record that compares equal to the new_record
 	uint32_t found_index = find_last_in_sorted_packed_page(
 											&(concerned_leaf), bpttd_p->page_size,
 											bpttd_p->record_def, bpttd_p->key_element_ids, bpttd_p->key_compare_direction, bpttd_p->key_element_count,
@@ -150,20 +175,21 @@ int inspected_update_in_bplus_tree(uint64_t root_page_id, void* new_record, cons
 		old_record_size = get_tuple_size(bpttd_p->record_def, old_record);
 	}
 
+	// result to return
+	int result = 0;
+
 	int ui_res = ui_p->update_inspect(ui_p->context, bpttd_p->record_def, old_record, &new_record, transaction_id, abort_error);
 	if((*abort_error) || ui_res == 0)
 	{
-		release_lock_on_persistent_page(dam_p, transaction_id, &concerned_leaf, NONE_OPTION, abort_error);
-		return 0;
+		result = 0;
+		goto ABORT_OR_FAIL;
 	}
 
 	// if old_record did not exist and the new_record is set to NULL (i.e. a request for deletion, then do nothing)
 	if(old_record == NULL && new_record == NULL)
 	{
-		release_lock_on_persistent_page(dam_p, transaction_id, &concerned_leaf, NONE_OPTION, abort_error);
-		if(*abort_error)
-			return 0;
-		return 1;
+		result = 1;
+		goto ABORT_OR_FAIL;
 	}
 	else if(old_record == NULL && new_record != NULL) // insert case
 	{
@@ -356,4 +382,21 @@ int inspected_update_in_bplus_tree(uint64_t root_page_id, void* new_record, cons
 
 		return updated;
 	}
+
+
+	ABORT_OR_FAIL:;
+	// release all locks of the locked_pages_stack and exit
+	// do not worry about abort_error here or in the loop, as we are only releasing locks
+	while(get_element_count_locked_pages_stack(locked_pages_stack_p) > 0)
+	{
+		locked_page_info* bottom = get_bottom_of_locked_pages_stack(locked_pages_stack_p);
+		release_lock_on_persistent_page(dam_p, transaction_id, &(bottom->ppage), NONE_OPTION, abort_error);
+		pop_bottom_from_locked_pages_stack(locked_pages_stack_p);
+	}
+
+	// on an abort, overide the result to 0
+	if(*abort_error)
+		result = 0;
+
+	return result;
 }
