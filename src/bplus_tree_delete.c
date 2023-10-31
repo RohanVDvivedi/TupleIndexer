2cccbb3573a74680a0825ae3a530afff93beea7e
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-31 03:30:53 -0700

    minor refactoring in bplus_tree delete

--- src/bplus_tree_delete.c
+++ src/bplus_tree_delete.c
@@ -40,59 +40,41 @@ int delete_from_bplus_tree(uint64_t root_page_id, const void* key, const bplus_t
 		return 0;
 	}
 
-	// deleted will be set if the record, was deleted
-	int deleted = 0;
-
 	// this has to be a leaf page
 	// we access it using pointer, so that upon deleting, its was modified bit get's set
 	locked_page_info* curr_locked_page = get_top_of_locked_pages_stack(locked_pages_stack_p);
 
-	// if this check fails, then you are not using the utilities rightly
-	if(is_bplus_tree_leaf_page(&(curr_locked_page->ppage), bpttd_p))
-	{
-		// find index of last record that has the given key on the page
-		uint32_t found_index = find_last_in_sorted_packed_page(
-											&(curr_locked_page->ppage), bpttd_p->page_size,
-											bpttd_p->record_def, bpttd_p->key_element_ids, bpttd_p->key_compare_direction, bpttd_p->key_element_count,
-											key, bpttd_p->key_def, NULL
-										);
+	// find index of last record that has the given key on the page
+	uint32_t found_index = find_last_in_sorted_packed_page(
+										&(curr_locked_page->ppage), bpttd_p->page_size,
+										bpttd_p->record_def, bpttd_p->key_element_ids, bpttd_p->key_compare_direction, bpttd_p->key_element_count,
+										key, bpttd_p->key_def, NULL
+									);
 
-		// if no such record can be found, we break and exit
-		if(NO_TUPLE_FOUND == found_index)
-			goto EXIT;
-
-		// perform a delete operation on the found index in this page
-		deleted = delete_in_sorted_packed_page(
-							&(curr_locked_page->ppage), bpttd_p->page_size,
-							bpttd_p->record_def,
-							found_index,
-							pmm_p,
-							transaction_id,
-							abort_error
-						);
-
-		if((*abort_error) || !deleted) // THIS IS AN ERR, WE CANT RECOVER FROM
-		{
-			// ABORT
-			// release locks on all the pages, we had locks on until now
-			goto EXIT;
-		}
-		else
-		{
-			merge_and_unlock_pages_up(root_page_id, locked_pages_stack_p, bpttd_p, dam_p, pmm_p, transaction_id, abort_error);
-			if(*abort_error)
-			{
-				// on a abort_error here, the corresponding locks to the pages in the locked_pages_stack would have aleady been released by merge_and_unlock_pages_up
-				deinitialize_locked_pages_stack(locked_pages_stack_p);
-				return 0;
-			}
-		}
-	}
-	else
-	{
-		// you must never arrive here
+	// if no such record can be found, we break and exit
+	if(NO_TUPLE_FOUND == found_index)
 		goto EXIT;
-	}
+
+	// perform a delete operation on the found index in this page, this has to succeed for a valid index
+	delete_in_sorted_packed_page(
+						&(curr_locked_page->ppage), bpttd_p->page_size,
+						bpttd_p->record_def,
+						found_index,
+						pmm_p,
+						transaction_id,
+						abort_error
+					);
+	if(*abort_error)
+		goto EXIT;
+
+	int result = merge_and_unlock_pages_up(root_page_id, locked_pages_stack_p, bpttd_p, dam_p, pmm_p, transaction_id, abort_error);
+
+	// on a abort_error here, the corresponding locks to the pages in the locked_pages_stack would have already been released by merge_and_unlock_pages_up
+	// we only need to deinitialize the stack
+	deinitialize_locked_pages_stack(locked_pages_stack_p);
+
+	// return merge result, which has to be 1, in absence of an abort_error
+	return result;
 
 	EXIT:;
 	while(get_element_count_locked_pages_stack(locked_pages_stack_p) > 0)
