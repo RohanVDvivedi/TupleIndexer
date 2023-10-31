44191e223f7b4ed9b9d7e1d9872ebc02f44b25e9
RohanVDvivedi <rohandvivedi@gmail.com>
2023-10-30 21:47:33 -0700

    bplus_tree_delete conforming to the new api without until

--- src/bplus_tree_delete.c
+++ src/bplus_tree_delete.c
@@ -32,8 +32,8 @@ int delete_from_bplus_tree(uint64_t root_page_id, const void* key, const bplus_t
 		push_to_locked_pages_stack(locked_pages_stack_p, &INIT_LOCKED_PAGE_INFO(root_page));
 	}
 
-	// walk down taking locks until you reach leaf page level = 0
-	walk_down_locking_parent_pages_for_merge_using_key(root_page_id, 0, locked_pages_stack_p, key, bpttd_p, dam_p, transaction_id, abort_error);
+	// walk down taking locks until you reach leaf page level
+	walk_down_locking_parent_pages_for_merge_using_key(root_page_id, locked_pages_stack_p, key, bpttd_p, dam_p, transaction_id, abort_error);
 	if(*abort_error)
 	{
 		deinitialize_locked_pages_stack(locked_pages_stack_p);
