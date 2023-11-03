12cf80b4a26ff121cac15af69bc5e72ac684ca1f
RohanVDvivedi <rohandvivedi@gmail.com>
2023-11-02 18:30:13 -0700

    refactoring to convert curr_page to leaf page and curr_tuple_index to leaf_tuple_index

--- src/bplus_tree_find.c
+++ src/bplus_tree_find.c
@@ -168,119 +168,97 @@ bplus_tree_iterator* find_in_bplus_tree(uint64_t root_page_id, const void* key, 
 	else
 		f_type = LESSER_THAN_KEY + (find_pos - LESSER_THAN);
 
-	// get lock on the root page of the bplus_tree
-	persistent_page curr_page = acquire_persistent_page_with_lock(dam_p, transaction_id, root_page_id, READ_LOCK, abort_error);
-	if(*abort_error)
+	locked_pages_stack lps = walk_down_for_find_using_key(root_page_id, key, f_type, key_element_count_concerned, READ_LOCK, 0, bpttd_p, dam_p, transaction_id, abort_error);
+	if(*abort_error) // on abort, all pages would have already been locked by this function
 		return NULL;
 
-	while(!is_bplus_tree_leaf_page(&curr_page, bpttd_p))
+	// unlock all parent pages, and extract the top page
+	while(get_element_count_locked_pages_stack(&lps) > 1)
 	{
-		uint32_t child_index = 0;
+		locked_page_info* bottom = get_bottom_of_locked_pages_stack(&lps);
+		release_lock_on_persistent_page(dam_p, transaction_id, &(bottom->ppage), NONE_OPTION, abort_error);
+		pop_bottom_from_locked_pages_stack(&lps);
 
-		// get lock on the next page based on the f_type
-		switch(f_type)
-		{
-			case MIN_TUPLE :
-			{
-				child_index = -1;
-				break;
-			}
-			case LESSER_THAN_KEY :
-			case LESSER_THAN_EQUALS_KEY :
-			case GREATER_THAN_EQUALS_KEY :
-			case GREATER_THAN_KEY :
-			{
-				child_index = find_child_index_for_key(&curr_page, key, key_element_count_concerned, bpttd_p);
-				break;
-			}
-			case MAX_TUPLE :
-			{
-				child_index = get_tuple_count_on_persistent_page(&curr_page, bpttd_p->page_size, &(bpttd_p->index_def->size_def)) - 1;
-				break;
-			}
-		}
-
-		uint64_t next_page_id = get_child_page_id_by_child_index(&curr_page, child_index, bpttd_p);
-		persistent_page next_page = acquire_persistent_page_with_lock(dam_p, transaction_id, next_page_id, READ_LOCK, abort_error);
+		// on abort error, relase all locks, deinitialize lps stack and exit
 		if(*abort_error)
 		{
-			release_lock_on_persistent_page(dam_p, transaction_id, &curr_page, NONE_OPTION, abort_error);
+			while(get_element_count_locked_pages_stack(&lps) > 0)
+			{
+				locked_page_info* bottom = get_bottom_of_locked_pages_stack(&lps);
+				release_lock_on_persistent_page(dam_p, transaction_id, &(bottom->ppage), NONE_OPTION, abort_error);
+				pop_bottom_from_locked_pages_stack(&lps);
+			}
+			deinitialize_locked_pages_stack(&lps);
 			return NULL;
 		}
-
-		// release lock on the curr_page and 
-		// make the next_page as the curr_page
-		release_lock_on_persistent_page(dam_p, transaction_id, &curr_page, NONE_OPTION, abort_error);
-		if(*abort_error)
-		{
-			release_lock_on_persistent_page(dam_p, transaction_id, &next_page, NONE_OPTION, abort_error);
-			return NULL;
-		}
-		curr_page = next_page;
 	}
+	persistent_page leaf_page = get_top_of_locked_pages_stack(&lps)->ppage;
+	deinitialize_locked_pages_stack(&lps);
 
-	// find the curr_tuple_index for the iterator to start with
-	uint32_t curr_tuple_index = 0;
+	// lps mus not be accessed from this point on
+
+	// find the leaf_tuple_index for the iterator to start with
+	uint32_t leaf_tuple_index = 0;
 
 	switch(f_type)
 	{
 		case MIN_TUPLE :
 		{
-			curr_tuple_index = 0;
+			leaf_tuple_index = 0;
 			break;
 		}
 		case LESSER_THAN_KEY :
 		{
-			curr_tuple_index = find_preceding_in_sorted_packed_page(
-									&curr_page, bpttd_p->page_size, 
+			leaf_tuple_index = find_preceding_in_sorted_packed_page(
+									&leaf_page, bpttd_p->page_size, 
 									bpttd_p->record_def, bpttd_p->key_element_ids, bpttd_p->key_compare_direction, key_element_count_concerned,
 									key, bpttd_p->key_def, NULL
 								);
 
-			curr_tuple_index = (curr_tuple_index != NO_TUPLE_FOUND) ? curr_tuple_index : 0;
+			leaf_tuple_index = (leaf_tuple_index != NO_TUPLE_FOUND) ? leaf_tuple_index : 0;
 			break;
 		}
 		case LESSER_THAN_EQUALS_KEY :
 		{
-			curr_tuple_index = find_preceding_equals_in_sorted_packed_page(
-									&curr_page, bpttd_p->page_size, 
+			leaf_tuple_index = find_preceding_equals_in_sorted_packed_page(
+									&leaf_page, bpttd_p->page_size, 
 									bpttd_p->record_def, bpttd_p->key_element_ids, bpttd_p->key_compare_direction, key_element_count_concerned,
 									key, bpttd_p->key_def, NULL
 								);
 
-			curr_tuple_index = (curr_tuple_index != NO_TUPLE_FOUND) ? curr_tuple_index : 0;
+			leaf_tuple_index = (leaf_tuple_index != NO_TUPLE_FOUND) ? leaf_tuple_index : 0;
 			break;
 		}
 		case GREATER_THAN_EQUALS_KEY :
 		{
-			curr_tuple_index = find_succeeding_equals_in_sorted_packed_page(
-									&curr_page, bpttd_p->page_size, 
+			leaf_tuple_index = find_succeeding_equals_in_sorted_packed_page(
+									&leaf_page, bpttd_p->page_size, 
 									bpttd_p->record_def, bpttd_p->key_element_ids, bpttd_p->key_compare_direction, key_element_count_concerned,
 									key, bpttd_p->key_def, NULL
 								);
 
-			curr_tuple_index = (curr_tuple_index != NO_TUPLE_FOUND) ? curr_tuple_index : LAST_TUPLE_INDEX_BPLUS_TREE_LEAF_PAGE;
+			leaf_tuple_index = (leaf_tuple_index != NO_TUPLE_FOUND) ? leaf_tuple_index : LAST_TUPLE_INDEX_BPLUS_TREE_LEAF_PAGE;
 			break;
 		}
 		case GREATER_THAN_KEY :
 		{
-			curr_tuple_index = find_succeeding_in_sorted_packed_page(
-									&curr_page, bpttd_p->page_size, 
+			leaf_tuple_index = find_succeeding_in_sorted_packed_page(
+									&leaf_page, bpttd_p->page_size, 
 									bpttd_p->record_def, bpttd_p->key_element_ids, bpttd_p->key_compare_direction, key_element_count_concerned,
 									key, bpttd_p->key_def, NULL
 								);
 
-			curr_tuple_index = (curr_tuple_index != NO_TUPLE_FOUND) ? curr_tuple_index : LAST_TUPLE_INDEX_BPLUS_TREE_LEAF_PAGE;
+			leaf_tuple_index = (leaf_tuple_index != NO_TUPLE_FOUND) ? leaf_tuple_index : LAST_TUPLE_INDEX_BPLUS_TREE_LEAF_PAGE;
 			break;
 		}
 		case MAX_TUPLE :
 		{
-			curr_tuple_index = LAST_TUPLE_INDEX_BPLUS_TREE_LEAF_PAGE;
+			leaf_tuple_index = LAST_TUPLE_INDEX_BPLUS_TREE_LEAF_PAGE;
 			break;
 		}
 	}
 
-	bplus_tree_iterator* bpi_p = get_new_bplus_tree_iterator(curr_page, curr_tuple_index, bpttd_p, dam_p);
+	bplus_tree_iterator* bpi_p = get_new_bplus_tree_iterator(leaf_page, leaf_tuple_index, bpttd_p, dam_p);
 
 	// iterate next or previous in bplus_tree_iterator, based on the f_type
 	// this is not required for MIN_TUPLE and MAX_TUPLE
