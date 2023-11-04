28f66ba0fa643efb70ce214e9bd37592522c99a7
RohanVDvivedi <rohandvivedi@gmail.com>
2023-11-03 17:31:50 -0700

    finalized new api of bplus_tree_iterator

--- inc/bplus_tree_iterator.h
+++ inc/bplus_tree_iterator.h
@@ -27,6 +27,7 @@ struct bplus_tree_iterator
 
 // if the lps has its leaf page locked in write_mode, then the iterator will lock all following leaf pages in WRITE_LOCK
 // if the size of the lps is > 1, then the iterator becomes stacked iterator, locking leaf pages using parent pages instead of the next and previous linkages
+// the capacity of lps is expected to be atleast 2
 // curr_tuple_index if UINT32_MAX then the iterator will point to the last 
 bplus_tree_iterator* get_new_bplus_tree_iterator(locked_pages_stack lps, uint32_t curr_tuple_index, const bplus_tree_tuple_defs* bpttd_p, const data_access_methods* dam_p);
 
