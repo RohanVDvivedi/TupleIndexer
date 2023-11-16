59878ba1be8adb0c1dc770e3065b7f7699139c0e
RohanVDvivedi <rohandvivedi@gmail.com>
2023-11-16 15:38:50 -0700

    added logic to insert tuple resiliently

--- src/utils/persistent_page_altered_util.c
+++ src/utils/persistent_page_altered_util.c
@@ -27,6 +27,31 @@ int append_tuple_on_persistent_page_resiliently(const page_modification_methods*
 	return append_tuple_on_persistent_page(pmm_p, transaction_id, ppage, page_size, tpl_sz_d, external_tuple, abort_error);
 }
 
+int insert_tuple_on_persistent_page_resiliently(const page_modification_methods* pmm_p, const void* transaction_id, persistent_page* ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d, uint32_t index, const void* external_tuple, int* abort_error)
+{
+	// try simply inserting, first
+	int res = insert_tuple_on_persistent_page(pmm_p, transaction_id, ppage, page_size, tpl_sz_d, index, external_tuple, abort_error);
+	if((*abort_error))
+		return 0;
+	else if(res)
+		return 1;
+
+	uint32_t unused_space_on_page = get_space_allotted_to_all_tuples_on_persistent_page(ppage, page_size, tpl_sz_d) - get_space_occupied_by_all_tuples_on_persistent_page(ppage, page_size, tpl_sz_d);
+	uint32_t space_required_by_external_tuple = ((external_tuple == NULL) ? 0 : get_tuple_size_using_tuple_size_def(tpl_sz_d, external_tuple)) + get_additional_space_overhead_per_tuple_on_persistent_page(page_size, tpl_sz_d);
+
+	// check suggests that there is not enough space on page
+	if(space_required_by_external_tuple > unused_space_on_page)
+		return 0;
+
+	// run_page_compaction, i.e. defragment the page
+	run_persistent_page_compaction(pmm_p, transaction_id, ppage, page_size, tpl_sz_d, abort_error);
+	if((*abort_error))
+		return 0;
+
+	// now try to insert, this must succeed, unless for an abort_error
+	return insert_tuple_on_persistent_page(pmm_p, transaction_id, ppage, page_size, tpl_sz_d, index, external_tuple, abort_error);
+}
+
 int update_tuple_on_persistent_page_resiliently(const page_modification_methods* pmm_p, const void* transaction_id, persistent_page* ppage, uint32_t page_size, const tuple_size_def* tpl_sz_d, uint32_t index, const void* external_tuple, int* abort_error)
 {
 	// try simply updating, first
