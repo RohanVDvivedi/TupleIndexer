915616f111cd5cf180454d5ef38cb62c0c7adde2
RohanVDvivedi <rohandvivedi@gmail.com>
2023-11-19 16:23:45 -0700

    decouple common_page_header from bpttd

--- src/page_table/page_table_page_header.c
+++ src/page_table/page_table_page_header.c
@@ -1,1 +1,25 @@
-//#include<page_table_page_header.h>
+#include<page_table_page_header.h>
+
+// number of bytes to store level of the page
+// 2 is more than enough for curent computer systems, and allows us to store large amount of data
+// values in range 1 to 4 both inclusive
+#define BYTES_FOR_PAGE_LEVEL 2
+
+uint32_t get_offset_to_end_of_page_table_page_header(const page_table_tuple_defs* pttd_p)
+{
+	return get_offset_to_end_of_common_page_header(bpttd_p) + BYTES_FOR_PAGE_LEVEL + pttd_p->page_id_width;
+}
+
+uint32_t get_level_of_page_table_page(const persistent_page* ppage, const page_table_tuple_defs* pttd_p);
+
+uint64_t get_first_bucket_id_of_page_table_page(const persistent_page* ppage, const page_table_tuple_defs* pttd_p);
+
+int is_page_table_leaf_page(const persistent_page* ppage, const page_table_tuple_defs* pttd_p);
+
+page_table_page_header get_page_table_page_header(const persistent_page* ppage, const page_table_tuple_defs* pttd_p);
+
+void serialize_page_table_page_header(void* hdr_serial, const page_table_page_header* bptiph_p, const page_table_tuple_defs* pttd_p);
+
+void set_page_table_page_header(persistent_page* ppage, const page_table_page_header* bptiph_p, const page_table_tuple_defs* pttd_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+
+void print_page_table_page_header(const persistent_page* ppage, const page_table_tuple_defs* pttd_p);
