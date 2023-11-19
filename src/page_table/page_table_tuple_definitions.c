dd684cf022e0745e3c2f171b6d02a9d86447293c
RohanVDvivedi <rohandvivedi@gmail.com>
2023-11-19 14:10:30 -0700

    minor fix

--- src/page_table/page_table_tuple_definitions.c
+++ src/page_table/page_table_tuple_definitions.c
@@ -11,7 +11,7 @@ int init_bplus_tree_tuple_definitions(page_table_tuple_defs* pttd_p, uint32_t sy
 		return 0;
 
 	// there must be room for atleast some bytes after the page_table_page_header
-	if(sizeof_PAGE_TABLE_PAGE_HEADER(bpttd_p) >= page_size)
+	if(sizeof_PAGE_TABLE_PAGE_HEADER(pttd_p) >= page_size)
 		return 0;
 
 	// initialize struct attributes
@@ -20,6 +20,16 @@ int init_bplus_tree_tuple_definitions(page_table_tuple_defs* pttd_p, uint32_t sy
 	pttd_p->page_size = page_size;
 
 	pttd_p->system_header_size = system_header_size;
+
+	int res = 1;
+
+	pttd_p->entry_def = get_new_tuple_def("temp_entry_def", 1, page_size);
+	res = insert_element_def(pttd_p->entry_def, "child_page_id", UINT, page_id_width, 1, &((user_value){.uint_value = bpttd_p->NULL_PAGE_ID}));
+	res = finalize_tuple_def(pttd_p->entry_def);
+
+	pttd_p->entries_per_page = get_maximum_tuple_count_on_page(sizeof_PAGE_TABLE_PAGE_HEADER(pttd_p), page_size, &(pttd_p->entry_def->size_def));
+
+	return 1;
 }
 
 void deinit_page_table_tuple_definitions(page_table_tuple_defs* pttd_p)
