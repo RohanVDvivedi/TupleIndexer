a47457e875849b17caa880dd15baeed244480800
RohanVDvivedi <rohandvivedi@gmail.com>
2023-11-19 14:16:14 -0700

    added fail cases for entry_def and entries_per_page

--- src/page_table/page_table_tuple_definitions.c
+++ src/page_table/page_table_tuple_definitions.c
@@ -24,11 +24,35 @@ int init_bplus_tree_tuple_definitions(page_table_tuple_defs* pttd_p, uint32_t sy
 
 	int res = 1;
 
+	// initialize entry_def
 	pttd_p->entry_def = get_new_tuple_def("temp_entry_def", 1, page_size);
+	if(pttd_p->entry_def == NULL)
+	{
+		deinit_page_table_tuple_definitions(pttd_p);
+		return 0;
+	}
 	res = insert_element_def(pttd_p->entry_def, "child_page_id", UINT, page_id_width, 1, &((user_value){.uint_value = bpttd_p->NULL_PAGE_ID}));
+	if(res == 0)
+	{
+		deinit_page_table_tuple_definitions(pttd_p);
+		return 0;
+	}
 	res = finalize_tuple_def(pttd_p->entry_def);
+	if(res == 0)
+	{
+		deinit_page_table_tuple_definitions(pttd_p);
+		return 0;
+	}
 
+	// number of entries that can fir on the page
 	pttd_p->entries_per_page = get_maximum_tuple_count_on_page(sizeof_PAGE_TABLE_PAGE_HEADER(pttd_p), page_size, &(pttd_p->entry_def->size_def));
+
+	// there has to be atleast 2 entries per page for it to be a tree
+	if(pttd_p->entries_per_page < 2)
+	{
+		deinit_page_table_tuple_definitions(pttd_p);
+		return 0;
+	}
 
 	return 1;
 }
