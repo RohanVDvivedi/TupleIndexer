7c8013b54f175c36bb77b379858abaf30601fbbd
RohanVDvivedi <rohandvivedi@gmail.com>
2023-11-19 17:35:24 -0700

    implemented get for page_table page header

--- src/page_table/page_table_page_header.c
+++ src/page_table/page_table_page_header.c
@@ -1,5 +1,11 @@
 #include<page_table_page_header.h>
 
+#include<persistent_page_functions.h>
+
+#include<serial_int.h>
+
+#include<stdlib.h>
+
 // number of bytes to store level of the page
 // 2 is more than enough for curent computer systems, and allows us to store large amount of data
 // values in range 1 to 4 both inclusive
@@ -25,7 +31,20 @@ int is_page_table_leaf_page(const persistent_page* ppage, const page_table_tuple
 	return get_page_table_page_header(ppage, pttd_p).level == 0;
 }
 
-page_table_page_header get_page_table_page_header(const persistent_page* ppage, const page_table_tuple_defs* pttd_p);
+static inline uint32_t get_offset_to_page_table_page_header_locals(const page_table_tuple_defs* pttd_p)
+{
+	return get_offset_to_end_of_common_page_header(&(pttd_p->pas));
+}
+
+page_table_page_header get_page_table_page_header(const persistent_page* ppage, const page_table_tuple_defs* pttd_p)
+{
+	const void* page_table_page_header_serial = get_page_header_ua_persistent_page(ppage, pttd_p->pas.page_size) + get_offset_to_page_table_page_header_locals(pttd_p);
+	return (page_table_page_header){
+		.parent = get_common_page_header(ppage, &(pttd_p->pas)),
+		.level = deserialize_uint32(page_table_page_header_serial, BYTES_FOR_PAGE_LEVEL),
+		.first_bucket_id = deserialize_uint64(page_table_page_header_serial + BYTES_FOR_PAGE_LEVEL, pttd_p->pas.page_id_width),
+	};
+}
 
 void serialize_page_table_page_header(void* hdr_serial, const page_table_page_header* bptiph_p, const page_table_tuple_defs* pttd_p);
 
