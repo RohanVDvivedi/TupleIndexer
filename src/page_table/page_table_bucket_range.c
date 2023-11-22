5fa014d0d5aabef995dbd397b6f277ac2a37a3f1
RohanVDvivedi <rohandvivedi@gmail.com>
2023-11-21 18:35:16 -0700

    ptbr functions implemented

--- src/page_table/page_table_bucket_range.c
+++ src/page_table/page_table_bucket_range.c
@@ -11,6 +11,12 @@ int are_disjoint_page_table_bucket_range(const page_table_bucket_range* ptbr1_p,
 	return (ptbr1_p->last_bucket_id < ptbr2_p->first_bucket_id) || (ptbr2_p->last_bucket_id < ptbr1_p->first_bucket_id);
 }
 
-int have_overlap_page_table_bucket_range(const page_table_bucket_range* ptbr1_p, const page_table_bucket_range* ptbr2_p);
+int have_overlap_page_table_bucket_range(const page_table_bucket_range* ptbr1_p, const page_table_bucket_range* ptbr2_p)
+{
+	return !are_disjoint_page_table_bucket_range(ptbr1_p, ptbr2_p);
+}
 
-int is_contained_page_table_bucket_range(const page_table_bucket_range* container_p, const page_table_bucket_range* containee_p);
+int is_contained_page_table_bucket_range(const page_table_bucket_range* container_p, const page_table_bucket_range* containee_p)
+{
+	return (container_p->first_bucket_id <= containee_p->first_bucket_id) && (containee_p->last_bucket_id <= container_p->last_bucket_id);
+}
