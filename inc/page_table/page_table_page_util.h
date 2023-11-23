fa94ac73f67a404975ed4d093ff61be745549382
RohanVDvivedi <rohandvivedi@gmail.com>
2023-11-22 18:52:49 -0700

    added more comments for page_table_page_util functions

--- inc/page_table/page_table_page_util.h
+++ inc/page_table/page_table_page_util.h
@@ -9,9 +9,14 @@
 void init_page_table_page(persistent_page* ppage, uint32_t level, uint64_t first_bucket_id, const page_table_tuple_defs* pttd_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
 
 // get child_page_id in the page at child_index
-uint64_t get_child_page_id_at_child_index(const persistent_page* ppage, const page_table_tuple_defs* pttd_p);
+uint64_t get_child_page_id_at_child_index_in_page_table_page(const persistent_page* ppage, const page_table_tuple_defs* pttd_p);
 
 // set child_page_id in the page at child_index
-int set_child_page_id_at_child_index(const persistent_page* ppage, uint64_t child_page_id, const page_table_tuple_defs* pttd_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+// fails if child_index > pttd_p->entries_per_page
+int set_child_page_id_at_child_index_in_page_table_page(const persistent_page* ppage, uint64_t child_page_id, const page_table_tuple_defs* pttd_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+
+// returns true if all the child pointers in the page_table_page are NULL_PAGE_ID
+// i.e. tombstones_count == tuples_count
+int has_all_NULL_PAGE_ID_in_page_table_page(const persistent_page* ppage, const page_table_tuple_defs* pttd_p);
 
 #endif
