9600d56585a0f02bb047b0cb9f7e01b294f2730a
RohanVDvivedi <rohandvivedi@gmail.com>
2023-11-22 19:23:08 -0700

    started implementing setter for page_table_page

--- src/page_table/page_table_page_util.c
+++ src/page_table/page_table_page_util.c
@@ -22,6 +22,10 @@ int init_page_table_page(persistent_page* ppage, uint32_t level, uint64_t first_
 
 uint64_t get_child_page_id_at_child_index_in_page_table_page(const persistent_page* ppage, uint32_t child_index, const page_table_tuple_defs* pttd_p)
 {
+	// child_index out of range
+	if(child_index >= pttd_p->pas_p->entries_per_page)
+		return pttd_p->pas_p->NULL_PAGE_ID;
+
 	const void* child_tuple = get_nth_tuple_on_persistent_page(ppage, pttd_p->pas_p->page_size, &(pttd_p->entry_def->size_def), child_index);
 
 	// if the child_tuple is NULL, then it is NULL_PAGE_ID
@@ -32,7 +36,14 @@ uint64_t get_child_page_id_at_child_index_in_page_table_page(const persistent_pa
 	return get_value_from_element_from_tuple(pttd_p->entry_def, 0, child_tuple).uint_value;
 }
 
-int set_child_page_id_at_child_index_in_page_table_page(const persistent_page* ppage, uint32_t child_index, uint64_t child_page_id, const page_table_tuple_defs* pttd_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+int set_child_page_id_at_child_index_in_page_table_page(const persistent_page* ppage, uint32_t child_index, uint64_t child_page_id, const page_table_tuple_defs* pttd_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error)
+{
+	// child_index out of range
+	if(child_index >= pttd_p->pas_p->entries_per_page)
+		return 0;
+
+	// TODO
+}
 
 int has_all_NULL_PAGE_ID_in_page_table_page(const persistent_page* ppage, const page_table_tuple_defs* pttd_p)
 {
