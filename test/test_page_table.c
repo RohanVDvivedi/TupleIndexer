e49eeae92956c035a42cded4509175d293ef8d2f
RohanVDvivedi <rohandvivedi@gmail.com>
2023-11-27 14:53:01 -0700

    prelimnary test cases for page table new and destroy and print passed

--- test/test_page_table.c
+++ test/test_page_table.c
@@ -2,8 +2,8 @@
 #include<stdlib.h>
 #include<stdint.h>
 
-#include<bplus_tree.h>
-#include<bplus_tree_tuple_definitions.h>
+#include<page_table.h>
+#include<page_table_tuple_definitions.h>
 #include<page_access_methods.h>
 #include<unWALed_in_memory_data_store.h>
 #include<unWALed_page_modification_methods.h>
@@ -12,6 +12,10 @@
 #define PAGE_ID_WIDTH        3
 #define PAGE_SIZE          256
 #define SYSTEM_HEADER_SIZE   3
+
+// initialize transaction_id and abort_error
+const void* transaction_id = NULL;
+int abort_error = 0;
 
 int main()
 {
@@ -28,10 +32,10 @@ int main()
 	init_page_table_tuple_definitions(&pttd, &(pam_p->pas));
 
 	// print the generated bplus tree tuple defs
-	print_bplus_tree_tuple_definitions(&pttd);
+	print_page_table_tuple_definitions(&pttd);
 
 	// create a bplus tree and get its root
-	uint64_t root_page_id = get_new_bplus_tree(&bpttd, pam_p, pmm_p, transaction_id, &abort_error);
+	uint64_t root_page_id = get_new_page_table(&pttd, pam_p, pmm_p, transaction_id, &abort_error);
 	if(abort_error)
 	{
 		printf("ABORTED\n");
@@ -44,7 +48,7 @@ int main()
 	/* TESTS STARTED */
 
 	// print the constructed page table
-	print_page_table(root_page_id, 0, &pttd_p, pam_p, transaction_id, &abort_error);
+	print_page_table(root_page_id, 0, &pttd, pam_p, transaction_id, &abort_error);
 
 	/* TESTS ENDED */
 
@@ -62,7 +66,7 @@ int main()
 	close_and_destroy_unWALed_in_memory_data_store(pam_p);
 
 	// destroy page_table_tuple_definitions
-	deinit_page_table_tuple_definitions(&bpttd);
+	deinit_page_table_tuple_definitions(&pttd);
 
 	// destory page_modification_methods
 	delete_unWALed_page_modification_methods(pmm_p);
