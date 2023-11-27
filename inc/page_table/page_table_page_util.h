d1faa565bcb72db11687a5c9ea91f2e7da997131
RohanVDvivedi <rohandvivedi@gmail.com>
2023-11-26 18:31:30 -0700

    added minor comments

--- inc/page_table/page_table_page_util.h
+++ inc/page_table/page_table_page_util.h
@@ -49,10 +49,12 @@ page_table_bucket_range get_delegated_bucket_range_for_child_index_on_page_table
 uint32_t get_child_index_for_bucket_id_on_page_table_page(const persistent_page* ppage, uint64_t bucket_id, const page_table_tuple_defs* pttd_p);
 
 // level up the page table page, moving its contents into one of its children
+// you must have write lock on the page_table_page to do this
 int level_up_page_table_page(persistent_page* ppage, const page_table_tuple_defs* pttd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
 
 // level down page table page, moving contents of its children into it
 // this succeeds only if there is exactly 1 non NULL_PAGE_ID child on the page, and the page is not a leaf
+// you must have write lock on the page_table_page to do this
 int level_down_page_table_page(persistent_page* ppage, const page_table_tuple_defs* pttd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
 
 #endif
