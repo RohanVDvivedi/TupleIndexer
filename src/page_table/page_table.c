1dd260f7b5f8047a41c4aa1e9de8a37c5eba6a62
RohanVDvivedi <rohandvivedi@gmail.com>
2023-11-27 14:37:02 -0700

    copied bplus_tree print to implement page table print

--- src/page_table/page_table.c
+++ src/page_table/page_table.c
@@ -35,7 +35,7 @@ int destroy_page_table(uint64_t root_page_id, const page_table_tuple_defs* pttd_
 	locked_pages_stack* locked_pages_stack_p = &((locked_pages_stack){});
 
 	{
-		// get lock on the root page of the bplus_tree
+		// get lock on the root page of the page_table
 		persistent_page root_page = acquire_persistent_page_with_lock(pam_p, transaction_id, root_page_id, READ_LOCK, abort_error);
 		if(*abort_error)
 			return 0;
@@ -118,5 +118,92 @@ int destroy_page_table(uint64_t root_page_id, const page_table_tuple_defs* pttd_
 
 void print_page_table(uint64_t root_page_id, int only_leaf_pages, const page_table_tuple_defs* pttd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error)
 {
-	// TODO
+	// print the root page id of the page_table
+	printf("\n\nPage_table @ root_page_id = %"PRIu64"\n\n", root_page_id);
+
+	// create a stack
+	locked_pages_stack* locked_pages_stack_p = &((locked_pages_stack){});
+
+	{
+		// get lock on the root page of the page_table
+		persistent_page root_page = acquire_persistent_page_with_lock(pam_p, transaction_id, root_page_id, READ_LOCK, abort_error);
+		if(*abort_error)
+			return;
+
+		// pre cache level of the root_page
+		uint32_t root_page_level = get_level_of_page_table_page(&root_page, pttd_p);
+
+		// create a stack of capacity = levels
+		initialize_locked_pages_stack(locked_pages_stack_p, root_page_level + 1);
+
+		// push the root page onto the stack
+		push_to_locked_pages_stack(locked_pages_stack_p, &INIT_LOCKED_PAGE_INFO(root_page, 0));
+	}
+
+	while(get_element_count_locked_pages_stack(locked_pages_stack_p) > 0)
+	{
+		locked_page_info* curr_locked_page = get_top_of_locked_pages_stack(locked_pages_stack_p);
+
+		// print current page as a leaf page
+		if(is_bplus_tree_leaf_page(&(curr_locked_page->ppage), bpttd_p))
+		{
+			// print this page and its page_id
+			printf("page_id : %"PRIu64"\n\n", curr_locked_page->ppage.page_id);
+			print_bplus_tree_leaf_page(&(curr_locked_page->ppage), bpttd_p);
+			printf("xxxxxxxxxxxxx\n\n");
+
+			// unlock it and pop it from the stack
+			release_lock_on_persistent_page(pam_p, transaction_id, &(curr_locked_page->ppage), NONE_OPTION, abort_error);
+			pop_from_locked_pages_stack(locked_pages_stack_p);
+			if(*abort_error)
+				goto ABORT_ERROR;
+		}
+		// print current page as an interior page
+		else
+		{
+			// get tuple_count of the page
+			uint32_t tuple_count = get_tuple_count_on_persistent_page(&(curr_locked_page->ppage), bpttd_p->pas_p->page_size, &(bpttd_p->index_def->size_def));
+
+			// if child index is -1 or lesser than tuple_count
+			if(curr_locked_page->child_index == -1 || curr_locked_page->child_index < tuple_count)
+			{
+				// then push it's child at child_index onto the stack (with child_index = -1), while incrementing its child index
+				uint64_t child_page_id = get_child_page_id_by_child_index(&(curr_locked_page->ppage), curr_locked_page->child_index++, bpttd_p);
+				persistent_page child_page = acquire_persistent_page_with_lock(pam_p, transaction_id, child_page_id, READ_LOCK, abort_error);
+				if(*abort_error)
+					goto ABORT_ERROR;
+
+				push_to_locked_pages_stack(locked_pages_stack_p, &INIT_LOCKED_PAGE_INFO(child_page, ALL_LEAST_KEYS_CHILD_INDEX));
+			}
+			else // we have printed all its children, now we print this page
+			{
+				// here the child_index of the page is tuple_count
+
+				if(!only_leaf_pages)
+				{
+					// print this page and its page_id
+					printf("page_id : %"PRIu64"\n\n", curr_locked_page->ppage.page_id);
+					print_bplus_tree_interior_page(&(curr_locked_page->ppage), bpttd_p);
+					printf("xxxxxxxxxxxxx\n");
+				}
+
+				// pop it from the stack and unlock it
+				release_lock_on_persistent_page(pam_p, transaction_id, &(curr_locked_page->ppage), NONE_OPTION, abort_error);
+				pop_from_locked_pages_stack(locked_pages_stack_p);
+				if(*abort_error)
+					goto ABORT_ERROR;
+			}
+		}
+	}
+
+	ABORT_ERROR:;
+	// release locks on all the pages, we had locks on until now
+	while(get_element_count_locked_pages_stack(locked_pages_stack_p) > 0)
+	{
+		locked_page_info* bottom = get_bottom_of_locked_pages_stack(locked_pages_stack_p);
+		release_lock_on_persistent_page(pam_p, transaction_id, &(bottom->ppage), NONE_OPTION, abort_error);
+		pop_bottom_from_locked_pages_stack(locked_pages_stack_p);
+	}
+
+	deinitialize_locked_pages_stack(locked_pages_stack_p);
 }
