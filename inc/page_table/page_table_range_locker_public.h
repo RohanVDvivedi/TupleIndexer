c475b5f97c34a4436bd8786efb0fa0d050e0ce31
RohanVDvivedi <rohandvivedi@gmail.com>
2023-11-28 23:20:47 -0700

    implemented get_new for the page_table_range_locker

--- inc/page_table/page_table_range_locker_public.h
+++ inc/page_table/page_table_range_locker_public.h
@@ -9,9 +9,11 @@ typedef struct page_table_range_locker page_table_range_locker;
 
 // creates a new page_table_range_locker for the given range
 // you want to call set then the lock_type must be WRITE_LOCK
+// on abort_error, NULL is returned
 page_table_range_locker* get_new_page_table_range_locker(uint64_t root_page_id, page_table_bucket_range lock_range, int lock_type, const page_table_tuple_defs* pttd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
 
 // minimizes the lock range of the range_locker
+// on an abort error, lock on the local root is released, then you only need to call delete_page_table_range_locker
 int minimize_lock_range_for_page_table_range_locker(page_table_range_locker* ptrl_p, page_table_bucket_range lock_range, const void* transaction_id, int* abort_error);
 
 // get lock range for the page_table_range_locker
@@ -22,9 +24,11 @@ page_table_bucket_range get_lock_range_for_page_table_range_locker(const page_ta
 int is_writable_page_table_range_locker(const page_table_range_locker* ptrl_p);
 
 // you may only get, if the bucket_id is within get_lock_range_for_page_table_range_locker()
+// on an abort error, lock on the local root is released, then you only need to call delete_page_table_range_locker
 uint64_t get_from_page_table(page_table_range_locker* ptrl_p, uint64_t bucket_id, const void* transaction_id, int* abort_error);
 
 // you may only set, if the bucket_id is within get_lock_range_for_page_table_range_locker() and if the ptrl is writable, returns 0 other wise
+// on an abort error, lock on the local root is released, then you only need to call delete_page_table_range_locker
 int set_in_page_table(page_table_range_locker* ptrl_p, uint64_t bucket_id, uint64_t page_id, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
 
 // deletes the page_table_range_locker, and releases lock on the local_root (if it is not NULL_persistent_page)
--- src/page_table/page_table_range_locker.c
+++ src/page_table/page_table_range_locker.c
@@ -10,7 +10,34 @@ page_table_range_locker* get_new_page_table_range_locker(uint64_t root_page_id, 
 	if(!is_valid_page_table_bucket_range(&lock_range))
 		return NULL;
 
-	// TODO
+	// the following 2 must be present
+	if(pttd_p == NULL || pam_p == NULL)
+		return NULL;
+
+	page_table_range_locker* ptrl_p = malloc(sizeof(page_table_range_locker));
+
+	// start initializing the ptrl, making it point to and lock the actual root of the page_table
+	ptrl_p->delegated_local_root_range = (page_table_bucket_range){0, UINT64_MAX};
+	ptrl_p->max_local_root_level = pttd_p->max_page_table_height - 1;
+	ptrl_p->local_root = acquire_persistent_page_with_lock(ptrl_p->pam_p, transaction_id, root_page_id, lock_type, abort_error);
+	if(*abort_error)
+	{
+		free(ptrl_p);
+		return NULL;
+	}
+	ptrl_p->root_page_id = root_page_id;
+	ptrl_p->pttd_p = pttd_p;
+	ptrl_p->pam_p = pam_p;
+
+	// minimize the lock range, from [0,UINT64_MAX] to lock_range
+	minimize_lock_range_for_page_table_range_locker(ptrl_p, lock_range, transaction_id, abort_error);
+	if(*abort_error)
+	{
+		free(ptrl_p);
+		return NULL;
+	}
+
+	return ptrl_p;
 }
 
 int minimize_lock_range_for_page_table_range_locker(page_table_range_locker* ptrl_p, page_table_bucket_range lock_range, const void* transaction_id, int* abort_error)
