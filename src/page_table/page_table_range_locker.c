f43f9f1c62e33caef191bbd7ab35ecdecfc64f7c
RohanVDvivedi <rohandvivedi@gmail.com>
2023-11-28 21:15:12 -0700

    selected 3 simple functions for implementation in ptrl

--- src/page_table/page_table_range_locker.c
+++ src/page_table/page_table_range_locker.c
@@ -1,12 +1,21 @@
 #include<page_table_range_locker.h>
 
-page_table_range_locker* get_new_page_table_range_locker(uint64_t root_page_id, page_table_bucket_range lock_range, int lock_type, const page_table_tuple_defs* pttd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
+page_table_range_locker* get_new_page_table_range_locker(uint64_t root_page_id, page_table_bucket_range lock_range, int lock_type, const page_table_tuple_defs* pttd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error)
+{
+	// TODO
+}
 
 int minimize_lock_range_for_page_table_range_locker(page_table_range_locker* ptrl_p, page_table_bucket_range lock_range, const void* transaction_id, int* abort_error);
 
-page_table_bucket_range get_lock_range_for_page_table_range_locker(const page_table_range_locker* ptrl_p);
+page_table_bucket_range get_lock_range_for_page_table_range_locker(const page_table_range_locker* ptrl_p)
+{
+	// TODO
+}
 
-int is_writable_page_table_range_locker(const page_table_range_locker* ptrl_p);
+int is_writable_page_table_range_locker(const page_table_range_locker* ptrl_p)
+{
+	// TODO
+}
 
 uint64_t get_from_page_table(page_table_range_locker* ptrl_p, uint64_t bucket_id, const void* transaction_id, int* abort_error);
 
