a786012872c9ecc686f60175234070c685078e6e
RohanVDvivedi <rohandvivedi@gmail.com>
2023-11-29 23:33:37 -0700

    added test cases to set and then print

--- test/test_page_table.c
+++ test/test_page_table.c
@@ -50,6 +50,28 @@ int main()
 	// print the constructed page table
 	print_page_table(root_page_id, 0, &pttd, pam_p, transaction_id, &abort_error);
 
+	page_table_range_locker* ptrl_p = get_new_page_table_range_locker(root_page_id, WHOLE_PAGE_TABLE_BUCKET_RANGE, WRITE_LOCK, &pttd, pam_p, transaction_id, &abort_error);
+	if(abort_error)
+	{
+		printf("ABORTED\n");
+		exit(-1);
+	}
+
+	for(uint64_t i = 0; i < 10000; i += 500)
+	{
+		set_in_page_table(ptrl_p, i, i, pmm_p, transaction_id, &abort_error);
+
+		// print the constructed page table
+		print_page_table(root_page_id, 0, &pttd, pam_p, transaction_id, &abort_error);
+	}
+
+	delete_page_table_range_locker(ptrl_p, transaction_id, &abort_error);
+	if(abort_error)
+	{
+		printf("ABORTED\n");
+		exit(-1);
+	}
+
 	/* TESTS ENDED */
 
 	/* CLEANUP */
