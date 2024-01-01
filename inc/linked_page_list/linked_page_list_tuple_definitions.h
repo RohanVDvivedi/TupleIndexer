78fa3ab563dc3068da82ab0f68319d0dd569f9d6
RohanVDvivedi <rohandvivedi@gmail.com>
2024-01-01 16:13:52 -0700

    renamed attribute fields

--- inc/linked_page_list/linked_page_list_tuple_definitions.h
+++ inc/linked_page_list/linked_page_list_tuple_definitions.h
@@ -17,13 +17,13 @@ struct linked_page_list_tuple_defs
 // returns 1 for success, it fails with 0
 // it also fails if the pas_p does not pass is_valid_page_access_specs(pas_p)
 // it also fails if all of the record_def' records does not fit on the page
-int init_linked_page_list_tuple_definitions(linked_page_list_tuple_defs* pttd_p, const page_access_specs* pas_p, const tuple_def* record_def);
+int init_linked_page_list_tuple_definitions(linked_page_list_tuple_defs* lpltd_p, const page_access_specs* pas_p, const tuple_def* record_def);
 
 // it deallocates the record_def and
 // then resets all the page_table_tuple_defs struct attributes to NULL or 0
-void deinit_linked_page_list_tuple_definitions(linked_page_list_tuple_defs* pttd_p);
+void deinit_linked_page_list_tuple_definitions(linked_page_list_tuple_defs* lpltd_p);
 
 // print linked_page_list_tuple_defs
-void print_linked_page_list_tuple_definitions(linked_page_list_tuple_defs* pttd_p);
+void print_linked_page_list_tuple_definitions(linked_page_list_tuple_defs* lpltd_p);
 
 #endif
