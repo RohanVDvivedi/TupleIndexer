5154343ba7f997cf41a31ec8d41fc86437a63ab0
RohanVDvivedi <rohandvivedi@gmail.com>
2024-01-01 16:49:41 -0700

    implemented basic functions for lpl page_header

--- src/linked_page_list/linked_page_list_page_header.c
+++ src/linked_page_list/linked_page_list_page_header.c
@@ -1,10 +1,19 @@
 #include<linked_page_list_page_header.h>
 
-uint32_t get_offset_to_end_of_linked_page_list_page_header(const linked_page_list_tuple_defs* lpltd_p);
+uint32_t get_offset_to_end_of_linked_page_list_page_header(const linked_page_list_tuple_defs* lpltd_p)
+{
+	return get_offset_to_end_of_common_page_header(bpttd_p->pas_p) + (2 * lpltd_p->pas_p->page_id_width);
+}
 
-uint32_t get_level_of_linked_page_list_page(const persistent_page* ppage, const linked_page_list_tuple_defs* lpltd_p);
+uint32_t get_prev_page_id_of_linked_page_list_page(const persistent_page* ppage, const linked_page_list_tuple_defs* lpltd_p)
+{
+	return get_linked_page_list_page_header(ppage, lpltd_p).prev_page_id;
+}
 
-uint64_t get_first_bucket_id_of_linked_page_list_page(const persistent_page* ppage, const linked_page_list_tuple_defs* lpltd_p);
+uint64_t get_next_page_id_of_linked_page_list_page(const persistent_page* ppage, const linked_page_list_tuple_defs* lpltd_p);
+{
+	return get_linked_page_list_page_header(ppage, lpltd_p).next_page_id;
+}
 
 linked_page_list_page_header get_linked_page_list_page_header(const persistent_page* ppage, const linked_page_list_tuple_defs* lpltd_p);
 
