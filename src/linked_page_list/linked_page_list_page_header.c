800cbf8af3f801336556a3ef2da5862586415479
RohanVDvivedi <rohandvivedi@gmail.com>
2024-01-01 17:02:46 -0700

    implemented getter for lpl header

--- src/linked_page_list/linked_page_list_page_header.c
+++ src/linked_page_list/linked_page_list_page_header.c
@@ -26,7 +26,15 @@ static inline uint32_t get_offset_to_linked_page_list_page_header_locals(const l
 	return get_offset_to_end_of_common_page_header(lpltd_p->pas_p);
 }
 
-linked_page_list_page_header get_linked_page_list_page_header(const persistent_page* ppage, const linked_page_list_tuple_defs* lpltd_p);
+linked_page_list_page_header get_linked_page_list_page_header(const persistent_page* ppage, const linked_page_list_tuple_defs* lpltd_p)
+{
+	const void* linked_page_list_page_header_serial = get_page_header_ua_persistent_page(ppage, lpltd_p->pas_p->page_size) + get_offset_to_linked_page_list_page_header_locals(lpltd_p);
+	return (linked_page_list_page_header){
+		.parent = get_common_page_header(ppage, lpltd_p->pas_p),
+		.next_page_id = deserialize_uint64(linked_page_list_page_header_serial, lpltd_p->pas_p->page_id_width),
+		.prev_page_id = deserialize_uint64(linked_page_list_page_header_serial + lpltd_p->pas_p->page_id_width, lpltd_p->pas_p->page_id_width),
+	};
+}
 
 void serialize_linked_page_list_page_header(void* hdr_serial, const linked_page_list_page_header* lplph_p, const linked_page_list_tuple_defs* lpltd_p);
 
