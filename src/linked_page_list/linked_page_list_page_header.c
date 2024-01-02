809b0808a04339907d89c6dc9f8db32fb799222a
RohanVDvivedi <rohandvivedi@gmail.com>
2024-01-01 17:19:59 -0700

    implemented linked_page_list page_header complete

--- src/linked_page_list/linked_page_list_page_header.c
+++ src/linked_page_list/linked_page_list_page_header.c
@@ -45,6 +45,31 @@ void serialize_linked_page_list_page_header(void* hdr_serial, const linked_page_
 	serialize_uint64(linked_page_list_page_header_serial + lpltd_p->pas_p->page_id_width, lpltd_p->pas_p->page_id_width, lplph_p->prev_page_id);
 }
 
-void set_linked_page_list_page_header(persistent_page* ppage, const linked_page_list_page_header* lplph_p, const linked_page_list_tuple_defs* lpltd_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+void set_linked_page_list_page_header(persistent_page* ppage, const linked_page_list_page_header* lplph_p, const linked_page_list_tuple_defs* lpltd_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error)
+{
+	uint32_t page_header_size = get_page_header_size_persistent_page(ppage, lpltd_p->pas_p->page_size);
 
-void print_linked_page_list_page_header(const persistent_page* ppage, const linked_page_list_tuple_defs* lpltd_p);
+	// allocate memory, to hold complete page_header
+	void* hdr_serial = malloc(page_header_size);
+	if(hdr_serial == NULL)
+		exit(-1);
+
+	// copy the old page_header to it
+	memory_move(hdr_serial, get_page_header_ua_persistent_page(ppage, lpltd_p->pas_p->page_size), page_header_size);
+
+	// serialize lplph_p on the hdr_serial
+	serialize_linked_page_list_page_header(hdr_serial, lplph_p, lpltd_p);
+
+	// write hdr_serial to the new header position
+	set_persistent_page_header(pmm_p, transaction_id, ppage, lpltd_p->pas_p->page_size, hdr_serial, abort_error);
+
+	// we need to free hdr_serial, even on an abort_error
+	free(hdr_serial);
+}
+
+void print_linked_page_list_page_header(const persistent_page* ppage, const linked_page_list_tuple_defs* lpltd_p)
+{
+	print_common_page_header(ppage, lpltd_p->pas_p);
+	printf("next_page_id : %"PRIu64"\n", get_next_page_id_of_linked_page_list_page(ppage, lpltd_p));
+	printf("prev_page_id : %"PRIu64"\n", get_prev_page_id_of_linked_page_list_page(ppage, lpltd_p));
+}
