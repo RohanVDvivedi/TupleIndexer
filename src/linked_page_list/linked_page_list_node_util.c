68751250a0c0b2f4e8b4d55ba6ecbaa9f7c98a0c
RohanVDvivedi <rohandvivedi@gmail.com>
2024-02-03 19:45:19 +0530

    implemented init function for linked page list page

--- src/linked_page_list/linked_page_list_node_util.c
+++ src/linked_page_list/linked_page_list_node_util.c
@@ -1,6 +1,26 @@
 #include<linked_page_list_node_util.h>
 
-int init_linked_page_list_page(persistent_page* ppage, const linked_page_list_tuple_defs* lpltd_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+#include<linked_page_list_page_header.h>
+
+#include<persistent_page_functions.h>
+
+int init_linked_page_list_page(persistent_page* ppage, const linked_page_list_tuple_defs* lpltd_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error)
+{
+	int inited = init_persistent_page(pmm_p, transaction_id, ppage, lpltd_p->pas_p->page_size, sizeof_LINKED_PAGE_LIST_PAGE_HEADER(lpltd_p), &(lpltd_p->record_def->size_def), abort_error);
+	if((*abort_error) || !inited)
+		return 0;
+
+	// get the header, initialize it and set it back on to the page
+	linked_page_list_page_header hdr = get_linked_page_list_page_header(ppage, lpltd_p);
+	hdr.parent.type = LINKED_PAGE_LIST_PAGE;
+	hdr.next_page_id = lpltd_p->pas_p->NULL_PAGE_ID;
+	hdr.prev_page_id = lpltd_p->pas_p->NULL_PAGE_ID;
+	set_linked_page_list_page_header(ppage, &hdr, lpltd_p, pmm_p, transaction_id, abort_error);
+	if((*abort_error))
+		return 0;
+
+	return 1;
+}
 
 int is_next_of_in_linked_page_list(const persistent_page* ppage, const persistent_page* ppage_test_next, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
 
