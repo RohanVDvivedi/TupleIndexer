cd728ea3623cf2b07dbbc2490d2c5c528650958d
RohanVDvivedi <rohandvivedi@gmail.com>
2024-02-03 19:22:32 +0530

    added const where ever necessary

--- inc/linked_page_list/linked_page_list_node_util.h
+++ inc/linked_page_list/linked_page_list_node_util.h
@@ -11,19 +11,19 @@ int init_linked_page_list_page(persistent_page* ppage, const linked_page_list_tu
 
 // test if the ppage_test is next/prev of the ppage, i.e. test if ppage->next/prev == ppage_test_*
 // only page_id of page_test_* is used
-int is_next_of_in_linked_page_list(persistent_page* ppage, persistent_page* ppage_test_next, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
-int is_prev_of_in_linked_page_list(persistent_page* ppage, persistent_page* ppage_test_prev, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
+int is_next_of_in_linked_page_list(const persistent_page* ppage, const persistent_page* ppage_test_next, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
+int is_prev_of_in_linked_page_list(const persistent_page* ppage, const persistent_page* ppage_test_prev, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
 
 // test if ppage_head->next = ppage_head and ppage_head->prev = ppage_head
-int is_singular_head_linked_page_list(persistent_page* ppage_head, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
+int is_singular_head_linked_page_list(const persistent_page* ppage_head, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
 
 // true, if not is_singular_head_linked_page_list AND ppage_head->next == ppage_head->prev
-int is_dual_node_linked_page_list(persistent_page* ppage_head, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
+int is_dual_node_linked_page_list(const persistent_page* ppage_head, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
 
 // lock and get next or previous of ppage, you need to ensure that the next or previous of the ppage is not already locked by you
 // returns NULL_PERSISTENT_PAGE, if the linked_page_list is is_singular_head_linked_page_list
-persistent_page lock_and_get_next_ppage_in_linked_page_list(persistent_page* ppage, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
-persistent_page lock_and_get_prev_ppage_in_linked_page_list(persistent_page* ppage, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
+persistent_page lock_and_get_next_ppage_in_linked_page_list(const persistent_page* ppage, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
+persistent_page lock_and_get_prev_ppage_in_linked_page_list(const persistent_page* ppage, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
 
 // with the below functions you can use, even singular_head OR dual_node linked_page_lists
 
