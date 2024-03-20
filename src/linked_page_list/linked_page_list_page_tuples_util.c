07aa47564d2f973477574d00aff403033b4c0f9b
RohanVDvivedi <rohandvivedi@gmail.com>
2024-03-20 18:47:11 +0530

    implemented must_split fnctionality for linked_page_list

--- src/linked_page_list/linked_page_list_page_tuples_util.c
+++ src/linked_page_list/linked_page_list_page_tuples_util.c
@@ -11,7 +11,20 @@
 
 int must_split_for_insert_linked_page_list_page(const persistent_page* page1, const void* tuple_to_insert, const linked_page_list_tuple_defs* lpltd_p)
 {
-	// TODO
+	// do not perform a split if the page can accomodate the new tuple
+	if(can_append_tuple_on_persistent_page(page1, lpltd_p->pas_p->page_size, &(lpltd_p->record_def->size_def), tuple_to_insert))
+		return 0;
+
+	// we need to make sure that the new_tuple will not be fitting on the page even after a compaction
+	// if tuple_to_insert can fit on the page after a compaction, then you should not be splitting the page
+	uint32_t space_to_be_occupied_by_new_tuple = get_space_to_be_occupied_by_tuple_on_persistent_page(lpltd_p->pas_p->page_size, &(lpltd_p->record_def->size_def), tuple_to_insert);
+	uint32_t space_available_page1 = get_space_allotted_to_all_tuples_on_persistent_page(page1, lpltd_p->pas_p->page_size, &(lpltd_p->record_def->size_def)) - get_space_occupied_by_all_tuples_on_persistent_page(page1, lpltd_p->pas_p->page_size, &(lpltd_p->record_def->size_def));
+
+	// we fail here because the new tuple can be accomodated in page1, if you had considered compacting the page
+	if(space_available_page1 >= space_to_be_occupied_by_new_tuple)
+		return 0;
+
+	return 1;
 }
 
 int can_merge_linked_page_list_pages(const persistent_page* page1, const persistent_page* page2, const linked_page_list_tuple_defs* lpltd_p)
