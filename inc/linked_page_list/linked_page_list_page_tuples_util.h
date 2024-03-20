523125bde9fc8e44c5fbf2bc4acb9c24ac5ca41c
RohanVDvivedi <rohandvivedi@gmail.com>
2024-03-20 17:13:41 +0530

    added declarations for the can_split and can_merge functions

--- inc/linked_page_list/linked_page_list_page_tuples_util.h
+++ inc/linked_page_list/linked_page_list_page_tuples_util.h
@@ -6,4 +6,21 @@
 **	while the linked_page_list_page_tuples_util handles managing tuples (splits/merges) on the page of a linked_page_list
 */
 
+//#include<stdint.h>
+
+#include<persistent_page.h>
+#include<linked_page_list_tuple_definitions.h>
+//#include<opaque_page_access_methods.h>
+//#include<opaque_page_modification_methods.h>
+
+// check if a linked_page_list page must split for an insertion of a tuple on the page
+int must_split_for_insert_linked_page_list_page(const persistent_page* page1, const void* tuple_to_insert, const linked_page_list_tuple_defs* lpltd_p);
+
+//persistent_page split_insert_bplus_tree_interior_page(persistent_page* page1, const void* tuple_to_insert, uint32_t tuple_to_insert_at, int split_upper_half/lower_half, int vacant_in_upper_half/vacant_page2/OR_equal_split, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+
+// check if 2 linked_page_list pages can be merged
+int can_merge_linked_page_list_pages(const persistent_page* page1, const persistent_page* page2, const linked_page_list_tuple_defs* lpltd_p);
+
+//int merge_linked_page_list_pages(persistent_page* page1, persistent_page* page2, int merge_into_page2/page1, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+
 #endif
