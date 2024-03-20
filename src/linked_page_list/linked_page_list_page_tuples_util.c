519abafcdc342553b9c0bfed988ff5a1b1d8db38
RohanVDvivedi <rohandvivedi@gmail.com>
2024-03-20 18:24:56 +0530

    started implementing must_split and can_merge functions

--- src/linked_page_list/linked_page_list_page_tuples_util.c
+++ src/linked_page_list/linked_page_list_page_tuples_util.c
@@ -1,1 +1,11 @@
 #include<linked_page_list_page_tuples_util.h>
+
+int must_split_for_insert_linked_page_list_page(const persistent_page* page1, const void* tuple_to_insert, const linked_page_list_tuple_defs* lpltd_p)
+{
+	// TODO
+}
+
+int can_merge_linked_page_list_pages(const persistent_page* page1, const persistent_page* page2, const linked_page_list_tuple_defs* lpltd_p)
+{
+	// TODO
+}
