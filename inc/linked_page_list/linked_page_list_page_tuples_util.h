96e5aa4abb2f374e4255a849c1abb2be6f514bb1
RohanVDvivedi <rohandvivedi@gmail.com>
2024-03-21 23:07:49 +0530

    started declaring the merge function for linked_page_list_page

--- inc/linked_page_list/linked_page_list_page_tuples_util.h
+++ inc/linked_page_list/linked_page_list_page_tuples_util.h
@@ -10,17 +10,23 @@
 
 #include<persistent_page.h>
 #include<linked_page_list_tuple_definitions.h>
-//#include<opaque_page_access_methods.h>
-//#include<opaque_page_modification_methods.h>
+#include<opaque_page_access_methods.h>
+#include<opaque_page_modification_methods.h>
 
 // check if a linked_page_list page must split for an insertion of a tuple on the page
 int must_split_for_insert_linked_page_list_page(const persistent_page* page1, const void* tuple_to_insert, const linked_page_list_tuple_defs* lpltd_p);
 
 //persistent_page split_insert_bplus_tree_interior_page(persistent_page* page1, const void* tuple_to_insert, uint32_t tuple_to_insert_at, int split_upper_half/lower_half, int vacant_in_upper_half/vacant_page2/OR_equal_split, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
 
+// for merging page1->next_page == page2 and page2->prev_page == page1
+
 // check if 2 linked_page_list pages can be merged
 int can_merge_linked_page_list_pages(const persistent_page* page1, const persistent_page* page2, const linked_page_list_tuple_defs* lpltd_p);
 
-//int merge_linked_page_list_pages(persistent_page* page1, persistent_page* page2, int merge_into_page2/page1, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+// if MERGE_INTO_PAGE1 is called, then upon a successfull merge tuples of page2 are copied at the end of page1
+// else uf MERGE_INTO_PAGE2 is called, then upon a successfull merge tuples of page1 are copied at the beginning of page2
+#define MERGE_INTO_PAGE1 0
+#define MERGE_INTO_PAGE2 1
+int merge_linked_page_list_pages(persistent_page* page1, persistent_page* page2, int merge_into, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
 
 #endif
