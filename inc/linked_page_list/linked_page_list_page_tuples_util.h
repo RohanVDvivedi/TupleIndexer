35cf6b9f095d515368e266249ce1aa8266d9a63c
RohanVDvivedi <rohandvivedi@gmail.com>
2024-03-22 21:30:54 +0530

    added declaration for the split_insert function

--- inc/linked_page_list/linked_page_list_page_tuples_util.h
+++ inc/linked_page_list/linked_page_list_page_tuples_util.h
@@ -16,7 +16,23 @@
 // check if a linked_page_list page must split for an insertion of a tuple on the page
 int must_split_for_insert_linked_page_list_page(const persistent_page* page1, const void* tuple_to_insert, const linked_page_list_tuple_defs* lpltd_p);
 
-//persistent_page split_insert_bplus_tree_interior_page(persistent_page* page1, const void* tuple_to_insert, uint32_t tuple_to_insert_at, int split_upper_half/lower_half, int vacant_in_upper_half/vacant_page2/OR_equal_split, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+// it performs a split insert to the linked_page_list page provided
+// you may call this function only if you are sure that the new_tuple will not fit on the page even after a compaction
+// it returns 0, on failure if the tuple was not inserted, and the split was not performed
+// failure to allocate a new page, may result in a failure (abort error)
+// lock on page1 is not released, and on success a locked page2 is returned
+// The split_type parameter suggests, which of the lower_half or upper_half of the tuples go to the new page
+// The split organization decides the tuple percentage that go to the new page
+// Even after the successful return of this function, the new page is not added to the linked_page_list in context, as explained earlier, this is the job of linked_page_list_node_util source and header files
+// It is you who need to add new_page after the page1 in case of SPLIT_LOWER_HALF, else before page1 in case of SPLIT_UPPER_HALF
+// split type
+#define SPLIT_LOWER_HALF 0 // lower_half of tuples of page1 go to new_page
+#define SPLIT_UPPER_HALF 1 // upper_half of tuples of page1 go to new_page
+// split organization
+#define EQUAL_SPLIT     0
+#define FULL_UPPER_HALF 1
+#define FULL_LOWER_HALF 2
+persistent_page split_insert_bplus_tree_interior_page(persistent_page* page1, const void* tuple_to_insert, uint32_t tuple_to_insert_at, int split_type, int split_organization, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
 
 // for merging page1->next_page == page2 and page2->prev_page == page1
 
