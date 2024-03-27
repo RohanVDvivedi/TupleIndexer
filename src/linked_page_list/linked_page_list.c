afd77c87069496d13983dc9401ac5eb8422505fe
RohanVDvivedi <rohandvivedi@gmail.com>
2024-03-27 23:29:44 +0530

    implemented basic loop logic to print the linked_page_list

--- src/linked_page_list/linked_page_list.c
+++ src/linked_page_list/linked_page_list.c
@@ -56,3 +56,48 @@ int destroy_linked_page_list(uint64_t head_page_id, const linked_page_list_tuple
 
 	return 1;
 }
+
+void print_linked_page_list(uint64_t head_page_id, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error)
+{
+	// lock the head page, it always exists
+	persistent_page ppage = acquire_persistent_page_with_lock(pam_p, transaction_id, head_page_id, READ_LOCK, abort_error);
+	if(*abort_error)
+		return 0;
+
+	while(1)
+	{
+		// print the current ppage
+		// TODO
+
+		// get it's next_page_id
+		uint64_t next_page_id = get_next_page_id_of_linked_page_list_page(&ppage, lpltd_p);
+
+		if(next_page_id == head_page_id) // i.e. this ppage is the tail page, so this was the last page to be processed
+		{
+			release_lock_on_persistent_page(pam_p, transaction_id, &ppage, NONE_OPTION, abort_error);
+			if(*abort_error)
+				return 0;
+			break;
+		}
+		else
+		{
+			next_ppage = acquire_persistent_page_with_lock(pam_p, transaction_id, next_page_id, READ_LOCK, abort_error);
+			if(*abort_error)
+			{
+				release_lock_on_persistent_page(pam_p, transaction_id, &ppage, NONE_OPTION, abort_error);
+				return 0;
+			}
+
+			release_lock_on_persistent_page(pam_p, transaction_id, &ppage, NONE_OPTION, abort_error);
+			if(*abort_error)
+			{
+				release_lock_on_persistent_page(pam_p, transaction_id, &next_ppage, NONE_OPTION, abort_error);
+				return 0;
+			}
+
+			ppage = next_ppage;
+		}
+	}
+
+	return 1;
+}
