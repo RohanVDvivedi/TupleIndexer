270b68a1115e7f1a5de85a9982234311f2d41ae8
RohanVDvivedi <rohandvivedi@gmail.com>
2024-04-12 23:23:40 +0530

    reverting to default test case

--- test/test_bplus_tree.c
+++ test/test_bplus_tree.c
@@ -15,13 +15,13 @@
 #include<executor.h>
 
 // uncomment based on the keys that you want to test with
-//#define KEY_NAME_EMAIL
+#define KEY_NAME_EMAIL
 //#define KEY_INDEX_PHONE
 //#define KEY_PHONE_SCORE
 //#define KEY_EMAIL_AGE_SEX
 //#define KEY_SEX_EMAIL
 //#define KEY_SCORE_INDEX
-#define KEY_SCORE_NAME
+//#define KEY_SCORE_NAME
 
 #define TEST_DATA_FILE         "./testdata.csv"
 #define TEST_DATA_RANDOM_FILE  "./testdata_random.csv"
