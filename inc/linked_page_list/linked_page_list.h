ce04b762c8b514630af58e56a41967b1b1b5e6aa
RohanVDvivedi <rohandvivedi@gmail.com>
2024-04-25 00:03:17 +0530

    made linked_page_list_iterator public api

--- inc/linked_page_list/linked_page_list.h
+++ inc/linked_page_list/linked_page_list.h
@@ -16,4 +16,6 @@ int destroy_linked_page_list(uint64_t head_page_id, const linked_page_list_tuple
 // it may return an abort_error, unable to print all of the linked_page_list pages
 void print_linked_page_list(uint64_t head_page_id, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
 
+#include<linked_page_list_iterator_public.h>
+
 #endif
