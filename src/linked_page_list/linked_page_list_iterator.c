fa61855475c4ab891ce0d2a958821f075f643cfb
RohanVDvivedi <rohandvivedi@gmail.com>
2024-04-24 23:51:27 +0530

    added logic to ensure if the iterator is pointing to the head or tail page

--- src/linked_page_list/linked_page_list_iterator.c
+++ src/linked_page_list/linked_page_list_iterator.c
@@ -2,6 +2,7 @@
 
 #include<persistent_page_functions.h>
 #include<linked_page_list_node_util.h>
+#include<linked_page_list_page_header.h>
 
 #include<stdlib.h>
 
@@ -45,18 +46,25 @@ linked_page_list_state get_state_for_linked_page_list(const linked_page_list_ite
 		return MANY_NODE_LINKED_PAGE_LIST;
 }
 
-// if state == HEAD_ONLY_LINKED_PAGE_LIST and the curr_page has tuple_count == 0
 int is_empty_linked_page_list(const linked_page_list_iterator* lpli_p)
 {
+	// check if the only page of the linked_page_list is empty
 	return HEAD_ONLY_LINKED_PAGE_LIST == get_state_for_linked_page_list(lpli_p)
 		&& 0 == get_tuple_count_on_persistent_page(&(lpli_p->curr_page), lpli_p->lpltd_p->pas_p->page_size, &(lpli_p->lpltd_p->record_def->size_def));
 }
 
-// curr_page.page_id == head_page_id
-int is_at_head_page_linked_page_list_iterator(const linked_page_list_iterator* lpli_p);
+int is_at_head_page_linked_page_list_iterator(const linked_page_list_iterator* lpli_p)
+{
+	// check if the page_id of curr_page is head_page_id
+	return lpli_p->curr_page.page_id == lpli_p->head_page_id;
+}
 
-// curr_page.next_page_id = head_page_id
-int is_at_tail_page_linked_page_list_iterator(const linked_page_list_iterator* lpli_p);
+int is_at_tail_page_linked_page_list_iterator(const linked_page_list_iterator* lpli_p)
+{
+	// check if the next_page_id of curr_page is head_page_id
+	linked_page_list_page_header hdr = get_linked_page_list_page_header(&(lpli_p->curr_page), lpli_p->lpltd_p);
+	return hdr.next_page_id == lpli_p->head_page_id;
+}
 
 // !is_empty && is_at_head_page && curr_tuple_index == 0
 int is_at_head_tuple_linked_page_list_iterator(const linked_page_list_iterator* lpli_p);
