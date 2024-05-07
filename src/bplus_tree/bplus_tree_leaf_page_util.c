3de0af110c023f43846a9d3172862a40a156b248
RohanVDvivedi <rohandvivedi@gmail.com>
2024-05-07 21:01:03 +0530

    started refactoring

--- src/bplus_tree/bplus_tree_leaf_page_util.c
+++ src/bplus_tree/bplus_tree_leaf_page_util.c
@@ -140,6 +140,9 @@ int must_split_for_insert_bplus_tree_leaf_page(const persistent_page* page1, con
 // for this function it is assumed, that last_tuple_page1 <= first_tuple_page2, on comparing key elements (at indices key_element_ids) sorted by key_compare_direction
 static int build_index_entry_from_record_tuples_for_split(const bplus_tree_tuple_defs* bpttd_p, const void* last_tuple_page1, const void* first_tuple_page2, uint64_t child_page_id, void* index_entry)
 {
+	// directly build_index_entry from the (first_tuple_page2, child_page_id)
+	return build_index_entry_from_record_tuple(bpttd_p, first_tuple_page2, child_page_id, index_entry);
+
 	// init the index_entry
 	init_tuple(bpttd_p->index_def, index_entry);
 
