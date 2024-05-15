1cd9e5b9a0d6588facc1945a1682e4c1c5293abf
RohanVDvivedi <rohandvivedi@gmail.com>
2024-05-16 00:06:11 +0530

    started refactoring the linked_page_list_iterator with head locking at all times

--- inc/linked_page_list/linked_page_list_iterator.h
+++ inc/linked_page_list/linked_page_list_iterator.h
@@ -6,17 +6,24 @@
 #include<opaque_page_access_methods.h>
 #include<opaque_page_modification_methods.h>
 
+typedef struct persistent_page_reference persistent_page_reference;
+struct persistent_page_reference
+{
+	int points_to_iterator_head;	// if set to 1, then the linked_page_list_iterator's head_page is what this reference points to
+	persistent_page non_head_page;	// else, this is what it will point to.
+};
+
 typedef struct linked_page_list_iterator linked_page_list_iterator;
 struct linked_page_list_iterator
 {
 	// current page that we are pointing to
-	persistent_page curr_page;
+	persistent_page_reference curr_page;
 
 	// tuple we are pointing at
 	uint32_t curr_tuple_index;
 
-	// head_page_id never changes, this is the head_page_id of the linked_page_list
-	uint64_t head_page_id;
+	// head_page never changes and is never freed, this is the head_page of the linked_page_list
+	persistent_page head_page;
 
 	const linked_page_list_tuple_defs* lpltd_p;
 
