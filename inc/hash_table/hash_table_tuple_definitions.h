4bb9ea34b0962ecdd41841555e54adba305442b2
RohanVDvivedi <rohandvivedi@gmail.com>
2024-05-23 23:45:50 +0530

    minor change

--- inc/hash_table/hash_table_tuple_definitions.h
+++ inc/hash_table/hash_table_tuple_definitions.h
@@ -33,7 +33,7 @@ struct hash_table_tuple_defs
 // it relies on lpltd and pttd for most of its fnctionality
 // returns 1 for success, it fails with 0, if the record_def has element_count 0 OR key_element_count == 0 OR key_element_ids == NULL OR if any of the key_element_ids is out of bounds
 // it also fails if the pas_p does not pass is_valid_page_access_specs(pas_p)
-int init_hash_table_tuple_definitions(hash_table_tuple_defs* httd_p, const page_access_specs* pas_p, const tuple_def* record_def, const uint32_t* key_element_ids, const compare_direction* key_compare_direction, uint32_t key_element_count);
+int init_hash_table_tuple_definitions(hash_table_tuple_defs* httd_p, const page_access_specs* pas_p, const tuple_def* record_def, const uint32_t* key_element_ids, uint32_t key_element_count);
 
 // checks to see if a record_tuple can be inserted into a hash_table
 // note :: you can not insert a NULL record in hash_table
