301868e7f03816697eafa3c930584c1080977030
RohanVDvivedi <rohandvivedi@gmail.com>
2024-06-01 21:58:16 +0530

    delete functionality completed

--- src/hash_table/hash_table_iterator.c
+++ src/hash_table/hash_table_iterator.c
@@ -1,5 +1,7 @@
 #include<hash_table_iterator.h>
 
+#include<linked_page_list.h>
+
 #include<stdlib.h>
 
 hash_table_iterator* get_new_hash_table_iterator(uint64_t root_page_id, page_table_bucket_range bucket_range, const void* key, const hash_table_tuple_defs* httd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error)
@@ -149,11 +151,64 @@ void delete_hash_table_iterator(hash_table_iterator* hti_p, const void* transact
 		if(*abort_error)
 			goto DELETE_EVERYTHING_AND_ABORT;
 
-		// TODO
-		// get range locker
+		// take a range lock on the page table, to get the bucket_count
+		hti_p->ptrl_p = get_new_page_table_range_locker(hti_p->root_page_id, WHOLE_PAGE_TABLE_BUCKET_RANGE, &(hti_p->httd_p->pttd), hti_p->pam_p, hti_p->pmm_p, transaction_id, abort_error);
+		if(*abort_error)
+			goto DELETE_EVERYTHING_AND_ABORT;
+
+		// get the current bucket_count of the hash_table
+		uint64_t bucket_count = UINT64_MAX;
+		find_non_NULL_PAGE_ID_in_page_table(hti_p->ptrl_p, &bucket_count, LESSER_THAN_EQUALS, transaction_id, abort_error);
+		if(*abort_error)
+			goto DELETE_EVERYTHING_AND_ABORT;
+
 		// go to the bucket that key points to
-		// if the corresponding linked_page_list is empty, then delete it and set it's entry in page_table as NULL
+		hti_p->curr_bucket_id = get_bucket_index_for_key_using_hash_table_tuple_definitions(hti_p->httd_p, hti_p->key, bucket_count);
+
+		// 
+		uint64_t curr_bucket_head_page_id = get_from_page_table(hti_p->ptrl_p, hti_p->curr_bucket_id, transaction_id, abort_error);
+		if(*abort_error)
+			goto DELETE_EVERYTHING_AND_ABORT;
+		if(curr_bucket_head_page_id != hti_p->httd_p->pttd.pas_p->NULL_PAGE_ID)
+		{
+			// open a linked_page_list_iterator at bucket_head_page_id
+			hti_p->lpli_p = get_new_linked_page_list_iterator(curr_bucket_head_page_id, &(hti_p->httd_p->lpltd), hti_p->pam_p, hti_p->pmm_p, transaction_id, abort_error);
+			if(*abort_error)
+				goto DELETE_EVERYTHING_AND_ABORT;
+
+			// if the corresponding linked_page_list exists and is empty, then delete it and set it's entry in page_table as NULL
+			if(is_empty_linked_page_list(hti_p->lpli_p))
+			{
+				// release lock on hti_p->lpli_p
+				delete_linked_page_list_iterator(hti_p->lpli_p, transaction_id, abort_error);
+				hti_p->lpli_p = NULL;
+				if(*abort_error)
+					goto DELETE_EVERYTHING_AND_ABORT;
+
+				// destroy the linked_page_list
+				destroy_linked_page_list(curr_bucket_head_page_id, &(hti_p->httd_p->lpltd), hti_p->pam_p, transaction_id, abort_error);
+				if(*abort_error)
+					goto DELETE_EVERYTHING_AND_ABORT;
+
+				set_in_page_table(hti_p->ptrl_p, hti_p->curr_bucket_id, hti_p->httd_p->pttd.pas_p->NULL_PAGE_ID, transaction_id, abort_error);
+				if(*abort_error)
+					goto DELETE_EVERYTHING_AND_ABORT;
+			}
+			else
+			{
+				// release lock on hti_p->lpli_p
+				delete_linked_page_list_iterator(hti_p->lpli_p, transaction_id, abort_error);
+				hti_p->lpli_p = NULL;
+				if(*abort_error)
+					goto DELETE_EVERYTHING_AND_ABORT;
+			}
+		}
+
 		// delete range locker
+		delete_page_table_range_locker(hti_p->ptrl_p, transaction_id, abort_error);
+		hti_p->ptrl_p = NULL;
+		if(*abort_error)
+			goto DELETE_EVERYTHING_AND_ABORT;
 	}
 
 	DELETE_EVERYTHING_AND_ABORT:;
