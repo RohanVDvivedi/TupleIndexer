395fe7deb32251d194bd00c6ed27918a89a12f66
RohanVDvivedi <rohandvivedi@gmail.com>
2024-06-12 20:26:43 +0530

    started with merge fiunction for linked_page_list

--- src/linked_page_list/linked_page_list.c
+++ src/linked_page_list/linked_page_list.c
@@ -104,3 +104,8 @@ void print_linked_page_list(uint64_t head_page_id, const linked_page_list_tuple_
 		}
 	}
 }
+
+int merge_linked_page_lists(uint64_t lpl1_head_page_id, uint64_t lpl2_head_page_id, const linked_page_list_tuple_defs* lpltd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error)
+{
+	// TODO
+}
