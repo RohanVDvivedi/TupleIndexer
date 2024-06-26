f7961b4b73c0b4077f70790ec0bba9c8d0a10224
RohanVDvivedi <rohandvivedi@gmail.com>
2024-06-26 22:36:53 +0530

    minor bug fix, we need to ensure that the page header surely does fit on the page

--- src/linked_page_list/linked_page_list_tuple_definitions.c
+++ src/linked_page_list/linked_page_list_tuple_definitions.c
@@ -22,6 +22,11 @@ int init_linked_page_list_tuple_definitions(linked_page_list_tuple_defs* lpltd_p
 	// initialize page_access_specs fo the bpttd
 	lpltd_p->pas_p = pas_p;
 
+	// this can only be done after setting the pas_p attribute of lpltd
+	// there must be room for atleast some bytes after the page_table_page_header
+	if(!can_page_header_fit_on_persistent_page(sizeof_LINKED_PAGE_LIST_PAGE_HEADER(lpltd_p), lpltd_p->pas_p->page_size))
+		return 0;
+
 	// check if the record_def's record's min_size fits on half of the page
 	uint32_t space_allotted_for_records = get_space_to_be_allotted_to_all_tuples_on_persistent_page(sizeof_LINKED_PAGE_LIST_PAGE_HEADER(lpltd_p), pas_p->page_size, &(record_def->size_def));
 	uint32_t space_additional_for_record = get_additional_space_overhead_per_tuple_on_persistent_page(pas_p->page_size, &(record_def->size_def));
