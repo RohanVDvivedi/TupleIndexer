fa3fb3e17e35ddbda40450ca88e85a1c66641cc2
RohanVDvivedi <rohandvivedi@gmail.com>
2024-07-07 20:56:08 +0530

    page_table_bucket_range is now bucket_range

--- inc/utils/bucket_range.h
+++ inc/utils/bucket_range.h
@@ -1,42 +1,46 @@
-#ifndef PAGE_TABLE_BUCKET_RANGE_H
-#define PAGE_TABLE_BUCKET_RANGE_H
+#ifndef BUCKET_RANGE_H
+#define BUCKET_RANGE_H
 
 #include<stdint.h>
 
-// this is an inclusive range of the bucket_id on values in the page_table
+/*
+	used for indexing in array_table, page_table, hash_table and the runs inside sorter
+*/
+
+// this is an inclusive range of the bucket_id on values in the array_table, page_table and hash_table (uses page_table), even the sorter uses page_table internally
 // from first_bucket_id to last_bucket_id both inclusive
 
-typedef struct page_table_bucket_range page_table_bucket_range;
-struct page_table_bucket_range
+typedef struct bucket_range bucket_range;
+struct bucket_range
 {
 	uint64_t first_bucket_id;
 	uint64_t last_bucket_id;
 };
 
 // bucket_range for a singular bucket
-#define PAGE_TABLE_BUCKET_RANGE_FOR_BUCKET(bucket_id_val) ((page_table_bucket_range){.first_bucket_id = bucket_id_val, .last_bucket_id = bucket_id_val})
+#define BUCKET_RANGE_FOR_BUCKET(bucket_id_val) ((bucket_range){.first_bucket_id = bucket_id_val, .last_bucket_id = bucket_id_val})
 
 // 64 bit range of all buckets in the page table
-#define WHOLE_PAGE_TABLE_BUCKET_RANGE ((page_table_bucket_range){.first_bucket_id = 0, .last_bucket_id = UINT64_MAX})
+#define WHOLE_BUCKET_RANGE ((bucket_range){.first_bucket_id = 0, .last_bucket_id = UINT64_MAX})
 
-// returns 1 if ptbr_p->first_bucket_id <= ptbr_p->last_bucket_id, else 0
-int is_valid_page_table_bucket_range(const page_table_bucket_range* ptbr_p);
+// returns 1 if br_p->first_bucket_id <= br_p->last_bucket_id, else 0
+int is_valid_bucket_range(const bucket_range* br_p);
 
 // obvious
-void print_page_table_bucket_range(const page_table_bucket_range* ptbr_p);
+void print_bucket_range(const bucket_range* br_p);
 
-// all the below functions assume that the ptbr's provided as parameters pass valid as per the above function's specifications
+// all the below functions assume that the br's provided as parameters pass valid as per the above function's specifications
 
-// returns 1, if the ptbr1 and ptbr2 are disjoint, i.e. no overlap
-int are_disjoint_page_table_bucket_range(const page_table_bucket_range* ptbr1_p, const page_table_bucket_range* ptbr2_p);
+// returns 1, if the br1 and br2 are disjoint, i.e. no overlap
+int are_disjoint_bucket_range(const bucket_range* br1_p, const bucket_range* br2_p);
 
-// returns !are_disjoint_page_table_bucket_range(ptbr1, ptbr2)
-int have_overlap_page_table_bucket_range(const page_table_bucket_range* ptbr1_p, const page_table_bucket_range* ptbr2_p);
+// returns !are_disjoint_bucket_range(br1, br2)
+int have_overlap_bucket_range(const bucket_range* br1_p, const bucket_range* br2_p);
 
 // returns true if the containee's range is completely contaned in the container's range
-int is_contained_page_table_bucket_range(const page_table_bucket_range* container_p, const page_table_bucket_range* containee_p);
+int is_contained_bucket_range(const bucket_range* container_p, const bucket_range* containee_p);
 
 // returns true if the containee bucket range is completely contaned in the container's range
-int is_bucket_contained_page_table_bucket_range(const page_table_bucket_range* container_p, uint64_t containee);
+int is_bucket_contained_bucket_range(const bucket_range* container_p, uint64_t containee);
 
 #endif
--- src/utils/bucket_range.c
+++ src/utils/bucket_range.c
@@ -1,35 +1,35 @@
-#include<page_table_bucket_range.h>
+#include<bucket_range.h>
 
 #include<inttypes.h>
 #include<stdio.h>
 
-int is_valid_page_table_bucket_range(const page_table_bucket_range* ptbr_p)
+int is_valid_bucket_range(const bucket_range* br_p)
 {
-	return ptbr_p->first_bucket_id <= ptbr_p->last_bucket_id;
+	return br_p->first_bucket_id <= br_p->last_bucket_id;
 }
 
-void print_page_table_bucket_range(const page_table_bucket_range* ptbr_p)
+void print_bucket_range(const bucket_range* br_p)
 {
-	printf("[%"PRIu64", %"PRIu64"]", ptbr_p->first_bucket_id, ptbr_p->last_bucket_id);
+	printf("[%"PRIu64", %"PRIu64"]", br_p->first_bucket_id, br_p->last_bucket_id);
 }
 
-int are_disjoint_page_table_bucket_range(const page_table_bucket_range* ptbr1_p, const page_table_bucket_range* ptbr2_p)
+int are_disjoint_bucket_range(const bucket_range* br1_p, const bucket_range* br2_p)
 {
 	// it is either range of 1 comes before 2 or when range of 2 comes before 1
-	return (ptbr1_p->last_bucket_id < ptbr2_p->first_bucket_id) || (ptbr2_p->last_bucket_id < ptbr1_p->first_bucket_id);
+	return (br1_p->last_bucket_id < br2_p->first_bucket_id) || (br2_p->last_bucket_id < br1_p->first_bucket_id);
 }
 
-int have_overlap_page_table_bucket_range(const page_table_bucket_range* ptbr1_p, const page_table_bucket_range* ptbr2_p)
+int have_overlap_bucket_range(const bucket_range* br1_p, const bucket_range* br2_p)
 {
-	return !are_disjoint_page_table_bucket_range(ptbr1_p, ptbr2_p);
+	return !are_disjoint_bucket_range(br1_p, br2_p);
 }
 
-int is_contained_page_table_bucket_range(const page_table_bucket_range* container_p, const page_table_bucket_range* containee_p)
+int is_contained_bucket_range(const bucket_range* container_p, const bucket_range* containee_p)
 {
 	return (container_p->first_bucket_id <= containee_p->first_bucket_id) && (containee_p->last_bucket_id <= container_p->last_bucket_id);
 }
 
-int is_bucket_contained_page_table_bucket_range(const page_table_bucket_range* container_p, uint64_t containee)
+int is_bucket_contained_bucket_range(const bucket_range* container_p, uint64_t containee)
 {
 	return (container_p->first_bucket_id <= containee) && (containee <= container_p->last_bucket_id);
 }
