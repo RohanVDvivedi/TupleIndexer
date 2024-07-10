5ac1b7dc55889ef75b2fc4f89163df7c6fc3ab03
RohanVDvivedi <rohandvivedi@gmail.com>
2024-07-10 21:57:34 +0530

    init function implemented for array_table_tuple_defs

--- src/array_table/array_table_tuple_definitions.c
+++ src/array_table/array_table_tuple_definitions.c
@@ -8,10 +8,10 @@
 
 #include<stdlib.h>
 
-int init_array_table_tuple_definitions(array_table_tuple_defs* attd_p, const page_access_specs* pas_p)
+int init_array_table_tuple_definitions(array_table_tuple_defs* attd_p, const page_access_specs* pas_p, const tuple_def* record_def)
 {
 	// zero initialize attd_p
-	(*attd_p) = (page_table_tuple_defs){};
+	(*attd_p) = (array_table_tuple_defs){};
 
 	// check id page_access_specs struct is valid
 	if(!is_valid_page_access_specs(pas_p))
@@ -20,43 +20,54 @@ int init_array_table_tuple_definitions(array_table_tuple_defs* attd_p, const pag
 	// initialize page_access_specs fo the bpttd
 	attd_p->pas_p = pas_p;
 
-	// this can only be done after setting the pas_p attribute of pttd
+	// this can only be done after setting the pas_p attribute of attd
 	// there must be room for atleast some bytes after the page_table_page_header
-	if(!can_page_header_fit_on_persistent_page(sizeof_PAGE_TABLE_PAGE_HEADER(attd_p), attd_p->pas_p->page_size))
+	if(!can_page_header_fit_on_persistent_page(sizeof_ARRAY_TABLE_PAGE_HEADER(attd_p), attd_p->pas_p->page_size))
+		return 0;
+
+	// if the record_def is not fixed sized, then fail
+	if(!is_fixed_sized_tuple_def(record_def))
 		return 0;
 
 	int res = 1;
 
-	// initialize entry_def
-	attd_p->entry_def = get_new_tuple_def("temp_entry_def", 1, attd_p->pas_p->page_size);
-	if(attd_p->entry_def == NULL) // memory allocation failed
+	// initialize index_def
+	attd_p->index_def = get_new_tuple_def("temp_entry_def", 1, attd_p->pas_p->page_size);
+	if(attd_p->index_def == NULL) // memory allocation failed
 		exit(-1);
-	res = insert_element_def(attd_p->entry_def, "child_page_id", UINT, attd_p->pas_p->page_id_width, 1, &((user_value){.uint_value = attd_p->pas_p->NULL_PAGE_ID}));
+	res = insert_element_def(attd_p->index_def, "child_page_id", UINT, attd_p->pas_p->page_id_width, 1, &((user_value){.uint_value = attd_p->pas_p->NULL_PAGE_ID}));
 	if(res == 0)
 	{
-		deinit_page_table_tuple_definitions(attd_p);
+		deinit_array_table_tuple_definitions(attd_p);
 		return 0;
 	}
-	res = finalize_tuple_def(attd_p->entry_def);
+	res = finalize_tuple_def(attd_p->index_def);
 	if(res == 0)
 	{
-		deinit_page_table_tuple_definitions(attd_p);
+		deinit_array_table_tuple_definitions(attd_p);
 		return 0;
 	}
 
-	// number of entries that can fir on the page
-	attd_p->entries_per_page = get_maximum_tuple_count_on_persistent_page(sizeof_PAGE_TABLE_PAGE_HEADER(attd_p), attd_p->pas_p->page_size, &(attd_p->entry_def->size_def));
+	attd_p->record_def = clone_tuple_def(record_def);
+	if(attd_p->record_def == NULL) // memory allocation failed
+		exit(-1);
+	finalize_tuple_def(attd_p->record_def);
+
+	// number of entries that can fit on the leaf page
+	attd_p->leaf_entries_per_page = get_maximum_tuple_count_on_persistent_page(sizeof_ARRAY_TABLE_PAGE_HEADER(attd_p), attd_p->pas_p->page_size, &(attd_p->record_def->size_def));
+
+	// number of entries that can fit on the index (interior) page
+	attd_p->index_entries_per_page = get_maximum_tuple_count_on_persistent_page(sizeof_ARRAY_TABLE_PAGE_HEADER(attd_p), attd_p->pas_p->page_size, &(attd_p->index_def->size_def));
 
 	// there has to be atleast 2 entries per page for it to be a tree
-	if(attd_p->entries_per_page < 2)
+	if(attd_p->index_entries_per_page < 2)
 	{
-		deinit_page_table_tuple_definitions(attd_p);
+		deinit_array_table_tuple_definitions(attd_p);
 		return 0;
 	}
 
 	// build power_table
-	initialize_power_table(&(attd_p->power_table_for_entries_per_page), attd_p->entries_per_page);
-	
+	initialize_power_table(&(attd_p->power_table_for_index_entries_per_page), attd_p->index_entries_per_page);
 
 	// calculations for max_page_table_height
 	// above attributes must be set successfully for this block of code to run properly
@@ -66,8 +77,8 @@ int init_array_table_tuple_definitions(array_table_tuple_defs* attd_p, const pag
 		while(l <= h)
 		{
 			uint64_t m = l + ((h - l) / 2);
-			uint64_t exp_m;
-			if(!get_power_of_entries_per_page_using_page_table_tuple_definitions(attd_p, m, &exp_m))
+			uint64_t power_result;
+			if(!get_leaf_entries_refrenceable_by_entry_at_given_level_using_array_table_tuple_definitions(attd_p, m, &power_result))
 			{
 				attd_p->max_page_table_height = m;
 				h = m - 1;
