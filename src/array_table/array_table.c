cccf65a37605bacae8fcda48a2c7c88e17828a4a
RohanVDvivedi <rohandvivedi@gmail.com>
2024-07-12 22:06:43 +0530

    print array_table function refactoring completed

--- src/array_table/array_table.c
+++ src/array_table/array_table.c
@@ -66,7 +66,7 @@ int destroy_array_table(uint64_t root_page_id, const array_table_tuple_defs* att
 				goto ABORT_ERROR;
 			pop_from_locked_pages_stack(locked_pages_stack_p);
 		}
-		// handle free-ing for index pages
+		// handle free-ing for index page
 		else
 		{
 			// get tuple_count of the page
@@ -122,7 +122,7 @@ int destroy_array_table(uint64_t root_page_id, const array_table_tuple_defs* att
 void print_array_table(uint64_t root_page_id, int only_leaf_pages, const array_table_tuple_defs* attd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error)
 {
 	// print the root page id of the page_table
-	printf("\n\nPage_table @ root_page_id = %"PRIu64"\n\n", root_page_id);
+	printf("\n\nArray_table @ root_page_id = %"PRIu64"\n\n", root_page_id);
 
 	// create a stack
 	locked_pages_stack* locked_pages_stack_p = &((locked_pages_stack){});
@@ -134,7 +134,7 @@ void print_array_table(uint64_t root_page_id, int only_leaf_pages, const array_t
 			return;
 
 		// pre cache level of the root_page
-		uint32_t root_page_level = get_level_of_page_table_page(&root_page, attd_p);
+		uint32_t root_page_level = get_level_of_array_table_page(&root_page, attd_p);
 
 		// create a stack of capacity = levels
 		if(!initialize_locked_pages_stack(locked_pages_stack_p, root_page_level + 1))
@@ -149,11 +149,11 @@ void print_array_table(uint64_t root_page_id, int only_leaf_pages, const array_t
 		locked_page_info* curr_locked_page = get_top_of_locked_pages_stack(locked_pages_stack_p);
 
 		// print current page as a leaf page
-		if(is_page_table_leaf_page(&(curr_locked_page->ppage), attd_p))
+		if(is_array_table_leaf_page(&(curr_locked_page->ppage), attd_p))
 		{
 			// print this page and its page_id
 			printf("page_id : %"PRIu64"\n\n", curr_locked_page->ppage.page_id);
-			print_page_table_page(&(curr_locked_page->ppage), attd_p);
+			print_array_table_page(&(curr_locked_page->ppage), attd_p);
 			printf("xxxxxxxxxxxxx\n\n");
 
 			// unlock it and pop it from the stack
@@ -162,11 +162,11 @@ void print_array_table(uint64_t root_page_id, int only_leaf_pages, const array_t
 			if(*abort_error)
 				goto ABORT_ERROR;
 		}
-		// print current page as an interior page
+		// print current page as an index page
 		else
 		{
 			// get tuple_count of the page
-			uint32_t tuple_count = get_tuple_count_on_persistent_page(&(curr_locked_page->ppage), attd_p->pas_p->page_size, &(attd_p->entry_def->size_def));
+			uint32_t tuple_count = get_tuple_count_on_persistent_page(&(curr_locked_page->ppage), attd_p->pas_p->page_size, &(attd_p->index_def->size_def));
 
 			int pushed_child = 0;
 
@@ -174,7 +174,7 @@ void print_array_table(uint64_t root_page_id, int only_leaf_pages, const array_t
 			while(curr_locked_page->child_index < tuple_count && pushed_child == 0)
 			{
 				// then push it's child at child_index onto the stack (with child_index = 0), while incrementing its child index
-				uint64_t child_page_id = get_child_page_id_at_child_index_in_page_table_page(&(curr_locked_page->ppage), curr_locked_page->child_index++, attd_p);
+				uint64_t child_page_id = get_child_page_id_at_child_index_in_array_table_index_page(&(curr_locked_page->ppage), curr_locked_page->child_index++, attd_p);
 
 				// if it is a NULL_PAGE_ID, then continue
 				if(child_page_id == attd_p->pas_p->NULL_PAGE_ID)
@@ -196,7 +196,7 @@ void print_array_table(uint64_t root_page_id, int only_leaf_pages, const array_t
 				{
 					// print this page and its page_id
 					printf("page_id : %"PRIu64"\n\n", curr_locked_page->ppage.page_id);
-					print_page_table_page(&(curr_locked_page->ppage), attd_p);
+					print_array_table_page(&(curr_locked_page->ppage), attd_p);
 					printf("xxxxxxxxxxxxx\n");
 				}
 
