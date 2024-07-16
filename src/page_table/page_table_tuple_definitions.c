88c790781a51348ae7ddbcfdd8bfc4c85493074d
RohanVDvivedi <rohandvivedi@gmail.com>
2024-07-16 22:07:05 +0530

    refactoring to make page_table use array table completed

--- src/page_table/page_table_tuple_definitions.c
+++ src/page_table/page_table_tuple_definitions.c
@@ -9,7 +9,17 @@
 int init_page_table_tuple_definitions(page_table_tuple_defs* pttd_p, const page_access_specs* pas_p)
 {
 	pttd_p->pas_p = pas_p;
-	return init_array_table_tuple_definitions(&(pttd_p->attd), pas_p, /*TODO*/);
+
+	tuple_def* record_def = get_new_tuple_def("temp_page_table_record_def", 1, pttd_p->pas_p->page_size);
+	if(record_def == NULL) // memory allocation failed
+		exit(-1);
+	insert_element_def(record_def, "page_id", UINT, pttd_p->pas_p->page_id_width, 1, &((user_value){.uint_value = pttd_p->pas_p->NULL_PAGE_ID}));
+
+	int res = init_array_table_tuple_definitions(&(pttd_p->attd), pas_p, record_def);
+
+	delete_tuple_def(record_def);
+
+	return res;
 }
 
 void deinit_page_table_tuple_definitions(page_table_tuple_defs* pttd_p)
