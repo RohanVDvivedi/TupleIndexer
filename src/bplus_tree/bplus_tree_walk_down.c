d03e33415e94036b5103d2ee50911d60ae144b96
RohanVDvivedi <rohandvivedi@gmail.com>
2024-09-20 21:50:42 +0530

    root_page_id not necessary for a walk down

--- src/bplus_tree/bplus_tree_walk_down.c
+++ src/bplus_tree/bplus_tree_walk_down.c
@@ -283,7 +283,7 @@ int walk_down_locking_parent_pages_for_update_using_record(uint64_t root_page_id
 		// if the interior page index record, at child_index in curr_locked_page if deleted, will the curr_locked_page require merging
 		// if not then release all locks above curr_locked_page
 		// mind well we still need lock on curr_locked_page, as merge on its child will require us to delete corresponding 1 index entry from curr_locked_page
-		if(curr_locked_page->ppage.page_id != root_page_id && !may_require_merge_or_redistribution_for_delete_for_bplus_tree_interior_page(&(curr_locked_page->ppage), bpttd_p->pas_p->page_size, bpttd_p->index_def, curr_locked_page->child_index) )
+		if(!may_require_merge_or_redistribution_for_delete_for_bplus_tree_interior_page(&(curr_locked_page->ppage), bpttd_p->pas_p->page_size, bpttd_p->index_def, curr_locked_page->child_index) )
 		{
 			// we can release locks on all the pages in stack except for the the curr_locked_page
 			(*release_for_merge) = get_element_count_locked_pages_stack(locked_pages_stack_p) - 1;
