a152c9ba3f8a40b3d4a75b13621600048742388b
RohanVDvivedi <rohandvivedi@gmail.com>
2024-09-24 20:29:58 +0530

    added more description in comments

--- inc/bplus_tree/bplus_tree_split_insert_util.h
+++ inc/bplus_tree/bplus_tree_split_insert_util.h
@@ -10,7 +10,7 @@
 // i.e. the root_page may not be in the locked pages stack
 // all the locks are released by this function, only on an abort_error
 // this function always returns 1, except on an abort_error
-// in absence of an abort error, locks to all pages untouched are left as is,
+// in absence of an abort error, locks to all pages untouched/unmodified are left as is,
 // so do call release_all_locks_and_deinitialize_stack_reenterable once you are done with the stack
 // insertion_index is an optional parameter, if you do not know it set it to INVALID_TUPLE_INDEX, this function inherently does not and will not check for duplicate insertions
 int split_insert_and_unlock_pages_up(uint64_t root_page_id, locked_pages_stack* locked_pages_stack_p, const void* record, uint32_t insertion_index, const bplus_tree_tuple_defs* bpttd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
