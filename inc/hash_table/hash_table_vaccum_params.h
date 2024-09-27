05ac859aa0cdd6237794bf2e004ac90cd2c58935
RohanVDvivedi <rohandvivedi@gmail.com>
2024-09-27 14:36:07 +0530

    added vaccum params struct for hash_table

---
+++ inc/hash_table/hash_table_vaccum_params.h
@@ -0,0 +1,16 @@
+#ifndef HASH_TABLE_VACCUM_PARAMS_H
+#define HASH_TABLE_VACCUM_PARAMS_H
+
+typedef struct hash_table_vaccum_params hash_table_vaccum_params;
+struct hash_table_vaccum_params
+{
+	// low level page table vaccum
+	int page_table_vaccum_needed;
+	uint64_t page_table_vaccum_id;
+
+	// high level hash table vaccum
+	int hash_table_vaccum_needed;
+	const void* hash_table_vaccum_key;
+};
+
+#endif
