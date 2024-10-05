7437cd4271e6cf77b6f5129c34907034cf0c978b
RohanVDvivedi <rohandvivedi@gmail.com>
2024-10-05 23:36:08 +0530

    --

--- src/bplus_tree/bplus_tree_index_tuple_functions_util.c
+++ src/bplus_tree/bplus_tree_index_tuple_functions_util.c
@@ -2,14 +2,17 @@
 
 #include<tuple.h>
 
+// index_def will always have (key_element_count + 1) elements
+// and the last element of any of it's tuple (i.e. the one at index key_element_count) will always be the child page id
+
 uint64_t get_child_page_id_from_index_tuple(const void* index_tuple, const bplus_tree_tuple_defs* bpttd_p)
 {
 	// get user value for the child_page_id element from the tuple
 	// we neet not worry, this element in the tuple will not be NULL, check bplus_tree_tuple_defs.c for more information
-	return get_value_from_element_from_tuple(bpttd_p->index_def, get_element_def_count_tuple_def(bpttd_p->index_def) - 1, index_tuple).uint_value;
+	return get_value_from_element_from_tuple(bpttd_p->index_def, STATIC_POSITION(bpttd_p->key_element_count), index_tuple).uint_value;
 }
 
 void set_child_page_id_in_index_tuple(void* index_tuple, uint64_t child_page_id, const bplus_tree_tuple_defs* bpttd_p)
 {
-	set_element_in_tuple(bpttd_p->index_def, get_element_def_count_tuple_def(bpttd_p->index_def) - 1, index_tuple, &((const user_value){.uint_value = child_page_id}));
+	set_element_in_tuple(bpttd_p->index_def, STATIC_POSITION(bpttd_p->key_element_count), index_tuple, &((const user_value){.uint_value = child_page_id}), UINT32_MAX);
 }
