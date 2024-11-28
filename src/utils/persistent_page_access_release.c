f49eb2727141f76b5630100d623cc7e8404ae901
RohanVDvivedi <rohandvivedi@gmail.com>
2024-11-28 14:33:10 +0530

    added more appropriate statements to effectively identify bugs in pam implementations

--- src/utils/persistent_page_access_release.c
+++ src/utils/persistent_page_access_release.c
@@ -18,7 +18,20 @@ persistent_page get_new_persistent_page_with_write_lock(const page_access_method
 
 	// if could not allocate a page on disk then just return a NULL persistent page
 	if(ppage.page == NULL)
+	{
+		if((*abort_error) == 0) // failure without abort_error is a bug
+		{
+			printf("BUG :: pam failure without an abort_error, buggy pam implementation\n");
+			exit(-1);
+		}
 		return get_NULL_persistent_page(pam_p);
+	}
+
+	if(*(abort_error)) // success but with abort_error is a bug
+	{
+		printf("BUG :: pam success with an abort_error, buggy pam implementation\n");
+		exit(-1);
+	}
 
 	// set the write locked flag on ppage
 	ppage.flags = 0;
@@ -45,7 +58,20 @@ persistent_page acquire_persistent_page_with_lock(const page_access_methods* pam
 			ppage.page = pam_p->acquire_page_with_reader_lock(pam_p->context, transaction_id, ppage.page_id, abort_error);
 
 			if(ppage.page == NULL)
+			{
+				if((*abort_error) == 0) // failure without abort_error is a bug
+				{
+					printf("BUG :: pam failure without an abort_error, buggy pam implementation\n");
+					exit(-1);
+				}
 				return get_NULL_persistent_page(pam_p);
+			}
+
+			if(*(abort_error)) // success but with abort_error is a bug
+			{
+				printf("BUG :: pam success with an abort_error, buggy pam implementation\n");
+				exit(-1);
+			}
 
 			ppage.flags = 0;
 			ppage.is_write_locked = 0;
@@ -57,7 +83,20 @@ persistent_page acquire_persistent_page_with_lock(const page_access_methods* pam
 			ppage.page = pam_p->acquire_page_with_writer_lock(pam_p->context, transaction_id, ppage.page_id, abort_error);
 
 			if(ppage.page == NULL)
-				ppage = get_NULL_persistent_page(pam_p);
+			{
+				if((*abort_error) == 0) // failure without abort_error is a bug
+				{
+					printf("BUG :: pam failure without an abort_error, buggy pam implementation\n");
+					exit(-1);
+				}
+				return get_NULL_persistent_page(pam_p);
+			}
+
+			if(*(abort_error)) // success but with abort_error is a bug
+			{
+				printf("BUG :: pam success with an abort_error, buggy pam implementation\n");
+				exit(-1);
+			}
 
 			ppage.flags = 0;
 			ppage.is_write_locked = 1;
@@ -92,6 +131,23 @@ int downgrade_to_reader_lock_on_persistent_page(const page_access_methods* pam_p
 		ppage->is_write_locked = 0;
 	}
 
+	if(!res)
+	{
+		if((*abort_error) == 0) // failure without abort_error is a bug
+		{
+			printf("BUG :: pam failure without an abort_error, buggy pam implementation\n");
+			exit(-1);
+		}
+	}
+	else
+	{
+		if(*(abort_error)) // success but with abort_error is a bug
+		{
+			printf("BUG :: pam success with an abort_error, buggy pam implementation\n");
+			exit(-1);
+		}
+	}
+
 	return res;
 }
 
@@ -118,15 +174,36 @@ int upgrade_to_write_lock_on_persistent_page(const page_access_methods* pam_p, c
 		ppage->is_write_locked = 1;
 	}
 
+	if(!res)
+	{
+		if((*abort_error) == 0) // failure without abort_error is a bug
+		{
+			printf("BUG :: pam failure without an abort_error, buggy pam implementation\n");
+			exit(-1);
+		}
+	}
+	else
+	{
+		if(*(abort_error)) // success but with abort_error is a bug
+		{
+			printf("BUG :: pam success with an abort_error, buggy pam implementation\n");
+			exit(-1);
+		}
+	}
+
 	return res;
 }
 
 int release_lock_on_persistent_page(const page_access_methods* pam_p, const void* transaction_id, persistent_page* ppage, int opts, int* abort_error)
 {
+	int final_opts = ppage->flags | opts;
+
+	int is_free_page_requested = !!(final_opts & FREE_PAGE);
+
 	// a page can not be freed, once a transaction is aborted
 	if(*(abort_error))
 	{
-		if((ppage->flags | opts) & FREE_PAGE)
+		if(is_free_page_requested)
 		{
 			printf("BUG :: attempting to free a page, while releasing a lock, after knowing of an abort\n");
 			exit(-1);
@@ -140,9 +217,16 @@ int release_lock_on_persistent_page(const page_access_methods* pam_p, const void
 
 	// release lock appropriately
 	if(is_persistent_page_write_locked(ppage))
-		res = pam_p->release_writer_lock_on_page(pam_p->context, transaction_id, ppage->page, opts | ppage->flags, abort_error);
+		res = pam_p->release_writer_lock_on_page(pam_p->context, transaction_id, ppage->page, final_opts, abort_error);
 	else
-		res = pam_p->release_reader_lock_on_page(pam_p->context, transaction_id, ppage->page, opts | ppage->flags, abort_error);
+		res = pam_p->release_reader_lock_on_page(pam_p->context, transaction_id, ppage->page, final_opts, abort_error);
+
+	// you can never transition from aborted to non-aborted transaction state
+	if(old_abort_error && (*abort_error) == 0)
+	{
+		printf("BUG :: transition of aborted to non-aborted state for a transaction, buggy pam implementation\n");
+		exit(-1);
+	}
 
 	// this means, we failed to release a lock on a page, after the abort
 	// this must not happen, unless there is a double free bug in TupleIndexer OR faulty pam implementation
@@ -156,6 +240,37 @@ int release_lock_on_persistent_page(const page_access_methods* pam_p, const void
 	if(res)
 		(*ppage) = get_NULL_persistent_page(pam_p);
 
+	if(!is_free_page_requested) // simple release page latch case
+	{
+		if(!res)
+		{
+			if((*abort_error) == 0) // failure without abort_error is a bug
+			{
+				printf("BUG :: pam failure without an abort_error, buggy pam implementation\n");
+				exit(-1);
+			}
+		}
+	}
+	else // release page latch with free page option
+	{
+		if(!res)
+		{
+			if((*abort_error) == 0) // failure without abort_error is a bug
+			{
+				printf("BUG :: pam failure without an abort_error, buggy pam implementation\n");
+				exit(-1);
+			}
+		}
+		else
+		{
+			if(*(abort_error)) // success but with abort_error is a bug
+			{
+				printf("BUG :: pam success with an abort_error, buggy pam implementation\n");
+				exit(-1);
+			}
+		}
+	}
+
 	return res;
 }
 
@@ -168,5 +283,24 @@ int free_persistent_page(const page_access_methods* pam_p, const void* transacti
 		exit(-1);
 	}
 
-	return pam_p->free_page(pam_p->context, transaction_id, page_id, abort_error);
+	int res = pam_p->free_page(pam_p->context, transaction_id, page_id, abort_error);
+
+	if(!res)
+	{
+		if((*abort_error) == 0) // failure without abort_error is a bug
+		{
+			printf("BUG :: pam failure without an abort_error, buggy pam implementation\n");
+			exit(-1);
+		}
+	}
+	else
+	{
+		if(*(abort_error)) // success but with abort_error is a bug
+		{
+			printf("BUG :: pam success with an abort_error, buggy pam implementation\n");
+			exit(-1);
+		}
+	}
+
+	return res;
 }
