30767bcd0743e3eed8edd49317a93e78a1f2558b
RohanVDvivedi <rohandvivedi@gmail.com>
2024-12-14 14:26:26 +0530

    made bplus tree header functions inline

--- inc/bplus_tree/bplus_tree_page_header.h
+++ inc/bplus_tree/bplus_tree_page_header.h
@@ -4,14 +4,34 @@
 #include<persistent_page.h>
 #include<bplus_tree_tuple_definitions.h>
 
+#include<common_page_header.h>
+#include<bplus_tree_interior_page_header.h>
+
 /*
 **	utility functions for bplus_tree pages
 */
 
-int is_bplus_tree_leaf_page(const persistent_page* ppage, const bplus_tree_tuple_defs* bpttd_p);
+static inline int is_bplus_tree_leaf_page(const persistent_page* ppage, const bplus_tree_tuple_defs* bpttd_p);
 
 // =0 => leaf page
 // >0 => interior page
-uint32_t get_level_of_bplus_tree_page(const persistent_page* ppage, const bplus_tree_tuple_defs* bpttd_p);
+static inline uint32_t get_level_of_bplus_tree_page(const persistent_page* ppage, const bplus_tree_tuple_defs* bpttd_p);
+
+// inline implementations
+
+static inline int is_bplus_tree_leaf_page(const persistent_page* ppage, const bplus_tree_tuple_defs* bpttd_p)
+{
+	return get_common_page_header(ppage, bpttd_p->pas_p).type == BPLUS_TREE_LEAF_PAGE;
+}
+
+static inline uint32_t get_level_of_bplus_tree_page(const persistent_page* ppage, const bplus_tree_tuple_defs* bpttd_p)
+{
+	// leaf page is at level 0
+	if(is_bplus_tree_leaf_page(ppage, bpttd_p))
+		return 0;
+
+	// else it is an interior page starting at 1
+	return get_level_of_bplus_tree_interior_page(ppage, bpttd_p);
+}
 
 #endif
