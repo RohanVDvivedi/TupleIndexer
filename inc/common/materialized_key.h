ab7acb38174b3cd5d2f136790e34e87b11a6be61
RohanVDvivedi <rohandvivedi@gmail.com>
2024-12-16 11:39:41 +0530

    added logic to materialize keys

---
+++ inc/common/materialized_key.h
@@ -0,0 +1,27 @@
+#ifndef MATERIALIZED_KEY_H
+#define MATERIALIZED_KEY_H
+
+#include<tuple.h>
+
+/*
+	You mught encounter cases where you need to compare the same key with a large number of tuples on the data structure like bplus_tree and hash_table
+	You may instead materialize the key in such cases and perform the search to speed it up
+
+	Before you materialie the key from the tuple, you must ensure that all the key elements in the tuple accessible
+*/
+
+typedef struct materialized_key materialized_key;
+struct materialized_key
+{
+	uint32_t key_element_count;
+
+	data_type_info const ** key_dtis;
+
+	user_value* keys;
+};
+
+materialized_key materialize_key_from_tuple(const void* tuple, const tuple_def* tpl_d, const positional_accessor* key_columns_to_materialize, uint32_t key_element_count);
+
+void destroy_materialized_tuple(materialized_key* mat_key);
+
+#endif
---
+++ src/common/materialized_key.c
@@ -0,0 +1,34 @@
+#include<materialized_key.h>
+
+#include<stdlib.h>
+
+materialized_key materialize_key_from_tuple(const void* tuple, const tuple_def* tpl_d, const positional_accessor* key_columns_to_materialize, uint32_t key_element_count)
+{
+	materialized_key mat_key = {};
+
+	mat_key.key_element_count = key_element_count;
+
+	mat_key.key_dtis = malloc(sizeof(data_type_info*) * key_element_count);
+	if(mat_key.key_dtis == NULL)
+		exit(-1);
+
+	mat_key.keys = malloc(sizeof(user_value) * key_element_count);
+	if(mat_key.keys == NULL)
+		exit(-1);
+
+	for(uint32_t i = 0; i < key_element_count; i++)
+	{
+		mat_key.key_dtis[i] = get_type_info_for_element_from_tuple_def(tpl_d, key_columns_to_materialize[i]);
+		get_value_from_element_from_tuple(&(mat_key.keys[i]), tpl_d, key_columns_to_materialize[i], tuple);
+	}
+
+	return mat_key;
+}
+
+void destroy_materialized_tuple(materialized_key* mat_key)
+{
+	if(mat_key->key_dtis != NULL)
+		free((void*)(mat_key->key_dtis));
+	if(mat_key->keys != NULL)
+		free((void*)(mat_key->keys));
+}
