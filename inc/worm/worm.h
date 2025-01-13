4bf116515ac760d33ca4a08ab4df6c99a82dc62d
RohanVDvivedi <rohandvivedi@gmail.com>
2025-01-13 15:49:26 +0530

    added worm.h for basic usage comments

---
+++ inc/worm/worm.h
@@ -0,0 +1,9 @@
+#ifndef WORM_H
+#define WORM_H
+
+/* WORM = Write-Once-Read-Multiple-times data structure
+** use it for storing larger than page data types, or types that do not fit on page
+** also has a reference counter and the data in worm and that of dependent root page id are destroyed when the reference count reaches 0
+*/
+
+#endif
