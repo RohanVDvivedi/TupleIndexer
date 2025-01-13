3471a44f3230027a98f22a0b0f27ace825a97c8e
RohanVDvivedi <rohandvivedi@gmail.com>
2025-01-13 13:51:55 +0530

    added dummy tuple_def header for internal use

---
+++ inc/worm/worm_tuple_definitions.h
@@ -0,0 +1,6 @@
+#ifndef WORM_TUPLE_DEFINITIONS_H
+#define WORM_TUPLE_DEFINITIONS_H
+
+#include<worm_tuple_definitions_public.h>
+
+#endif
--- src/worm/worm_tuple_definitions.c
+++ src/worm/worm_tuple_definitions.c
@@ -1,1 +1,4 @@
 #include<worm_tuple_definitions_public.h>
+
+#include<worm_head_page_header.h>
+#include<worm_any_page_header.h>
