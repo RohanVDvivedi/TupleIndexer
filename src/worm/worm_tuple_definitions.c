036a7ac49380e13474b2dfc54a0a1413cdc55c4d
RohanVDvivedi <rohandvivedi@gmail.com>
2025-01-14 14:18:13 +0530

    started implementing the worm tuple_defs

--- src/worm/worm_tuple_definitions.c
+++ src/worm/worm_tuple_definitions.c
@@ -2,3 +2,18 @@
 
 #include<worm_head_page_header.h>
 #include<worm_any_page_header.h>
+
+int init_worm_tuple_definitions(worm_tuple_defs* wtd_p, const page_access_specs* pas_p)
+{
+	// TODO
+}
+
+void deinit_worm_tuple_definitions(worm_tuple_defs* wtd_p)
+{
+	// TODO
+}
+
+void print_worm_tuple_definitions(const worm_tuple_defs* wtd_p)
+{
+	// TODO
+}
