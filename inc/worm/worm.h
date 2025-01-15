a74b2e32e0f49384ca3cf6d2b0237f077ad760de
RohanVDvivedi <rohandvivedi@gmail.com>
2025-01-15 10:55:50 +0530

    added declarations for basic worm functionality

--- inc/worm/worm.h
+++ inc/worm/worm.h
@@ -7,4 +7,18 @@
 ** each page has atleast 1 partial_blob_type's tuple_def on it
 */
 
+#include<worm_tuple_definitions_public.h>
+
+#include<opaque_page_access_methods.h>
+#include<opaque_page_modification_methods.h>
+
+// if reference_counter == 0, then it is forced to 1
+uint64_t get_new_worm(uint64_t reference_counter, uint64_t dependent_root, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+
+void print_worm(uint64_t root_page_id, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
+
+int increment_reference_counter_for_worm(uint64_t head_page_id, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+
+int decrement_reference_counter_for_worm(uint64_t head_page_id, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+
 #endif
