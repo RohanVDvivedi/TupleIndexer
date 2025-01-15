8e9be14514ef1db7c91c56219fa64f1369b3167d
RohanVDvivedi <rohandvivedi@gmail.com>
2025-01-15 11:07:16 +0530

    worm get new functions implemented

--- inc/worm/worm.h
+++ inc/worm/worm.h
@@ -13,7 +13,7 @@
 #include<opaque_page_modification_methods.h>
 
 // if reference_counter == 0, then it is forced to 1
-uint64_t get_new_worm(uint64_t reference_counter, uint64_t dependent_root, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+uint64_t get_new_worm(uint64_t reference_counter, uint64_t dependent_root_page_id, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
 
 int increment_reference_counter_for_worm(uint64_t head_page_id, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
 
--- src/worm/worm.c
+++ src/worm/worm.c
@@ -1,8 +1,31 @@
 #include<worm.h>
 
-uint64_t get_new_worm(uint64_t reference_counter, uint64_t dependent_root, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error)
+#include<persistent_page_functions.h>
+
+#include<worm_page_util.h>
+
+uint64_t get_new_worm(uint64_t reference_counter, uint64_t dependent_root_page_id, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error)
 {
-	// TODO
+	persistent_page head_page = get_new_persistent_page_with_write_lock(pam_p, transaction_id, abort_error);
+
+	// failure to acquire a new page
+	if(*abort_error)
+		return wtd_p->pas_p->NULL_PAGE_ID;
+
+	// init head page
+	init_worm_head_page(&head_page, reference_counter, dependent_root_page_id, wtd_p, pmm_p, transaction_id, abort_error);
+	if(*abort_error)
+	{
+		release_lock_on_persistent_page(pam_p, transaction_id, &head_page, NONE_OPTION, abort_error);
+		return wtd_p->pas_p->NULL_PAGE_ID;
+	}
+
+	uint64_t res = head_page.page_id;
+	release_lock_on_persistent_page(pam_p, transaction_id, &head_page, NONE_OPTION, abort_error);
+	if(*abort_error)
+		return wtd_p->pas_p->NULL_PAGE_ID;
+
+	return res;
 }
 
 int increment_reference_counter_for_worm(uint64_t head_page_id, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error)
