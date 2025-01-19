80355084a3e71fce1788b1ec9197ce917a8e7e0c
RohanVDvivedi <rohandvivedi@gmail.com>
2025-01-19 15:11:29 +0530

    basic logic implemented for skeleton of the append function for the worm

--- src/worm/worm_append_iterator.c
+++ src/worm/worm_append_iterator.c
@@ -36,7 +36,51 @@ uint32_t append_to_worm(worm_append_iterator* wai_p, const char* data, uint32_t 
 	if(data_size == 0)
 		return 0;
 
-	// TODO
+	// persistent page for locking tail page, if tail page is not the head page
+	persistent_page tail_page = get_NULL_persistent_page(wai_p->pam_p);
+
+	// this pointer may point to wai_p->head_page, if head is the tail page, else it will point to the local variable above
+	persistent_page* tail_page_p = NULL;
+
+	// grab the tail_page_id from the head page
+	uint64_t tail_page_id = get_tail_page_id_of_worm_head_page(&(wai_p->head_page), wai_p->wtd_p);
+
+	if(tail_page_id == wai_p->head_page.page_id) // if head_page is the tail page
+		tail_page_p = &(wai_p->head_page);
+	else // else we need to lock the tail
+	{
+		tail_page_p = &tail_page;
+		(*tail_page_p) = acquire_persistent_page_with_lock(pam_p, transaction_id, tail_page_id, WRITE_LOCK, abort_error);
+		if(*abort_error)
+			goto ABORT_ERROR
+	}
+
+	// now we can directly use the tail_page_p to access tail
+
+	uint32_t bytes_appended = 0;
+
+	while(data_size > 0)
+	{
+		// TODO
+	}
+
+	// only tail_page could be locked, in thelocal scope, if you read here
+	// if it is locked release it
+	if(is_persistent_page_NULL(&tail_page, wai_p->pam_p))
+	{
+		release_lock_on_persistent_page(wai_p->pam_p, transaction_id, &tail_page, NONE_OPTION, abort_error);
+		if(*abort_error)
+			goto ABORT_ERROR;
+	}
+
+	return bytes_appended;
+
+	ABORT_ERROR:;
+	// in case of abort, release lock on head_page and tail_page (if tail_page is not NULL)
+	if(is_persistent_page_NULL(&tail_page, wai_p->pam_p))
+		release_lock_on_persistent_page(wai_p->pam_p, transaction_id, &tail_page, NONE_OPTION, abort_error);
+	release_lock_on_persistent_page(wai_p->pam_p, transaction_id, &(wai_p->head_page), NONE_OPTION, abort_error);
+	return 0;
 }
 
 void delete_worm_append_iterator(worm_append_iterator* wai_p, const void* transaction_id, int* abort_error)
