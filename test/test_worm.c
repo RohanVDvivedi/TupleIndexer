b59d9e49e4a182c2622188973ce06945fda6989e
RohanVDvivedi <rohandvivedi@gmail.com>
2025-01-19 17:52:45 +0530

    added base create and destroy test case for worm

---
+++ test/test_worm.c
@@ -0,0 +1,84 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdint.h>
+#include<string.h>
+
+#include<worm.h>
+
+#include<unWALed_in_memory_data_store.h>
+#include<unWALed_page_modification_methods.h>
+
+#define BUFFER_APPEND_COUNT 10
+
+#define BUFFER_APPEND_SIZE 100
+//#define BUFFER_APPEND_SIZE 150
+//#define BUFFER_APPEND_SIZE 300
+//#define BUFFER_APPEND_SIZE 1000
+
+char buffer[BUFFER_APPEND_SIZE];
+
+void prepare_buffer(uint8_t id)
+{
+	for(uint32_t i = 0; i < BUFFER_APPEND_SIZE; i++)
+	{
+		if((i % 2) == 0)
+			buffer[i/2] = id;
+		else
+			buffer[i/2+1] = (i / 2);
+	}
+}
+
+// attributes of the page_access_specs suggestions for creating page_access_methods
+#define PAGE_ID_WIDTH        3
+#define PAGE_SIZE          256
+
+// initialize transaction_id and abort_error
+const void* transaction_id = NULL;
+int abort_error = 0;
+
+int main()
+{
+	/* SETUP STARTED */
+
+	// construct an in-memory data store
+	page_access_methods* pam_p = get_new_unWALed_in_memory_data_store(&((page_access_specs){.page_id_width = PAGE_ID_WIDTH, .page_size = PAGE_SIZE}));
+
+	// construct unWALed page_modification_methods
+	page_modification_methods* pmm_p = get_new_unWALed_page_modification_methods();
+
+	// construct tuple definitions for worm
+	worm_tuple_defs wtd;
+	init_worm_tuple_definitions(&wtd, &(pam_p->pas));
+
+	// print the generated bplus tree tuple defs
+	print_bplus_tree_tuple_definitions(&bpttd);
+
+	// create a worm and get its head
+	uint64_t head_page_id = get_new_worm(&wtd, pam_p, pmm_p, transaction_id, &abort_error);
+	if(abort_error)
+	{
+		printf("ABORTED\n");
+		exit(-1);
+	}
+
+	/* CLEANUP */
+
+	// destroy bplus tree
+	destroy_worm(head_page_id, &wtd, pam_p, transaction_id, &abort_error);
+	if(abort_error)
+	{
+		printf("ABORTED\n");
+		exit(-1);
+	}
+
+	// close the in-memory data store
+	close_and_destroy_unWALed_in_memory_data_store(pam_p);
+
+	// destroy worm_tuple_definitions
+	deinit_worm_tuple_definitions(&wtd);
+
+	// destory page_modification_methods
+	delete_unWALed_page_modification_methods(pmm_p);
+
+	return 0;
+}
