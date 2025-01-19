4a6d1d703b1cccd65449bb426fc3599781a6f563
RohanVDvivedi <rohandvivedi@gmail.com>
2025-01-19 18:11:26 +0530

    minor fix makeing worm the only required header

--- inc/worm/worm_append_iterator_public.h
+++ inc/worm/worm_append_iterator_public.h
@@ -5,7 +5,7 @@
 	This iterator can only be used for appending to the worm
 */
 
-typedef struct worm_append_iterator wom_append_iterator;
+typedef struct worm_append_iterator worm_append_iterator;
 
 worm_append_iterator* get_new_worm_append_iterator(uint64_t head_page_id, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
 
