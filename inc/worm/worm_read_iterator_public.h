98249db33eb3ec473645669bb474591ebe51e1e4
RohanVDvivedi <rohandvivedi@gmail.com>
2025-01-27 15:04:01 +0530

    added declaration for the new utility functon to get dependent root of the worm using read iterator

--- inc/worm/worm_read_iterator_public.h
+++ inc/worm/worm_read_iterator_public.h
@@ -6,7 +6,7 @@
 */
 
 /*
-	IT will only keep the current page that we re looking at locked, and always latch crab forward
+	It will only keep the current page that we re looking at locked, and always latch crab forward
 */
 
 typedef struct worm_read_iterator worm_read_iterator;
@@ -14,6 +14,13 @@ typedef struct worm_read_iterator worm_read_iterator;
 worm_read_iterator* get_new_worm_read_iterator(uint64_t head_page_id, const worm_tuple_defs* wtd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
 
 worm_read_iterator* clone_worm_read_iterator(worm_read_iterator* wri_p, const void* transaction_id, int* abort_error);
+
+// since worm_read_iterator only keeps the curr_page that it is looking at read locked
+// you can only use this function if the worm_read_iterator is at the head page (not necessarily at it's first byte)
+// so if you intend to use this function, then use it directly after opening the iterator, as there is not mechanism provided to know if the worm_read_iterator is at the head page
+// this means this functions MAY not work for cloned worm_read_iterators
+// With an indeterminate position when not at head, this function fails silently with returning NULL_PAGE_ID
+uint64_t get_dependent_root_page_id_worm_read_iterator(const worm_read_iterator* wri_p);
 
 // if data_size == 0, then 0 is returned right away
 // if data == NULL, then a forward relative seek is performed for data_size bytes
