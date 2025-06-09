3d2b72daf8c69738b20174177c937fe05c70fe08
RohanVDvivedi <rohandvivedi@gmail.com>
2025-06-09 22:32:42 +0530

    added more comments for the description of the dead lock avoiding bplus tree lock

--- inc/tupleindexer/bplus_tree/bplus_tree_deadlock_avoiding_lock_compatibility_matrix.h
+++ inc/tupleindexer/bplus_tree/bplus_tree_deadlock_avoiding_lock_compatibility_matrix.h
@@ -7,7 +7,7 @@
 	This only works when you actually have page level lock, this prevents your page level locks (ideally latches) from deadlocking
 	Note::
 		1. This deadlocks primarily arise from leaf only un-stacked iterators iterating in opposite directions
-		2. Deadlocks can be entirely avoided and you may not need this glock if you avoid leaf only un-stacked iterator in backward direction (yes backward leaf only iterators are the main cause)
+		2. Deadlocks can be entirely avoided and you may not need this glock, if you avoid leaf only un-stacked iterator in backward direction (yes backward leaf only iterators are the main cause)
 		3. If your data_access_methods are designed to not have any page level locks, then you must atleast rely on a more generic rwlock (again, defined and implemented in LockKing), but this will give you less concurrency.
 */
 
@@ -32,5 +32,11 @@
 #include<lockking/glock.h>
 
 extern const glock_matrix bplus_tree_deadlock_avoidance_lock_matrix;
+
+/*
+	As discussed earlier
+	1. Do not use this lock when there are no page level locks provided by your data_access_methods, in that particular case you need a standard reader-writer lock, similar to the one provided in LockKing library
+	2. You can avoid using this lock entirely by using stacked iterators for backwards scanning, instead of leaf only iterators for backwards scanning, as they are the main source of deadlocks
+*/
 
 #endif
