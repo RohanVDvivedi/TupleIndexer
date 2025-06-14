478b5dddcb00a3ca788dfd789089124565c438e4
RohanVDvivedi <rohandvivedi@gmail.com>
2025-06-14 23:46:21 +0530

    implemented heap page get new function with init functionality

--- inc/tupleindexer/heap_page/heap_page.h
+++ inc/tupleindexer/heap_page/heap_page.h
@@ -4,7 +4,9 @@
 #include<tupleindexer/common/page_access_specification.h>
 #include<tupleindexer/utils/persistent_page_functions.h>
 
-persistent_page get_new_heap_page_with_write_lock(const page_access_specs* pas_p, const tuple_def* tpl_d, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+// the below function returns a is_persistent_page_NULL(), only on an error
+// if the error is not an abort_error, then it is just impossible to create a heap_page out of a database page, which is only possible for very small page sizes
+persistent_page get_new_heap_page_with_write_lock(const page_access_specs* pas_p, const tuple_def* tpl_d, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
 
 void print_heap_page(const persistent_page* ppage, const page_access_specs* pas_p, const tuple_def* tpl_d);
 
--- src/heap_page/heap_page.c
+++ src/heap_page/heap_page.c
@@ -2,7 +2,31 @@
 
 #include<tupleindexer/heap_page/heap_page_header.h>
 
-persistent_page get_new_heap_page_with_write_lock(const page_access_specs* pas_p, const tuple_def* tpl_d, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+persistent_page get_new_heap_page_with_write_lock(const page_access_specs* pas_p, const tuple_def* tpl_d, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error)
+{
+	persistent_page heap_page = get_new_persistent_page_with_write_lock(pam_p, transaction_id, abort_error);
+	if(*abort_error)
+		return heap_page;
+
+	int inited = init_persistent_page(pmm_p, transaction_id, &heap_page, pas_p->page_size, sizeof_HEAP_PAGE_HEADER(pas_p), &(tpl_d->size_def), abort_error);
+	if((*abort_error) || !inited)
+	{
+		release_lock_on_persistent_page(pam_p, transaction_id, &heap_page, NONE_OPTION, abort_error);
+		return heap_page;
+	}
+
+	// get the header, initialize it and set it back on to the page
+	heap_page_header hdr = get_heap_page_header(&heap_page, pas_p);
+	hdr.parent.type = HEAP_PAGE; // always a heap page
+	set_heap_page_header(&heap_page, &hdr, pas_p, pmm_p, transaction_id, abort_error);
+	if(*abort_error)
+	{
+		release_lock_on_persistent_page(pam_p, transaction_id, &heap_page, NONE_OPTION, abort_error);
+		return heap_page;
+	}
+
+	return heap_page;
+}
 
 void print_heap_page(const persistent_page* ppage, const page_access_specs* pas_p, const tuple_def* tpl_d)
 {
