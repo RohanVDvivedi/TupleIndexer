7c58d40007726b033ef61342dd6246b5e66321f7
RohanVDvivedi <rohandvivedi@gmail.com>
2025-06-14 18:21:03 +0530

    minor function declaration change for heap_page

--- inc/tupleindexer/heap_page/heap_page.h
+++ inc/tupleindexer/heap_page/heap_page.h
@@ -4,8 +4,8 @@
 #include<tupleindexer/common/page_access_specification.h>
 #include<tupleindexer/utils/persistent_page_functions.h>
 
-persistent_page get_new_heap_page_with_write_lock(const page_access_specs* pas_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+persistent_page get_new_heap_page_with_write_lock(const page_access_specs* pas_p, const tuple_def* tpl_d, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
 
-void print_heap_page(const persistent_page* ppage, const page_access_specs* pas_p);
+void print_heap_page(const persistent_page* ppage, const page_access_specs* pas_p, const tuple_def* tpl_d);
 
 #endif
--- src/heap_page/heap_page.c
+++ src/heap_page/heap_page.c
@@ -2,6 +2,6 @@
 
 #include<tupleindexer/heap_page/heap_page_header.h>
 
-persistent_page get_new_heap_page_with_write_lock(const page_access_specs* pas_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+persistent_page get_new_heap_page_with_write_lock(const page_access_specs* pas_p, const tuple_def* tpl_d, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
 
-void print_heap_page(const persistent_page* ppage, const page_access_specs* pas_p);
+void print_heap_page(const persistent_page* ppage, const page_access_specs* pas_p, const tuple_def* tpl_d);
