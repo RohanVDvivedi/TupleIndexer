27277d7b5f5aa8dede0df344aea9ea4889f3cc01
RohanVDvivedi <rohandvivedi@gmail.com>
2025-06-14 18:15:56 +0530

    added declarations for the heap page functions

--- inc/tupleindexer/heap_page/heap_page.h
+++ inc/tupleindexer/heap_page/heap_page.h
@@ -1,6 +1,11 @@
 #ifndef HEAP_PAGE_H
 #define HEAP_PAGE_H
 
+#include<tupleindexer/common/page_access_specification.h>
 #include<tupleindexer/utils/persistent_page_functions.h>
+
+persistent_page get_new_heap_page_with_write_lock(const page_access_specs* pas_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+
+void print_heap_page(const persistent_page* ppage, const page_access_specs* pas_p);
 
 #endif
--- src/heap_page/heap_page.c
+++ src/heap_page/heap_page.c
@@ -1,3 +1,7 @@
 #include<tupleindexer/heap_page/heap_page.h>
 
 #include<tupleindexer/heap_page/heap_page_header.h>
+
+persistent_page get_new_heap_page_with_write_lock(const page_access_specs* pas_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
+
+void print_heap_page(const persistent_page* ppage, const page_access_specs* pas_p);
