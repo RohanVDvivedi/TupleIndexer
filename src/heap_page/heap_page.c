e606f9ad5f651fec80dc1c55e674413d945c2f74
RohanVDvivedi <rohandvivedi@gmail.com>
2025-06-14 23:06:30 +0530

    implemented print function for heap page

--- src/heap_page/heap_page.c
+++ src/heap_page/heap_page.c
@@ -4,4 +4,8 @@
 
 persistent_page get_new_heap_page_with_write_lock(const page_access_specs* pas_p, const tuple_def* tpl_d, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
 
-void print_heap_page(const persistent_page* ppage, const page_access_specs* pas_p, const tuple_def* tpl_d);
+void print_heap_page(const persistent_page* ppage, const page_access_specs* pas_p, const tuple_def* tpl_d)
+{
+	print_heap_page_header(ppage, pas_p);
+	print_persistent_page(ppage, pas_p->page_size, tpl_d);
+}
