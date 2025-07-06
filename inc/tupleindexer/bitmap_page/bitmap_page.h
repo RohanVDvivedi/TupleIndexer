0f8e121f994773fb16bd45ad9e366c8df5a257d1
RohanVDvivedi <rohandvivedi@gmail.com>
2025-07-06 18:17:33 +0530

    added declaration for the bitfield tuple def getter function

--- inc/tupleindexer/bitmap_page/bitmap_page.h
+++ inc/tupleindexer/bitmap_page/bitmap_page.h
@@ -1,4 +1,11 @@
 #ifndef BITMAP_PAGE_H
 #define BITMAP_PAGE_H
 
+#include<tuplestore/tupledef.h>
+
+// returns the tuple_def for accessing a bitmap page, that is a fixed array page, consisting of only 1 tuple
+// this tuple has a tuple_def having a fixed length array of non-nullable bit-fields that has bits_per_field number of bits
+// the number of elements on the page depends on the size of the page and is also returned
+tuple_def* get_tuple_definition_for_bitmap_page(const page_access_specs* pas_p, uint8_t bits_per_field, uint64_t* elements_per_page);
+
 #endif
