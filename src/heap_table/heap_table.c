8fa757d1c540f23811375f50dedd6bccd226e2b8
RohanVDvivedi <rohandvivedi@gmail.com>
2025-08-16 18:52:58 +0530

    get new function implemented for heap table

--- src/heap_table/heap_table.c
+++ src/heap_table/heap_table.c
@@ -1,1 +1,8 @@
 #include<tupleindexer/heap_table/heap_table.h>
+
+#include<tupleindexer/bplus_tree/bplus_tree.h>
+
+uint64_t get_new_heap_table(const heap_table_tuple_defs* httd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error)
+{
+	return get_new_bplus_tree(&(httd_p->bpttd), pam_p, pmm_p, transaction_id, abort_error);
+}
