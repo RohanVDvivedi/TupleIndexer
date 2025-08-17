a5986ce422c4019bac69d1b8ba64f74534f8abe2
RohanVDvivedi <rohandvivedi@gmail.com>
2025-08-17 22:17:37 +0530

    minor fixes in heap_table track function

--- src/heap_table/heap_table.c
+++ src/heap_table/heap_table.c
@@ -114,12 +114,18 @@ int track_unused_space_in_heap_table(uint64_t root_page_id, const persistent_pag
 	if(is_persistent_page_NULL(ppage, pam_p))
 		return 0;
 
+	// else create and insert the entry (unused_space, page_id) in the heap_table
+
 	uint32_t unused_space = get_unused_space_on_heap_page(ppage, httd_p->pas_p, httd_p->record_def);
 
 	char entry_tuple[HEAP_TABLE_ENTRY_TUPLE_MAX_SIZE];
 	build_heap_table_entry_tuple(httd_p, entry_tuple, unused_space, ppage->page_id);
 
-	return insert_in_bplus_tree(root_page_id, entry_tuple, &(httd_p->bpttd), pam_p, pmm_p, transaction_id, abort_error);
+	int inserted = insert_in_bplus_tree(root_page_id, entry_tuple, &(httd_p->bpttd), pam_p, pmm_p, transaction_id, abort_error);
+	if(*abort_error)
+		return 0;
+
+	return inserted;
 }
 
 int destroy_heap_table(uint64_t root_page_id, const heap_table_tuple_defs* httd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
