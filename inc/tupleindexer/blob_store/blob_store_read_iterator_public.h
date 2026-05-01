d0d356c5c4e11852cf69b38d0736bfb100523bed
RohanVDvivedi <rohandvivedi@gmail.com>
2026-05-01 23:02:44 +0530

    api name changes

--- inc/tupleindexer/blob_store/blob_store_read_iterator_public.h
+++ inc/tupleindexer/blob_store/blob_store_read_iterator_public.h
@@ -20,17 +20,17 @@ blob_store_read_iterator* clone_blob_store_read_iterator(const blob_store_read_i
 // if data_size > 0, then a 0 is returned only if there are no more bytes in blob_store to be read
 // lesser than data_size bytes are read, possibly because of you have reached the end of the blob_store
 // on an abort_error all locks are released and 0 is returned
-uint32_t read_from_blob_store(blob_store_read_iterator* bsri_p, char* data, uint32_t data_size, const void* transaction_id, int* abort_error);
+uint32_t read_from_blob(blob_store_read_iterator* bsri_p, char* data, uint32_t data_size, const void* transaction_id, int* abort_error);
 
 // this function can be used to directly peek the next available consecutive bytes from blob_store
 // this function allows peeking directly into the page, by returning the memory pointer on the page, pointing to data_size number of bytes
 // returns NULL and 0, if there are no more bytes/binarys in the blob_store, this implies end of blob_store
 // peek will not move the iterator forward, you will need to call read_from_blob_store(wri_p, NULL, data_size, transaction_id, &abort_error); to make it move forward by that many bytes
 // on an abort_error all locks are released and NULL and 0 is returned
-const char* peek_in_blob_store(blob_store_read_iterator* bsri_p, uint32_t* data_size, const void* transaction_id, int* abort_error);
+const char* peek_in_blob(blob_store_read_iterator* bsri_p, uint32_t* data_size, const void* transaction_id, int* abort_error);
 
 // get current position of the blob_store_read_iterator, the curr_binary_index and curr_byte_index are output parameters, and the return value is the curr_page_id of the blob_store page that we are in
-uint64_t get_position_in_blob_store(blob_store_read_iterator* bsri_p, uint32_t* curr_tuple_index, uint32_t* curr_byte_index);
+uint64_t get_position_in_blob(blob_store_read_iterator* bsri_p, uint32_t* curr_tuple_index, uint32_t* curr_byte_index);
 
 void delete_blob_store_read_iterator(blob_store_read_iterator* bsri_p, const void* transaction_id, int* abort_error);
 
--- inc/tupleindexer/blob_store/blob_store_write_iterator_public.h
+++ inc/tupleindexer/blob_store/blob_store_write_iterator_public.h
@@ -21,21 +21,24 @@ typedef struct blob_store_write_iterator blob_store_write_iterator;
 
 blob_store_write_iterator* get_new_blob_store_write_iterator(uint64_t root_page_id, uint64_t head_page_id, uint32_t head_tuple_index, uint64_t tail_page_id, uint32_t tail_tuple_index, const blob_store_tuple_defs* wtd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error);
 
+// returns true only if both head and tail have NULL_PAGE_ID
+int is_empty_blob_in_blob_store(const blob_store_read_iterator* bsri_p);
+
 // returns head_page_id
-uint64_t get_head_position_in_blob_store(blob_store_read_iterator* bsri_p, uint32_t* head_tuple_index);
+uint64_t get_head_position_in_blob(blob_store_read_iterator* bsri_p, uint32_t* head_tuple_index);
 
 // tail_byte_index will be the size of the binary in the tail
-uint64_t get_tail_position_in_blob_store(blob_store_read_iterator* bsri_p, uint32_t* tail_tuple_index, uint32_t* tail_byte_index);
+uint64_t get_tail_position_in_blob(blob_store_read_iterator* bsri_p, uint32_t* tail_tuple_index, uint32_t* tail_byte_index);
 
 // if data_size == 0, return 0 directly
 // if data_size > 0, it returns bytes_appended for success, where 0 < bytes_appended <= data_size, bytes_appended could be non-zero lesser than data_size
 // on an abort_error, all the pages will be unlocked by the blob_store_write_iterator, and return value will be 0
-uint32_t append_to_tail_blob_store(blob_store_write_iterator* bswi_p, const char* data, uint32_t data_size, const void* transaction_id, int* abort_error);
+uint32_t append_to_tail_in_blob(blob_store_write_iterator* bswi_p, const char* data, uint32_t data_size, const void* transaction_id, int* abort_error);
 
 // if data_size == 0, return 0 directly
 // if data_size > 0, it returns bytes_discarded for success, where 0 < bytes_discarded <= data_size, bytes_discarded could be non-zero lesser than data_size
 // on an abort_error, all the pages will be unlocked by the blob_store_write_iterator, and return value will be 0
-uint32_t discard_from_head_blob_store(blob_store_write_iterator* bswi_p, uint32_t data_size, const void* transaction_id, int* abort_error);
+uint32_t discard_from_head_in_blob(blob_store_write_iterator* bswi_p, uint32_t data_size, const void* transaction_id, int* abort_error);
 
 // both the above functions try to modify minimal pages while appending a singular chunk of data
 // this allows you to brek your insets into smaller and smaller transactions
