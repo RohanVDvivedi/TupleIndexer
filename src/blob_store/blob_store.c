1e7ad3149017838cbc1ff11ff940cc9eaddb3d6e
RohanVDvivedi <rohandvivedi@gmail.com>
2026-05-01 23:36:28 +0530

    implemented blob store wrapper fuinctions to start working with it as heap page

--- src/blob_store/blob_store.c
+++ src/blob_store/blob_store.c
@@ -1,1 +1,18 @@
 #include<tupleindexer/blob_store/blob_store.h>
+
+#include<tupleindexer/heap_table/heap_table.h>
+
+uint64_t get_new_blob_store(const blob_store_tuple_defs* bstd_p, const page_access_methods* pam_p, const page_modification_methods* pmm_p, const void* transaction_id, int* abort_error)
+{
+	return get_new_heap_table(&(bstd_p->httd), pam_p, pmm_p, transaction_id, abort_error);
+}
+
+int destroy_blob_store(uint64_t root_page_id, const blob_store_tuple_defs* bstd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error)
+{
+	return destroy_heap_table(root_page_id, &(bstd_p->httd), pam_p, transaction_id, abort_error);
+}
+
+uint32_t get_root_level_blob_store(uint64_t root_page_id, const blob_store_tuple_defs* bstd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error)
+{
+	return get_root_level_heap_table(root_page_id, &(bstd_p->httd), pam_p, transaction_id, abort_error) + 1;
+}
