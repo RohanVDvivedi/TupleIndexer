3e60aff33788950c2f3470aec167b3a566420936
RohanVDvivedi <rohandvivedi@gmail.com>
2026-05-02 13:05:11 +0530

    implemented clone for the blob_store_read_iterator

--- inc/tupleindexer/blob_store/blob_store_read_iterator_public.h
+++ inc/tupleindexer/blob_store/blob_store_read_iterator_public.h
@@ -13,7 +13,7 @@ typedef struct blob_store_read_iterator blob_store_read_iterator;
 
 blob_store_read_iterator* get_new_blob_store_read_iterator(uint64_t head_page_id, uint32_t curr_tuple_index, uint32_t curr_byte_index, const blob_store_tuple_defs* bstd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
 
-blob_store_read_iterator* clone_blob_store_read_iterator(const blob_store_read_iterator* wri_p, const void* transaction_id, int* abort_error);
+blob_store_read_iterator* clone_blob_store_read_iterator(const blob_store_read_iterator* bsri_p, const void* transaction_id, int* abort_error);
 
 // if data_size == 0, then 0 is returned right away
 // if data == NULL, then a forward relative seek is performed for data_size bytes
@@ -25,7 +25,7 @@ uint32_t read_from_blob(blob_store_read_iterator* bsri_p, char* data, uint32_t d
 // this function can be used to directly peek the next available consecutive bytes from blob_store
 // this function allows peeking directly into the page, by returning the memory pointer on the page, pointing to data_size number of bytes
 // returns NULL and 0, if there are no more bytes/binarys in the blob_store, this implies end of blob_store
-// peek will not move the iterator forward, you will need to call read_from_blob_store(wri_p, NULL, data_size, transaction_id, &abort_error); to make it move forward by that many bytes
+// peek will not move the iterator forward, you will need to call read_from_blob_store(bsri_p, NULL, data_size, transaction_id, &abort_error); to make it move forward by that many bytes
 // on an abort_error all locks are released and NULL and 0 is returned
 const char* peek_in_blob(blob_store_read_iterator* bsri_p, uint32_t* data_size, const void* transaction_id, int* abort_error);
 
--- src/blob_store/blob_store_read_iterator.c
+++ src/blob_store/blob_store_read_iterator.c
@@ -6,7 +6,26 @@
 
 blob_store_read_iterator* get_new_blob_store_read_iterator(uint64_t head_page_id, uint32_t curr_tuple_index, uint32_t curr_byte_index, const blob_store_tuple_defs* bstd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
 
-blob_store_read_iterator* clone_blob_store_read_iterator(const blob_store_read_iterator* wri_p, const void* transaction_id, int* abort_error);
+blob_store_read_iterator* clone_blob_store_read_iterator(const blob_store_read_iterator* bsri_p, const void* transaction_id, int* abort_error)
+{
+	// allocate enough memory
+	blob_store_read_iterator* clone_p = malloc(sizeof(blob_store_read_iterator));
+	if(clone_p == NULL)
+		exit(-1);
+
+	clone_p->curr_page = acquire_persistent_page_with_lock(bsri_p->pam_p, transaction_id, bsri_p->curr_page.page_id, READ_LOCK, abort_error);
+	if(*abort_error)
+	{
+		free(clone_p);
+		return NULL;
+	}
+	clone_p->curr_tuple_index = bsri_p->curr_tuple_index;
+	clone_p->curr_byte_index = bsri_p->curr_byte_index;
+	clone_p->bstd_p = bsri_p->bstd_p;
+	clone_p->pam_p = bsri_p->pam_p;
+
+	return clone_p;
+}
 
 uint32_t read_from_blob(blob_store_read_iterator* bsri_p, char* data, uint32_t data_size, const void* transaction_id, int* abort_error);
 
