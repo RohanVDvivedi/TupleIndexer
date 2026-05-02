5fa1ca066194a3e81ac22d6939e3e1f5a7de0799
RohanVDvivedi <rohandvivedi@gmail.com>
2026-05-02 12:57:35 +0530

    started implementing read iterator for blob

--- src/blob_store/blob_store_read_iterator.c
+++ src/blob_store/blob_store_read_iterator.c
@@ -1,8 +1,30 @@
 #include<tupleindexer/blob_store/blob_store_read_iterator.h>
 
+#include<tupleindexer/utils/persistent_page_functions.h>
+
+#include<stdlib.h>
+
+blob_store_read_iterator* get_new_blob_store_read_iterator(uint64_t head_page_id, uint32_t curr_tuple_index, uint32_t curr_byte_index, const blob_store_tuple_defs* bstd_p, const page_access_methods* pam_p, const void* transaction_id, int* abort_error);
+
+blob_store_read_iterator* clone_blob_store_read_iterator(const blob_store_read_iterator* wri_p, const void* transaction_id, int* abort_error);
+
+uint32_t read_from_blob(blob_store_read_iterator* bsri_p, char* data, uint32_t data_size, const void* transaction_id, int* abort_error);
+
+const char* peek_in_blob(blob_store_read_iterator* bsri_p, uint32_t* data_size, const void* transaction_id, int* abort_error);
+
 uint64_t get_position_in_blob(const blob_store_read_iterator* bsri_p, uint32_t* curr_tuple_index, uint32_t* curr_byte_index)
 {
 	(*curr_byte_index) = bsri_p->curr_byte_index;
 	(*curr_tuple_index) = bsri_p->curr_tuple_index;
 	return bsri_p->curr_page.page_id;
+}
+
+void delete_blob_store_read_iterator(blob_store_read_iterator* bsri_p, const void* transaction_id, int* abort_error)
+{
+	// if curr_page is still locked, then release this lock
+	// it may not be locked, if we encountered an abort error
+	if(!is_persistent_page_NULL(&(bsri_p->curr_page), bsri_p->pam_p))
+		release_lock_on_persistent_page(bsri_p->pam_p, transaction_id, &(bsri_p->curr_page), NONE_OPTION, abort_error);
+
+	free(bsri_p);
 }
